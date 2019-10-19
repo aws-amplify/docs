56ab3d2dbf1d4090b6de0d05ad16f0d6376c8db6
Harry Solovay <harrysolovay@gmail.com>
2019-10-19 10:48:52 -0700

    removed unused service worker code

--- theme/assets/js/scripts.js
+++ theme/assets/js/scripts.js
@@ -1,18 +1,5 @@
 ( function( $ ) {
 	'use strict';
-	/*
-	if ('serviceWorker' in navigator) {
-		window.addEventListener('load', function() {
-			navigator.serviceWorker.register('/sw.js').then(function(registration) {
-			// Registration was successful
-			console.log('ServiceWorker registration successful with scope: ', registration.scope);
-			}, function(err) {
-			// registration failed :(
-			console.log('ServiceWorker registration failed: ', err);
-			});
-		});
-	}
-	*/
 
 (function(document, history, location) {
 	
