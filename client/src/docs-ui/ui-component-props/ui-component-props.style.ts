6a242e7e08959ac1cbefb5a01eca7f2a0b033455
jordanranz <jordanmranz@gmail.com>
2020-03-27 14:16:17 -0700

    Add fixed width for left table headers

--- client/src/docs-ui/ui-component-props/ui-component-props.style.ts
+++ client/src/docs-ui/ui-component-props/ui-component-props.style.ts
@@ -1,10 +1,13 @@
-import {css} from "emotion";
+import { css } from "emotion";
 
 export const tableStyle = css`
   text-align: left;
   width: 100%;
   thead tr {
     background-color: var(--bg-color-tertiary);
+  }
+  tbody tr th {
+    width: 5rem;
   }
   h3 {
     font-size: 1.25rem;
