37e58c40ecfa0bb1f0f406737458655d81bf2cf0
Harry Solovay <harrysolovay@gmail.com>
2020-03-30 13:21:42 -0700

    fix: filter state linking behavior

--- client/src/docs-ui/internal-link/internal-link.tsx
+++ client/src/docs-ui/internal-link/internal-link.tsx
@@ -32,6 +32,7 @@ export class DocsInternalLink {
   // @ts-ignore
   @Watch("selectedFilters")
   computeURL() {
+    console.log("computing ", this.selectedFilters);
     if (this.href) {
       const parsed = new Url(this.href, true);
       const {query, pathname, origin, hash} = parsed;
