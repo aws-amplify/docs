64484133b29f4ca5b513bfe865db5f546d7b49fc
jordanranz <jordanmranz@gmail.com>
2020-03-31 10:03:14 -0700

    Fix no rendering of TOC

--- client/src/utils/heading-is-visible.ts
+++ client/src/utils/heading-is-visible.ts
@@ -1,5 +1,6 @@
 const descendsFromDisplayNone = (element: Element): boolean => {
   if (!element?.parentElement) return false;
+  if (element.parentElement.tagName === "AMPLIFY-TOC-CONTENTS") return false;
   return !!(
     element.parentElement.style.display === "none" ||
     descendsFromDisplayNone(element.parentElement)
