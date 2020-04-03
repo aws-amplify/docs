82645ac7ae85f6a0bfbec5ed8ae273a72f5e6ad3
Harry Solovay <harrysolovay@gmail.com>
2020-04-03 13:28:03 -0700

    chore: improve mechanism for measuring nav height

--- client/src/utils/get-nav-height.ts
+++ client/src/utils/get-nav-height.ts
@@ -1,6 +1,28 @@
 import {Breakpoint} from "../amplify-ui/styles/media";
 
-export const getNavHeight = (rems?: boolean) => {
+let cachedAt = window.innerWidth;
+let cachedPxResult: number | undefined;
+let cachedRemsResult: number | undefined;
+
+export const getNavHeight = (rems?: boolean): number => {
+  if (cachedAt === innerWidth) {
+    if (rems && cachedRemsResult) {
+      return cachedRemsResult;
+    } else if (cachedPxResult) {
+      return cachedPxResult;
+    }
+  }
+
+  const nav = document.getElementById("secondary-nav");
+
+  if (nav) {
+    cachedAt = innerWidth;
+    cachedPxResult = parseInt(getComputedStyle(nav).height);
+    cachedRemsResult = cachedPxResult / 16;
+    return rems ? cachedRemsResult : cachedPxResult;
+  }
+
+  // fallback
   const value = innerWidth > Breakpoint.LAPTOP * 16 ? 3 : 6.25;
   return rems ? value : value * 16;
 };
