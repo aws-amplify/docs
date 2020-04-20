dd2472dc63bf75aa2a33a7be1772ef23f7071b52
Harry Solovay <harrysolovay@gmail.com>
2020-04-20 11:48:50 -0700

    chore: remove noise from update document head in favor of stencil approaach

--- client/src/utils/update-document-head.ts
+++ client/src/utils/update-document-head.ts
@@ -1,12 +1,6 @@
-import {Build} from "@stencil/core";
 import {Page} from "../api";
 
 export const updateDocumentHead = (page: Page): void => {
   const title = `Amplify Docs –– ${page.title}`;
   document.title = title;
-  if (Build.isBrowser) {
-    const meta = document.getElementsByTagName("meta");
-    meta.namedItem("description")?.setAttribute("content", title);
-    meta.namedItem("description")?.setAttribute("content", page.description);
-  }
 };
