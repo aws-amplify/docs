ff6ddac4d8fa345a1184b4e8bf902865f9ff3c06
jordanranz <jordanmranz@gmail.com>
2020-04-21 13:05:18 -0700

    Refactor callout style

--- client/src/docs-ui/feedback-callout/feedback-callout.tsx
+++ client/src/docs-ui/feedback-callout/feedback-callout.tsx
@@ -1,5 +1,12 @@
 import {Component, h, Host, State} from "@stencil/core";
-import {hostStyle, displayStyle} from "./feedback-callout.style";
+import {
+  hostStyle,
+  displayStyle,
+  arrowStyle,
+  arrowUpStyle,
+  exStyle,
+  calloutTextStyle,
+} from "./feedback-callout.style";
 
 const IS_DISMISSED_LOCAL_STORAGE_KEY =
   "amplify-docs::feedback-request-dismissed";
@@ -28,9 +35,17 @@ export class DocsFeedbackCallout {
 `,
           )}`}
         >
-          <h4>{`^ We'd love your feedback`}</h4>
+          <span class={calloutTextStyle}>
+            <i
+              class={{
+                [arrowStyle]: true,
+                [arrowUpStyle]: true,
+              }}
+            ></i>
+            <span class="text">{`We'd love your feedback`}</span>
+          </span>
         </amplify-external-link>
-        <button onClick={this.dismiss}>
+        <button class={exStyle} onClick={this.dismiss}>
           <img src="/assets/close.svg" />
         </button>
       </Host>
