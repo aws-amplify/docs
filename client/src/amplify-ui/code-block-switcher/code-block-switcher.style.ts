bac05b3914737a68403d903cf4a24516baa37e89
Harry Solovay <harrysolovay@gmail.com>
2020-04-29 11:20:56 -0700

    chore: update capi tests

--- client/src/amplify-ui/code-block-switcher/code-block-switcher.style.ts
+++ client/src/amplify-ui/code-block-switcher/code-block-switcher.style.ts
@@ -17,19 +17,19 @@ export const hostStyle = css`
 
 export const activeTabStyle = css`
   font-weight: 700;
+  border-bottom: 0.0625rem solid var(--font-color-contrast);
 `;
 
-// TODO: add code bg color secondary
-// TODO: use code font color instead
 export const tabStyle = css`
   color: var(--font-color-contrast);
-  border-radius: 0.25rem;
+  border-top-right-radius: 0.25rem;
+  border-top-left-radius: 0.25rem;
   height: 100%;
   appearance: none;
   background-color: transparent;
   padding: 0.75rem 0.5rem;
   cursor: pointer;
-  font-size: 0.75rem;
+  font-size: 0.8125rem;
 
   &:first-child {
     margin-left: 0.5rem;
