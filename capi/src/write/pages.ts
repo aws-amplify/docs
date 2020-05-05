3078b752a47f4b12819893ee243295c9267ec70e
Harry Solovay <harrysolovay@gmail.com>
2020-05-05 11:04:31 -0700

    chore: begin generating filter-specific page assets

--- capi/src/write/pages.ts
+++ capi/src/write/pages.ts
@@ -6,6 +6,14 @@ export async function pages(ctx: t.Ctx): Promise<void> {
   for (const [srcPath, page] of ctx.pageBySrcPath.entries()) {
     const pathDeduction = ctx.pathDeductionBySrcPath.get(srcPath);
     if (pathDeduction && pathDeduction.destinationPath) {
+      const filters =
+        page.filters ||
+        (page.filterKey
+          ? {
+              [page.filterKey]: ctx.config.filters?.[page.filterKey],
+            }
+          : undefined);
+      console.log(pathDeduction.route, filters);
       await fs.ensureDir(path.dirname(pathDeduction.destinationPath));
       await fs.writeFile(pathDeduction.destinationPath, JSON.stringify(page), {
         encoding: "utf8",
