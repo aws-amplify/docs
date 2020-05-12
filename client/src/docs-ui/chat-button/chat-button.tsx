1c36c28557f9dc1321924e947a380921f6c09d2b
Harry Solovay <harrysolovay@gmail.com>
2020-05-11 17:57:32 -0700

    chore: move links to constants module

--- client/src/docs-ui/chat-button/chat-button.tsx
+++ client/src/docs-ui/chat-button/chat-button.tsx
@@ -1,12 +1,13 @@
 import {Component, h} from "@stencil/core";
 import {discordChatStyle, logoStyle} from "./chat-button.style";
+import {DISCORD} from "../../constants/links";
 
 @Component({tag: "docs-chat-button", shadow: false})
 export class DocsChatButton {
   render() {
     return (
       <amplify-external-link
-        href="https://discord.gg/jWVbPfC"
+        href={DISCORD}
         class={discordChatStyle}
         graphic="white"
       >
--- client/src/docs-ui/links-banner/link-banner.tsx
+++ client/src/docs-ui/links-banner/link-banner.tsx
@@ -4,6 +4,7 @@ import {
   containerOuterStyle,
   logoStyle,
 } from "./link-banner.style";
+import * as links from "../../constants/links";
 
 @Component({tag: "docs-link-banner", shadow: false})
 export class DocsLinkBanner {
@@ -13,31 +14,19 @@ export class DocsLinkBanner {
         class={containerOuterStyle}
         innerClass={containerInnerStyle}
       >
-        <amplify-external-link
-          href="https://github.com/aws-amplify"
-          graphic="black"
-        >
+        <amplify-external-link href={links.GITHUB} graphic="black">
           <img src="/assets/github.svg" class={logoStyle} />
           Amplify GitHub
         </amplify-external-link>
-        <amplify-external-link
-          href="https://discord.gg/jWVbPfC"
-          graphic="black"
-        >
+        <amplify-external-link href={links.GITHUB} graphic="black">
           <img src="/assets/discord-blue.svg" class={logoStyle} />
           Amplify on Discord
         </amplify-external-link>
-        <amplify-external-link
-          href="https://aws.amazon.com/amplify/framework/"
-          graphic="black"
-        >
+        <amplify-external-link href={links.MARKETING} graphic="black">
           <img src="/assets/aws-dark.svg" class={logoStyle} />
           Amplify Resources
         </amplify-external-link>
-        <amplify-external-link
-          href="https://amplify.aws/community/"
-          graphic="black"
-        >
+        <amplify-external-link href={links.COMMUNITY} graphic="black">
           <img src="/assets/logo-dark.svg" class={logoStyle} />
           Amplify Community
         </amplify-external-link>
