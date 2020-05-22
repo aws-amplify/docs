ce4a9d1c77cf0dbc1f97af7ad5da1b067a94f75d
Rene Brandel <renbran@amazon.com>
2020-05-22 09:01:56 -0700

    making the switcher fill out unused space

--- client/src/docs-ui/menu/menu.tsx
+++ client/src/docs-ui/menu/menu.tsx
@@ -65,7 +65,6 @@ export class DocsMenu {
                     }}
                     rightOption={{
                       title: "SDK",
-                      subTitle: "\u00A0",
                       href: "/sdk",
                     }}
                   />
