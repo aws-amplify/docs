885cbc570f61368156af731cf1a0bc0fb1cdf0b7
Harry Solovay <harrysolovay@gmail.com>
2020-06-05 13:37:09 -0700

    fix: console error from reinitialization with no valid selection

--- client/src/docs-ui/search-bar/search-bar.tsx
+++ client/src/docs-ui/search-bar/search-bar.tsx
@@ -1,20 +1,31 @@
-import {Component, Host, h, Build, Listen} from "@stencil/core";
+import {Component, Host, h, Build, Listen, Element} from "@stencil/core";
 import {searchStyle} from "./search-bar.style";
 import {transformData} from "../../utils/transform-search-data";
-import {ALGOLIA_API_KEY, ALGOLIA_INDEX_NAME} from "../../constants/algolia";
+import {
+  ALGOLIA_API_KEY,
+  ALGOLIA_INDEX_NAME,
+  UNINITIALIZED_SEARCH_INPUT_SELECTOR,
+} from "../../constants/algolia";
 
 @Component({tag: "docs-search-bar", shadow: false})
 export class DocsSearchBar {
+  @Element() element: HTMLElement;
+
   initDocSearch() {
     if (Build.isBrowser) {
-      // @ts-ignore
-      docsearch({
-        apiKey: ALGOLIA_API_KEY,
-        indexName: ALGOLIA_INDEX_NAME,
-        inputSelector: ".amplify-docs-search-input:not(.ds-input)",
-        debug: false,
-        transformData,
-      });
+      const nToInit = this.element.querySelectorAll(
+        UNINITIALIZED_SEARCH_INPUT_SELECTOR,
+      ).length;
+      if (nToInit > 0) {
+        // @ts-ignore
+        docsearch({
+          apiKey: ALGOLIA_API_KEY,
+          indexName: ALGOLIA_INDEX_NAME,
+          inputSelector: UNINITIALIZED_SEARCH_INPUT_SELECTOR,
+          debug: false,
+          transformData,
+        });
+      }
     }
   }
 
