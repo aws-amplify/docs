a892af79b01e6e9e88a2089b5b65bd43c0ba687d
Thomas Leing <bluezebragames@gmail.com>
2021-01-08 11:03:57 -0500

    Dependabot version bump (#2829)
    * Update Jest version to fix dependabot alert
    
    * Fix ts-jest version; also update aws-amplify packages to current latest for axios vuln.
    
    * Fix tests; package.json back to using "latest" because of the amplify-js release
    
    Co-authored-by: Thomas Leing <leint@amazon.com>

--- client/src/docs-ui/landing-hero-cta/landing-hero-cta.spec.ts
+++ client/src/docs-ui/landing-hero-cta/landing-hero-cta.spec.ts
@@ -4,11 +4,13 @@ import {newSpecPage} from "@stencil/core/testing";
 describe("docs-landing-hero-cta", () => {
   describe("Render logic", () => {
     it("should render", async () => {
+      Error.stackTraceLimit = 50;
+
       expect(
         (
           await newSpecPage({
             components: [DocsLandingHeroCTA],
-            html: `<docs-landing-page-hero-cta />`,
+            html: `<docs-landing-hero-cta />`,
           })
         ).root,
       ).toMatchSnapshot();
