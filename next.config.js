d8d9f53d5b3e836d294b7c5931b95c1e8c02b2f0
Jake Burden <jakburde@amazon.com>
2021-04-27 23:09:03 -0400

    Ignore typescript build errors

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withMDX = require("@next/mdx")();
-module.exports = withMDX({pageExtensions: ["js", "jsx", "mdx"]});
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "mdx"],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+});
