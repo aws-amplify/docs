0e30dbf48231bebe670e6ced472f0db832b37d4e
Jake Burden <jakburde@amazon.com>
2021-04-28 10:49:14 -0400

    Add tsx pages and future webpack support

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withMDX = require("@next/mdx")();
 module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "mdx"],
+  pageExtensions: ["js", "jsx", "mdx", "tsx"],
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
     ignoreBuildErrors: true,
+  },
+  future: {
+    webpack5: true,
   },
 });
