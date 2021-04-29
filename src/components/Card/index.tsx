3f60077f00c3d17eb98307d936df85fee05583cf
Jake Burden <jakburde@amazon.com>
2021-04-29 16:29:04 -0400

    Add Card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,9 @@
+import {Card, CardDetail, CardGraphic} from './styles'
+
+export default function DocsCard ({children}) {
+  return (
+    <Card>{children}</Card>
+  )
+}
+
+export { CardDetail, CardGraphic }
