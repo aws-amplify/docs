faaacc7e1d5ca32f474b7c97e209f8692ba7055f
Jake Burden <jakburde@amazon.com>
2021-04-29 09:58:13 -0400

    Add Grid and Container components

---
+++ src/components/Container/styles.tsx
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+import { MAX_WIDTH } from "../media";
+
+export const Container = styled.section(
+  (props) => `
+  display: block;
+  width: 100%;
+  ${
+    props.backgroundColor
+      ? `background-color: var(--${props.backgroundColor});`
+      : ``
+  }
+
+  > div {
+    max-width: ${MAX_WIDTH};
+    margin: 0px auto;
+  }
+`
+);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,33 @@
+import { Grid } from 'theme-ui'
+
 import Hero from "../components/Hero";
 import LandingHeroCTA from "../components/LandingHeroCTA";
+import Container from "../components/Container";
 
 const Page = () => {
   return (
-    <Hero>
-      <h1 className="font-weight-300">Amplify Framework Documentation</h1>
-      <p>
-        Learn how to use Amplify to develop and deploy cloud-powered mobile and
-        web apps
-      </p>
+    <>
+      <Hero>
+        <h1 className="font-weight-300">Amplify Framework Documentation</h1>
+        <p>
+          Learn how to use Amplify to develop and deploy cloud-powered mobile
+          and web apps
+        </p>
 
-      <LandingHeroCTA />
-    </Hero>
+        <LandingHeroCTA />
+      </Hero>
+      <Container backgroundColor="color-off-white">
+        <div className="padding-top-lg padding-bottom-lg padding-horizontal-md">
+          <h4 className="text-align-center">Discover the end-to-end AWS solution for mobile and front-end web developers</h4>
+        </div>
+        <Grid columns={4}>
+          <p>card</p>
+          <p>card</p>
+          <p>card</p>
+          <p>card</p>
+        </Grid>
+      </Container>
+    </>
   );
 };
 
