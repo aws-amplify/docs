cfcd20d061664c4edea2d3ed0f054f8832636125
Jake Burden <jakburde@amazon.com>
2021-04-29 16:29:04 -0400

    Add Card component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Grid } from 'theme-ui'
 import Hero from "../components/Hero";
 import LandingHeroCTA from "../components/LandingHeroCTA";
 import Container from "../components/Container";
+import Card, {CardDetail, CardGraphic} from '../components/Card'
 
 const Page = () => {
   return (
@@ -21,10 +22,42 @@ const Page = () => {
           <h4 className="text-align-center">Discover the end-to-end AWS solution for mobile and front-end web developers</h4>
         </div>
         <Grid columns={4}>
-          <p>card</p>
-          <p>card</p>
-          <p>card</p>
-          <p>card</p>
+          <Card>
+            <CardGraphic src="/lib.png"/>
+            <CardDetail>
+              <h4>Amplify Libraries</h4>
+              <p>
+                Connect app to new or existing AWS services (Cognito, S3, and more).
+              </p>
+            </CardDetail>
+          </Card>
+          <Card>
+            <CardGraphic src="/cli.png"/>
+            <CardDetail>
+              <h4>Amplify CLI</h4>
+              <p>
+                Configure an app backend with a guided CLI workflow.
+              </p>
+            </CardDetail>
+          </Card>
+          <Card>
+            <CardGraphic src="/console.png"/>
+            <CardDetail>
+              <h4>Amplify Console</h4>
+              <p>
+                AWS service for creating an app backend and hosting full-stack web apps.
+              </p>
+            </CardDetail>
+          </Card>
+          <Card>
+            <CardGraphic src="/console.png"/>
+            <CardDetail>
+              <h4>NEW! Amplify Admin UI</h4>
+              <p>
+                Visually configure and manage your app backend.
+              </p>
+            </CardDetail>
+          </Card>
         </Grid>
       </Container>
     </>
