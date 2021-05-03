bd79cb35fa56620ad57dd9b98e6010d927cfc54a
Jake Burden <jakburde@amazon.com>
2021-05-03 12:35:20 -0400

    Add backgroundColor prop type to Container

--- src/components/Container/styles.tsx
+++ src/components/Container/styles.tsx
@@ -1,15 +1,20 @@
 import styled from "@emotion/styled";
-import { MAX_WIDTH } from "../media";
+import {MAX_WIDTH} from "../media";
 
-export const Container = styled.section(
+type ContainerProps = {
+  backgroundColor: string;
+};
+
+export const Container = styled.section<ContainerProps>(
   (props) => `
   display: block;
   width: 100%;
-  ${props.backgroundColor && `background-color: var(--${props.backgroundColor});`}
+  ${props.backgroundColor &&
+    `background-color: var(--${props.backgroundColor});`}
 
   > div {
     max-width: ${MAX_WIDTH};
     margin: 0px auto;
   }
-`
+`,
 );
