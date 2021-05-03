8032588bba55b52287426290eec22a62368d04f4
Jake Burden <jakburde@amazon.com>
2021-05-03 10:43:10 -0400

    Clean up Hero component

--- src/components/Hero/styles.tsx
+++ src/components/Hero/styles.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 
-export const Container = styled.div`
+export const Hero = styled.div`
   display: block;
   text-align: center;
   padding: 3rem 1.5rem 1.5rem;
