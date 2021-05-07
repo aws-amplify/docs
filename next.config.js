8ec987031740f4db6b6b566ec206188eacce95bd
Thomas Leing <leint@amazon.com>
2021-05-07 16:03:29 -0400

    404 on incorrect platform demo; link headings with # URI paths

--- next.config.js
+++ next.config.js
@@ -1,5 +1,72 @@
+const mdxRenderer = `
+  import { mdx } from "@mdx-js/react";
+
+  export async function getStaticProps ({params}) {
+    console.log(params);
+    console.log(params.platform);
+
+    if(params.platform == 'js') {
+      return {
+        props: {},
+      };
+    }
+    else {
+      return {
+        props: {},
+        notFound: true,
+      };
+    }
+  }
+
+  export async function getStaticPaths ({params}) {
+    return {
+      paths: [],
+      fallback: true,
+    }
+  }
+`;
+
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withMDX = require("@next/mdx")();
+const visit = require("unist-util-visit");
+const mdxTocPlugin = () => (tree) => {
+  const slug = function(str) {
+    let slugged = "";
+    for (const c of str) {
+      if (c >= "A" && c <= "z") {
+        slugged += c.toLowerCase();
+      } else if (c >= "0" && c <= "9") {
+        slugged += c;
+      } else if (c === " " || c === "-") {
+        slugged += "-";
+      }
+    }
+    return slugged;
+  };
+
+  visit(tree, "heading", (heading) => {
+    const node = {...heading};
+    const data = node.data || (node.data = {});
+    const props = data.hProperties || (data.hProperties = {});
+    const id = slug(node.children[0].value);
+    data.id = id;
+    props.id = id;
+    node.depth++;
+    heading.children = [node];
+    heading.type = "link";
+    heading.url = `#${id}`;
+
+    return visit.SKIP;
+  });
+};
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMDX = require("@next/mdx")({
+  options: {
+    remarkPlugins: [mdxTocPlugin],
+    renderer: mdxRenderer,
+  },
+});
+
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx", "tsx"],
   typescript: {
---
+++ src/pages/lib/auth/getting-started/q/platform/fragments/js/_getting-started-steps-basic-auth.mdx
@@ -0,0 +1,34 @@
+<amplify-block-switcher>
+<amplify-block name="Web">
+
+Install the necessary dependencies by running the following command:
+
+```sh
+npm install aws-amplify
+```
+
+</amplify-block>
+<amplify-block name="React Native">
+
+Install the necessary dependencies by running the following command:
+
+```sh
+npm install aws-amplify amazon-cognito-identity-js @react-native-community/netinfo
+```
+
+You will also need to install the pod dependencies for iOS:
+
+```sh
+npx pod-install
+```
+</amplify-block>
+<amplify-block name="Expo">
+
+Install the necessary dependencies by running the following command:
+
+```sh
+npm install aws-amplify @react-native-community/netinfo
+```
+
+</amplify-block>
+</amplify-block-switcher>
