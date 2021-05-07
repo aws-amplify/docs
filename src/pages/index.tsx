fd2fc13ef4a198dd9d7bdff78de2ee80774014f4
Jake Burden <jakburde@amazon.com>
2021-05-07 18:08:44 -0400

    Adjust padding around grids

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,43 +26,44 @@ const Page = () => {
             Discover the end-to-end AWS solution for mobile and front-end web
             developers
           </h4>
+
+          <Grid columns={[1, null, null, 4]}>
+            <Card href="/lib/q/platform/js">
+              <CardGraphic src="/lib.png" />
+              <CardDetail>
+                <h4>Amplify Libraries</h4>
+                <p>
+                  Connect app to new or existing AWS services (Cognito, S3, and
+                  more).
+                </p>
+              </CardDetail>
+            </Card>
+            <Card href="/cli">
+              <CardGraphic src="/cli.png" />
+              <CardDetail>
+                <h4>Amplify CLI</h4>
+                <p>Configure an app backend with a guided CLI workflow.</p>
+              </CardDetail>
+            </Card>
+            <Card href="/console">
+              <CardGraphic src="/console.png" />
+              <CardDetail>
+                <h4>Amplify Console</h4>
+                <p>
+                  AWS service for creating an app backend and hosting full-stack
+                  web apps.
+                </p>
+              </CardDetail>
+            </Card>
+            <Card href="/console/adminui/intro">
+              <CardGraphic src="/console.png" />
+              <CardDetail>
+                <h4>NEW! Amplify Admin UI</h4>
+                <p>Visually configure and manage your app backend.</p>
+              </CardDetail>
+            </Card>
+          </Grid>
         </div>
-        <Grid columns={[1, null, null, 4]}>
-          <Card href="/lib/q/platform/js">
-            <CardGraphic src="/lib.png" />
-            <CardDetail>
-              <h4>Amplify Libraries</h4>
-              <p>
-                Connect app to new or existing AWS services (Cognito, S3, and
-                more).
-              </p>
-            </CardDetail>
-          </Card>
-          <Card href="/cli">
-            <CardGraphic src="/cli.png" />
-            <CardDetail>
-              <h4>Amplify CLI</h4>
-              <p>Configure an app backend with a guided CLI workflow.</p>
-            </CardDetail>
-          </Card>
-          <Card href="/console">
-            <CardGraphic src="/console.png" />
-            <CardDetail>
-              <h4>Amplify Console</h4>
-              <p>
-                AWS service for creating an app backend and hosting full-stack
-                web apps.
-              </p>
-            </CardDetail>
-          </Card>
-          <Card href="/console/adminui/intro">
-            <CardGraphic src="/console.png" />
-            <CardDetail>
-              <h4>NEW! Amplify Admin UI</h4>
-              <p>Visually configure and manage your app backend.</p>
-            </CardDetail>
-          </Card>
-        </Grid>
       </Container>
 
       <FeaturesGrid />
