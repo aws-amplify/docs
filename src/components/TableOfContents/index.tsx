f424d33d1950ec1767b1f0e71b7eb02991b39f7c
Thomas Leing <leint@amazon.com>
2021-05-18 16:45:28 -0400

    Quick fix ToC CSS

--- src/components/TableOfContents/index.tsx
+++ src/components/TableOfContents/index.tsx
@@ -5,7 +5,9 @@ import {TOCStyle, H2AnchorStyle, H3AnchorStyle, HeaderStyle} from "./styles";
 export default function TableOfContents({children, title}) {
   return (
     <TOCStyle>
-      <HeaderStyle>{title}</HeaderStyle>
+      <HeaderStyle>
+        <h4>{title}</h4>
+      </HeaderStyle>
       {children.map(([name, level], index) => {
         const slugged = `#${slug(name)}`;
         const anchor = <a href={slugged}>{name}</a>;
