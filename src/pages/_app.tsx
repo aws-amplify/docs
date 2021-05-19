62eb973c0060a1a8cf9d6043c8ae1d205d1190f9
Jake Burden <jakburde@amazon.com>
2021-05-18 21:04:14 -0400

    Add favicon references

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,91 @@
 import "../styles/styles.css";
+import Head from "next/head";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="msapplication-TileImage"
+          content="/icon/ms-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/icon/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/icon/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/icon/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/icon/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/icon/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/icon/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/icon/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icon/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icon/apple-icon-180x180.png"
+        />
+        <link rel="apple-touch-icon" href="/icon/apple-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/icon/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/icon/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icon/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/icon/icon.png" />
+        <link rel="icon" type="image/x-icon" href="/icon/favicon.ico" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
