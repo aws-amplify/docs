9cea9661b26fbc927c5c2768889d953d1c55463c
Jake Burden <jakburde@amazon.com>
2021-05-21 11:36:51 -0400

    Replace next/image with img tag for static exports

--- src/components/LandingHeroCTA/index.tsx
+++ src/components/LandingHeroCTA/index.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 import {PlatformsGroup} from "./styles";
 import InternalLinkButton from "../InternalLinkButton";
 
@@ -12,32 +11,32 @@ export default function LandingHeroCTA() {
       <PlatformsGroup>
         <Link href="/start/q/integration/react?sc_icampaign=react-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/react.svg" />
+            <img height={40} width={40} src="/integrations/react.svg" />
           </a>
         </Link>
         <Link href="/start/q/integration/vue?sc_icampaign=vue-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/vue.svg" />
+            <img height={40} width={40} src="/integrations/vue.svg" />
           </a>
         </Link>
         <Link href="/start/q/integration/js?sc_icampaign=js-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/js.svg" />
+            <img height={40} width={40} src="/integrations/js.svg" />
           </a>
         </Link>
         <Link href="/start/q/integration/ios?sc_icampaign=ios-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/ios.svg" />
+            <img height={40} width={40} src="/integrations/ios.svg" />
           </a>
         </Link>
         <Link href="/start/q/integration/android?sc_icampaign=android-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/android.svg" />
+            <img height={40} width={40} src="/integrations/android.svg" />
           </a>
         </Link>
         <Link href="/start/q/integration/flutter?sc_icampaign=flutter-start&sc_ichannel=docs-home">
           <a>
-            <Image height={40} width={40} src="/integrations/flutter.svg" />
+            <img height={40} width={40} src="/integrations/flutter.svg" />
           </a>
         </Link>
       </PlatformsGroup>
