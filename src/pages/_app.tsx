7fa29297992e94fce33598a3201e4949479de0ef
Jake Burden <jakburde@amazon.com>
2021-05-29 01:06:43 -0400

    Update favicon path

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,80 +7,80 @@ function MyApp({Component, pageProps}) {
       <Head>
         <meta
           name="msapplication-TileImage"
-          content="/icon/ms-icon-144x144.png"
+          content="/assets/icon/ms-icon-144x144.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="57x57"
-          href="/icon/apple-icon-57x57.png"
+          href="/assets/icon/apple-icon-57x57.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="60x60"
-          href="/icon/apple-icon-60x60.png"
+          href="/assets/icon/apple-icon-60x60.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="72x72"
-          href="/icon/apple-icon-72x72.png"
+          href="/assets/icon/apple-icon-72x72.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="76x76"
-          href="/icon/apple-icon-76x76.png"
+          href="/assets/icon/apple-icon-76x76.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="114x114"
-          href="/icon/apple-icon-114x114.png"
+          href="/assets/icon/apple-icon-114x114.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="120x120"
-          href="/icon/apple-icon-120x120.png"
+          href="/assets/icon/apple-icon-120x120.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="144x144"
-          href="/icon/apple-icon-144x144.png"
+          href="/assets/icon/apple-icon-144x144.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="152x152"
-          href="/icon/apple-icon-152x152.png"
+          href="/assets/icon/apple-icon-152x152.png"
         />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/icon/apple-icon-180x180.png"
+          href="/assets/icon/apple-icon-180x180.png"
         />
-        <link rel="apple-touch-icon" href="/icon/apple-icon.png" />
+        <link rel="apple-touch-icon" href="/assets/icon/apple-icon.png" />
         <link
           rel="icon"
           type="image/png"
           sizes="192x192"
-          href="/icon/android-icon-192x192.png"
+          href="/assets/icon/android-icon-192x192.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/icon/favicon-32x32.png"
+          href="/assets/icon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="96x96"
-          href="/icon/favicon-96x96.png"
+          href="/assets/icon/favicon-96x96.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/icon/favicon-16x16.png"
+          href="/assets/icon/favicon-16x16.png"
         />
-        <link rel="apple-touch-icon" href="/icon/icon.png" />
-        <link rel="icon" type="image/x-icon" href="/icon/favicon.ico" />
+        <link rel="apple-touch-icon" href="/assets/icon/icon.png" />
+        <link rel="icon" type="image/x-icon" href="/assets/icon/favicon.ico" />
       </Head>
 
       <Component {...pageProps} />
