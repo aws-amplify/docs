4879d304a4d842bb86badd5b3fa717d80a75c35d
Jake Burden <jakburde@amazon.com>
2021-06-14 15:18:54 -0400

    Init /start page

---
+++ src/pages/start.tsx
@@ -0,0 +1,10 @@
+import Layout from "../components/Layout";
+
+export default function Page() {
+  return (
+    <Layout>
+      <h2>Choose a platform or framework</h2>
+      <h3>Web</h3>
+    </Layout>
+  );
+}
