86b7e57ddef7bb297b1e1a8490669124427fc6de
Thomas Leing <leint@amazon.com>
2021-07-08 23:23:32 -0700

    UI VersionSwitcher component; break out FilterSelect into its own directory

---
+++ src/components/Menu/FilterSelect/index.tsx
@@ -0,0 +1,115 @@
+import Link from "next/link";
+import Image from "next/image";
+import {filterMetadataByOption} from "../../../utils/filter-data";
+import {
+  FilterSelectStyle,
+  CurrentlySelectedStyle,
+  DropdownStyle,
+} from "./styles";
+import React from "react";
+import {NextRouter, withRouter} from "next/router";
+
+type FilterSelectProps = {
+  filters: string[];
+  filterKey: string;
+  pathname: string;
+  router: NextRouter;
+};
+
+type FilterSelectState = {
+  isOpen: boolean;
+};
+
+class FilterSelect extends React.Component<
+  FilterSelectProps,
+  FilterSelectState
+> {
+  wrapperRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props) {
+    super(props);
+
+    this.wrapperRef = React.createRef();
+    this.closeMenu = this.closeMenu.bind(this);
+    this.state = {isOpen: false};
+  }
+
+  componentDidMount() {
+    document.addEventListener("mousedown", this.closeMenu);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("mousedown", this.closeMenu);
+  }
+
+  closeMenu = (event: MouseEvent) => {
+    if (
+      this.wrapperRef &&
+      !this.wrapperRef.current.contains(event.target as Node) &&
+      this.state.isOpen
+    ) {
+      this.setState({
+        isOpen: false,
+      });
+    }
+  };
+
+  toggleVis = () => {
+    this.setState((oldState) => {
+      return {isOpen: !oldState.isOpen};
+    });
+  };
+
+  render() {
+    let filterKind = "";
+    if ("platform" in this.props.router.query) {
+      filterKind = "platform";
+    } else if ("integration" in this.props.router.query) {
+      filterKind = "integration";
+    } else {
+      filterKind = "framework";
+    }
+
+    return (
+      <FilterSelectStyle ref={this.wrapperRef}>
+        <CurrentlySelectedStyle>
+          <a onClick={this.toggleVis}>
+            <Image
+              src={filterMetadataByOption[this.props.filterKey].graphicURI}
+              height="28px"
+              width="28px"
+            />
+            <span>{filterMetadataByOption[this.props.filterKey].label}</span>
+          </a>
+        </CurrentlySelectedStyle>
+        <DropdownStyle shouldDisplay={this.state.isOpen}>
+          {this.props.filters.map((name) => {
+            if (name === this.props.filterKey) return;
+            const query = {};
+            query[filterKind] = name;
+            return (
+              <Link
+                href={{
+                  pathname: this.props.pathname,
+                  query: query,
+                }}
+                key={name}
+              >
+                <a onClick={this.toggleVis}>
+                  <Image
+                    src={filterMetadataByOption[name].graphicURI}
+                    height="28px"
+                    width="28px"
+                  />
+                  <span>{filterMetadataByOption[name].label}</span>
+                </a>
+              </Link>
+            );
+          })}
+        </DropdownStyle>
+      </FilterSelectStyle>
+    );
+  }
+}
+
+export default withRouter(FilterSelect);
---
+++ src/components/Menu/VersionSwitcher/index.tsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import {ActiveSwitchStyle, SwitchStyle} from "./styles";
+
+const Option = function({href, title, isActive}) {
+  const SwitchStyle = isActive ? ActiveSwitchStyle : "a";
+  return (
+    <div>
+      <Link href={href}>
+        <SwitchStyle href={href}>
+          <span>{title}</span>
+        </SwitchStyle>
+      </Link>
+    </div>
+  );
+};
+
+export default function VersionSwitcher({href}) {
+  let leftActive = false;
+  let hrefEnd;
+  if (href.includes("/ui/")) {
+    leftActive = true;
+    hrefEnd = href.split("/ui/")[1];
+  } else {
+    hrefEnd = href.split("/ui-legacy/")[1];
+  }
+  const leftOption = {
+    title: "Latest",
+    href: "/ui/" + hrefEnd,
+  };
+  const rightOption = {
+    title: "Legacy",
+    href: "/ui-legacy/" + hrefEnd,
+  };
+
+  return (
+    <SwitchStyle>
+      <Option
+        href={leftOption.href}
+        title={leftOption.title}
+        isActive={leftActive}
+      />
+      <Option
+        href={rightOption.href}
+        title={rightOption.title}
+        isActive={!leftActive}
+      />
+    </SwitchStyle>
+  );
+}
