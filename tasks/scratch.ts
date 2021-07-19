75e4b5de1e9d19d668cf83dced0f2d1cab824c74
Jake Burden <jakburde@amazon.com>
2021-07-19 01:15:12 -0400

    Skip routes with no filters for now

--- tasks/scratch.ts
+++ tasks/scratch.ts
@@ -7,7 +7,7 @@ import fs from "fs";
 
 const pathMap = generatePathMap(directory);
 
-console.log(pathMap);
+// console.log(pathMap);
 
 function generatePathMap(obj, pathMap = {}) {
   for (const [_, value] of Object.entries(obj)) {
@@ -19,30 +19,33 @@ function generatePathMap(obj, pathMap = {}) {
     if (items) {
       generatePathMap(items, pathMap);
     } else {
-      if (filters) {
-        let page = "";
-        let routeType = "";
-        ["platform", "framework", "integration"].forEach((type) => {
-          const src = `${route}/q/${type}/[${type}].mdx`;
-          const maybeFile = "./src/pages" + src;
-          if (fs.existsSync(maybeFile)) {
-            page = src;
-            routeType = type;
-          }
-        });
-        filters.forEach((filter) => {
-          const query = {};
-          query[routeType] = filter;
-          console.log({
-            page,
-            query,
-          });
-          pathMap[route + "/q/" + routeType + "/" + filter] = {
-            page,
-            query,
-          };
-        });
+      if (!filters.length) {
+        continue;
       }
+
+      let page = "";
+      let routeType = "";
+      ["platform", "framework", "integration"].forEach((type) => {
+        const src = `${route}/q/${type}/[${type}].mdx`;
+        const maybeFile = "./src/pages" + src;
+        if (fs.existsSync(maybeFile)) {
+          page = src;
+          routeType = type;
+        }
+      });
+
+      if (!page || !routeType) {
+        continue;
+      }
+
+      filters.forEach((filter) => {
+        const query = {};
+        query[routeType] = filter;
+        pathMap[route + "/q/" + routeType + "/" + filter] = {
+          page,
+          query,
+        };
+      });
     }
   }
   return pathMap;
