ce4cdbaf312a38b7d19737ec533c199e3176a12c
Thomas Leing <leint@amazon.com>
2021-07-21 17:14:07 -0700

    Migration changes: better `hash`ing, fix src paths, fix <br>s, fix multiple newlines

--- tasks/migrate.ts
+++ tasks/migrate.ts
@@ -1,51 +1,55 @@
 import * as fs from "fs-extra";
 import fg from "fast-glob";
 import mkdirp from "mkdirp";
-import {createHash, randomBytes} from "crypto";
 import {isProductRoot} from "../src/utils/getLocalDirectory";
 
-const hash = function(str: string) {
-  return createHash("md5")
-    .update(str)
-    .digest("hex");
+let counter = 0;
+const hash = function() {
+  return String(counter++);
 };
 
 const replacer1 = function(_1, p1: string, p2: string, _2, _3) {
-  const salt = randomBytes(1).toString("hex");
-  return `\n\nimport all${hash(p1 + p2) +
-    salt} from "/src/fragments/${p1}${p2}x";\n
-<Fragments fragments={{all: all${hash(p1 + p2) + salt}}} />`;
+  const h = hash();
+  return `\n\nimport all${h} from "/src/fragments/${p1}${p2}x";\n
+<Fragments fragments={{all: all${h}}} />`;
 };
 
 const replacer2 = function(_1, p1: string, p2: string, p3: string, _2, _3) {
-  const salt = randomBytes(1).toString("hex");
-  return `\n\nimport ${p1 +
-    hash(p2 + p3) +
-    salt} from "/src/fragments/${p2}${p3}x";\n
-<Fragments fragments={{${p1}: ${p1 + hash(p2 + p3) + salt}}} />`;
+  const h = hash();
+  if (p1 === "react-native") {
+    return `\n\nimport ${"react_native" +
+      h} from "/src/fragments/${p2}${p3}x";\n
+  <Fragments fragments={{"react-native": ${"react_native" + h}}} />`;
+  }
+  return `\n\nimport ${p1 + h} from "/src/fragments/${p2}${p3}x";\n
+<Fragments fragments={{${p1}: ${p1 + h}}} />`;
 };
 
 const grab = function() {
   for (const chunk of fg.sync("docs/**/*.md", {
     cwd: "./",
   })) {
-    let toWrite = "";
+    let frontMatterToWrite = "";
     let destination = chunk.slice(5); // slice off docs/
     let file = fs.readFileSync(chunk).toString();
     if (chunk.includes("fragments")) {
-      destination = destination.split("fragments").join("");
+      destination = destination.split("fragments/").join("");
+      destination = destination.split("/fragments").join("");
       destination = "src/fragments/" + destination + "x";
     } else {
       const frontMatter = file.split("---")[1].split("\n");
-      file = file.split("---")[2];
+      file = file
+        .split("---")
+        .slice(2)
+        .join("---");
       let filterKey = "";
-      toWrite += "export const meta = {\n";
+      frontMatterToWrite += "export const meta = {\n";
       for (const line of frontMatter) {
         const [key, value] = line.split(": ");
         if (typeof key === "undefined" || typeof value === "undefined")
           continue;
         if (key === "filterKey") filterKey = value;
-        toWrite += `  ${key}: "${value}",\n`;
+        frontMatterToWrite += `  ${key}: \`${value}\`,\n`;
       }
 
       destination = destination.slice(0, -3); // slice off .md
@@ -65,7 +69,7 @@ const grab = function() {
           destination = "src/pages/" + destination + ".mdx";
         }
       }
-      toWrite += "};\n\n";
+      frontMatterToWrite += "};\n\n";
 
       try {
         if (isProductRoot(destination)) {
@@ -80,25 +84,26 @@ const grab = function() {
       }
     }
 
+    // src=[path] -> src="[path]"
+    // complicated regex because we don't want to surround strings
+    // that already have quotes with more quotes
+    file = file.replace(/src=([^"](?:.*?)[^"])>/g, 'src="$1">');
+    file = file.replace(/src=~(.*?)>/g, "src=$1>");
+
+    // reset counter
+    counter = 0;
     // inline-fragment -> Fragments
     // <inline-fragment src="~/{1}/fragments{2}"></inline-fragment> ->
     //   import all from "/src/fragments/{1}{2}x";
     //   <Fragments fragments={all: all} />
-    // file = file.replace(
-    //   /<inline-fragment src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/g,
-    //   replacer1,
-    // );
-
-    while (
-      file.search(
-        /<inline-fragment src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/,
-      ) > -1
-    ) {
-      file = file.replace(
-        /<inline-fragment src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/,
-        replacer1,
-      );
-    }
+    file = file.replace(
+      /<inline-fragment src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/g,
+      replacer1,
+    );
+    file = file.replace(
+      /<inline-fragment src="\/(.*?)fragments\/(.*?)"><\/inline-fragment>/g,
+      replacer1,
+    );
 
     // <inline-fragment platform/integration="{1}" src="~/{2}/fragments{3}"></inline-fragment> ->
     //   import {1} from "/src/fragments/{2}{3}x";
@@ -107,17 +112,6 @@ const grab = function() {
       /<inline-fragment .*?="(.*?)" src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/g,
       replacer2,
     );
-
-    while (
-      file.search(
-        /<inline-fragment .*?="(.*?)" src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/,
-      ) > -1
-    ) {
-      file = file.replace(
-        /<inline-fragment .*?="(.*?)" src="~\/(.*?)fragments\/(.*?)"><\/inline-fragment>/,
-        replacer2,
-      );
-    }
 
     // amplify-block + amplify-block-switcher -> CodeBlock + BlockSwitcher
     file = file.split("amplify-block-switcher").join("BlockSwitcher");
@@ -151,6 +145,16 @@ const grab = function() {
     file = file.split("(~").join("(");
     file = file.split(".md)").join(")");
 
+    // misformatted </br>s
+    file = file.split("<br>").join("<br/>");
+    file = file.split("</br>").join("<br/>");
+
+    file = frontMatterToWrite + file;
+    // 2 blank lines
+    while (file.match(/\n\n\n/g)) {
+      file = file.replace(/\n\n\n/g, "\n\n");
+    }
+
     try {
       mkdirp.sync(
         destination
@@ -159,7 +163,7 @@ const grab = function() {
           .join("/"),
       );
     } catch (e) {}
-    fs.writeFileSync(destination, toWrite + file);
+    fs.writeFileSync(destination, file);
   }
 };
 
