ebdb4d825c72a74029fd45eb35c4b7ea2861ef81
Harshita Daddala <daddala@amazon.com>
2021-09-24 12:22:51 -0400

    remove mentions to developer preview for Geo titles

--- src/directory/directory.js
+++ src/directory/directory.js
@@ -401,7 +401,7 @@ const directory = {
         ],
       },
       geo: {
-        title: "Geo (Developer Preview)",
+        title: "Geo",
         items: [
           {
             title: "Getting started",
@@ -1259,7 +1259,7 @@ const directory = {
         ],
       },
       geo: {
-        title: "Geo (Developer Preview)",
+        title: "Geo",
         items: [
           {
             title: "Maps",
