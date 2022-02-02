355811af0e989423ef34d91dbeb19ec2a2d030c0
Tim Nguyen <timngyn@amazon.com>
2022-02-01 17:04:56 -0800

    Remove unused import for prettyDOM

--- src/components/BlockSwitcher/__tests__/index.test.tsx
+++ src/components/BlockSwitcher/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { prettyDOM, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import BlockSwitcher from '..';
 import Block from '../../Block';
