2aadb61e80c42cff7bb9bd3251ca5ecc46da2d85
Jake Burden <jakburde@amazon.com>
2022-03-08 01:33:39 -0500

    Push indicies to algolia

--- tasks/build-algolia-search.mjs
+++ tasks/build-algolia-search.mjs
@@ -2,29 +2,52 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
 import dotenv from 'dotenv';
-// import algoliasearch from 'algoliasearch/lite';
+import algoliasearch from 'algoliasearch/lite.js';
 import directory from '../src/directory/directory.js';
 import generatePathMap from '../generatePathMap.cjs.js';
 import parseImports from 'parse-imports';
+import extractMdxMeta from 'extract-mdx-metadata';
+import { remark } from 'remark';
+import mdx from 'remark-mdx';
+import searchable from 'remark-mdx-searchable';
+
+const platformMap = {
+  js: {
+    label: 'JavaScript'
+  },
+  android: {
+    label: 'Android'
+  },
+  ios: {
+    label: 'iOS'
+  },
+  flutter: {
+    label: 'Flutter'
+  },
+  react: {
+    label: 'React'
+  },
+  'react-native': {
+    label: 'React Native'
+  },
+  angular: {
+    label: 'Angular'
+  },
+  vue: {
+    label: 'Vue'
+  },
+  ionic: {
+    label: 'Ionic'
+  },
+  next: {
+    label: 'Next.js'
+  }
+};
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-try {
-  dotenv.config();
-
-  if (!process.env.PUBLIC_ALGOLIA_APP_ID) {
-    throw new Error('PUBLIC_ALGOLIA_APP_ID is not defined');
-  }
-
-  if (!process.env.ALGOLIA_SEARCH_ADMIN_KEY) {
-    throw new Error('ALGOLIA_SEARCH_ADMIN_KEY is not defined');
-  }
-} catch (error) {
-  console.error(error);
-  process.exit(1);
-}
-
+const articles = [];
 const pathmap = generatePathMap(directory);
 const allFilters = [
   'js',
@@ -53,16 +76,52 @@ Object.keys(pathmap).forEach(async (key) => {
   }
 });
 
-function readPages() {
-  if (!pageValues.length) return;
+try {
+  dotenv.config();
+
+  if (!process.env.PUBLIC_ALGOLIA_APP_ID) {
+    throw new Error('PUBLIC_ALGOLIA_APP_ID is not defined');
+  }
+
+  if (!process.env.ALGOLIA_SEARCH_ADMIN_KEY) {
+    throw new Error('ALGOLIA_SEARCH_ADMIN_KEY is not defined');
+  }
+
+  readPages(async function() {
+    const transformed = transformPostsToSearchObjects(articles);
+    console.log(transformed);
+    const client = algoliasearch(
+      process.env.PUBLIC_ALGOLIA_APP_ID,
+      process.env.ALGOLIA_SEARCH_ADMIN_KEY
+    );
+    const index = client.initIndex('custom_search_staging');
+    const algoliaResponse = await index.saveObjects(transformed);
+
+    console.log(
+      `Successfully added ${
+        algoliaResponse.objectIDs.length
+      } records to Algolia search! Object IDs:\n${algoliaResponse.objectIDs.join(
+        '\n'
+      )}`
+    );
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+function readPages(cb) {
+  if (!pageValues.length) {
+    if (cb) cb();
+    return;
+  }
   const page = pageValues.pop();
   const { filename, platform } = page;
   const doc = fs.readFileSync(filename, 'utf8');
-  tryParseImports(doc, filename, platform);
+  tryParseImports(doc, filename, platform, cb);
 }
-readPages();
 
-async function tryParseImports(source, filename, platform) {
+async function tryParseImports(source, filename, platform, cb) {
   try {
     const imports = [...(await parseImports(source))];
     const lines = source.split('\n');
@@ -87,8 +146,9 @@ async function tryParseImports(source, filename, platform) {
 
     // derive URL path
     filename = filename.split('[');
-    filename[1] = platform + '.mdx';
+    filename[1] = platform;
     filename = filename.join('');
+    filename = filename.split('src/pages')[1];
 
     // add platform specific fragments to source
     fragments[platform].forEach((fragment) => {
@@ -106,15 +166,47 @@ async function tryParseImports(source, filename, platform) {
       return line;
     });
 
-    source = source.join('');
+    source = source.join('\n');
 
-    console.log({ filename, source });
-    readPages();
+    const meta = await extractMdxMeta(source);
+
+    meta.title = platformMap[platform]
+      ? meta.title + ' - ' + platformMap[platform].label
+      : meta.title;
+
+    meta.description = platformMap[platform]
+      ? meta.description + ' - ' + platformMap[platform].label
+      : meta.description;
+
+    const result = remark()
+      .use(mdx)
+      .use(searchable)
+      .processSync(source);
+
+    const searchableText = result.data;
+
+    articles.push({ searchableText, source, meta });
+    readPages(cb);
   } catch (e) {
     console.log('Pages remaing:', pageValues.length);
     console.error(`cannot parse ES imports in '${filename}'`);
-    readPages();
+    readPages(cb);
   }
+}
+
+function transformPostsToSearchObjects(articles) {
+  const transformed = articles.map((article) => {
+    return {
+      objectID: article.meta.title,
+      title: article.meta.title,
+      description: article.meta.description,
+      slug: article.filename,
+      type: 'article',
+      content: article.searchableText
+    };
+  });
+
+  return transformed;
 }
 
 // const CONTENT_PATH = path.join(process.cwd(), 'content/articles');
