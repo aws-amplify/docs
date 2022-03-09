14fa9991fb990faa12897919fc90d60865b162a4
Tim Nguyen <timngyn@amazon.com>
2022-03-09 15:13:36 -0800

    Remove unused routes

--- amplify/backend/function/submissionsLambda/lib/app.ts
+++ amplify/backend/function/submissionsLambda/lib/app.ts
@@ -6,11 +6,6 @@ import {postCallback} from './controllers/SubmissionsController'
 AWS.config.update({ region: process.env.TABLE_REGION });
 
 const path = '/submissions';
-const partitionKeyName: string = 'page_path';
-const sortKeyName: string = 'created';
-const hasSortKey = sortKeyName !== '';
-const hashKeyPath = '/:' + partitionKeyName;
-const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
 
 // declare a new express app
 const app = express();
@@ -23,58 +18,13 @@ app.use(function(req, res, next) {
   next();
 });
 
-/********************************
- * HTTP Get method for list objects *
- ********************************/
-
-app.get(path, function(req, res) {
-  // Method not allowed.
-  res.statusCode = 405;
-  res.json({ error: 'Method is not allowed.' });
-});
-
-/*****************************************
- * HTTP Get method for get single object *
- *****************************************/
-
-app.get(path + hashKeyPath + sortKeyPath, function(req, res) {
-  // Method not allowed.
-  res.statusCode = 405;
-  res.json({ error: 'Method is not allowed.' });
-});
-
-/************************************
- * HTTP put method for insert object *
- *************************************/
-
-app.put(path, function(req, res) {
-  // Method not allowed.
-  res.statusCode = 405;
-  res.json({ error: 'Method is not allowed.' });
-});
-
 /************************************
  * HTTP post method for insert object *
  *************************************/
 
 app.post(path, postCallback);
 
-/**************************************
- * HTTP remove method to delete object *
- ***************************************/
-
-app.delete(path + hashKeyPath + sortKeyPath, function(req, res) {
-  // Method not allowed.
-  res.statusCode = 405;
-  res.json({ error: 'Method is not allowed.' });
-});
-
-app.listen(3000, function() {
-  console.log('App started');
-});
-
 // Export the app object. When executing the application local this does nothing. However,
 // to port it to AWS Lambda we will create a wrapper around that will load the app from
 // this file
-// module.exports = app;
 export { app };
--- amplify/backend/function/submissionsLambda/src/app.js
+++ amplify/backend/function/submissionsLambda/src/app.js
@@ -8,11 +8,6 @@ const express_1 = (0, tslib_1.__importDefault)(require("express"));
 const SubmissionsController_1 = require("./controllers/SubmissionsController");
 aws_sdk_1.default.config.update({ region: process.env.TABLE_REGION });
 const path = '/submissions';
-const partitionKeyName = 'page_path';
-const sortKeyName = 'created';
-const hasSortKey = sortKeyName !== '';
-const hashKeyPath = '/:' + partitionKeyName;
-const sortKeyPath = hasSortKey ? '/:' + sortKeyName : '';
 const app = (0, express_1.default)();
 exports.app = app;
 app.use(body_parser_1.default.json());
@@ -21,23 +16,4 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Headers', '*');
     next();
 });
-app.get(path, function (req, res) {
-    res.statusCode = 405;
-    res.json({ error: 'Method is not allowed.' });
-});
-app.get(path + hashKeyPath + sortKeyPath, function (req, res) {
-    res.statusCode = 405;
-    res.json({ error: 'Method is not allowed.' });
-});
-app.put(path, function (req, res) {
-    res.statusCode = 405;
-    res.json({ error: 'Method is not allowed.' });
-});
 app.post(path, SubmissionsController_1.postCallback);
-app.delete(path + hashKeyPath + sortKeyPath, function (req, res) {
-    res.statusCode = 405;
-    res.json({ error: 'Method is not allowed.' });
-});
-app.listen(3000, function () {
-    console.log('App started');
-});
