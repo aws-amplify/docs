b48b318deeb22938ee12a3ddcf2e172bb0fa49f9
Jake Burden <jakburde@amazon.com>
2022-04-13 15:49:37 -0400

    Increase hits per page

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -33,7 +33,7 @@ function App() {
                     indexName: 'custom_search_staging',
                     query,
                     params: {
-                      hitsPerPage: 6
+                      hitsPerPage: 10
                     }
                   }
                 ]
