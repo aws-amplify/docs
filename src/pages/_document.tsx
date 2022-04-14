b252f2bc2442d800cac924bc628824bba6d2d17b
Tim Nguyen <timngyn@amazon.com>
2022-04-14 14:58:55 -0700

    Update frame-src

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,7 +19,7 @@ const getCspContent = (context) => {
       prefetch-src 'self';
       style-src 'self' 'unsafe-inline';
       font-src 'self' data:;
-      frame-src 'self' https://www.youtube-nocookie.com;
+      frame-src 'self' aws.demdex.net https://www.youtube-nocookie.com;
       connect-src 'self' *.shortbread.aws.dev amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net
       https://*.algolia.net https://*.algolianet.com https://ym8kdcu1hl.execute-api.us-west-2.amazonaws.com/feedback/submissions;
       img-src 'self' data: cm.everesttech.net amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net;
@@ -33,7 +33,7 @@ const getCspContent = (context) => {
     prefetch-src 'self';
     style-src 'self' 'unsafe-inline';
     font-src 'self';
-    frame-src 'self' https://www.youtube-nocookie.com;
+    frame-src 'self' aws.demdex.net https://www.youtube-nocookie.com;
     connect-src 'self' *.shortbread.aws.dev amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net
     https://*.algolia.net https://*.algolianet.com https://ym8kdcu1hl.execute-api.us-west-2.amazonaws.com/feedback/submissions;
     img-src 'self' cm.everesttech.net amazonwebservices.d2.sc.omtrdc.net dpm.demdex.net;
