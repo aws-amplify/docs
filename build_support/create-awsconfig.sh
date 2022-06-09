5164593744632065f2bccaa3a567f30b2538679d
Tim Nguyen <timngyn@gmail.com>
2022-06-09 08:23:45 -0700

    Add 'export default {}' line to aws-exports script

--- build_support/create-awsconfig.sh
+++ build_support/create-awsconfig.sh
@@ -1,5 +1,6 @@
 FILE=aws-exports.js
 if [ ! -f "./src/$FILE" ]; then
-  echo "$FILE does not exist in ./src/, creating blank aws-exports.js"
+  echo "$FILE does not exist in ./src/, creating aws-exports.js"
   touch "./src/$FILE"
+  echo "export default {}" > "./src/$FILE"
 fi
