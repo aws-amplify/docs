82a7d590149ff7069c6900454538a11796ab5886
josefaidt <josef.aidt@gmail.com>
2022-06-24 14:35:37 -0500

    fix: setup script for Windows

---
+++ build_support/create-awsconfig.mjs
@@ -0,0 +1,10 @@
+import { access, writeFile } from 'node:fs/promises';
+
+const awsExports = new URL('../src/aws-exports.js', import.meta.url);
+
+try {
+  await access(awsExports);
+} catch (error) {
+  // file does not exist, create it
+  await writeFile(awsExports, 'export default {}');
+}
