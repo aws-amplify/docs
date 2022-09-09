616fcb21f77fb0fc83b879d9814cd7e6507c9df7
Tim Nguyen <timngyn@gmail.com>
2022-09-08 18:38:06 -0700

    Update scrolling shadow so it hides sooner

--- src/components/SecondaryNav/styles.tsx
+++ src/components/SecondaryNav/styles.tsx
@@ -63,7 +63,7 @@ export const SecondaryNavStyle = styled.div`
       linear-gradient(to left, rgba(0, 0, 0, 0.25), rgba(255, 255, 255, 0));
     background-position: left center, right center, left center, right center;
     background-repeat: no-repeat;
-    background-size: 20px 100%, 20px 100%, 20px 100%, 20px 100%;
+    background-size: 20px 100%, 20px 100%, 10px 100%, 10px 100%;
     background-attachment: local, local, scroll, scroll;
   }
 `;
