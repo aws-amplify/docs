bc6805ef4429f0875d0c1c15735a26c8260e1e18
Jacob Logan <lognjc@amazon.com>
2022-10-20 11:27:44 -0700

    add FilterSelect tests

--- src/components/Menu/FilterSelect/__tests__/FilterSelect.test.tsx
+++ src/components/Menu/FilterSelect/__tests__/FilterSelect.test.tsx
@@ -1,39 +1,87 @@
 import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import FilterSelect from '../index';
+import {
+  filterOptionsByName,
+  filterMetadataByOption
+} from '../../../../utils/filter-data';
 
 describe('FilterSelect', () => {
-  const url = '/start/q/integration/js';
-  const filterKind = 'integration';
-  const filterKey = 'js';
-  const filters = [
-    'swift',
-    'android',
-    'flutter',
-    'js',
-    'next',
-    'react',
-    'react-native',
-    'vue'
-  ];
+  describe('Unknown filterKind', () => {
+    const url = '/start';
+    const filterKind = 'unknown';
+    const filterKey = 'all';
 
-  function capitalize(input) {
-    return input && input[0].toUpperCase() + input.slice(1);
-  }
+    it('should render the passed in filters', () => {
+      const filters = ['android', 'ios'];
 
-  it('should render a link for each filter value', async () => {
-    render(
-      <FilterSelect
-        filters={filters}
-        filterKey={filterKey}
-        filterKind={filterKind}
-        url={url}
-      />
-    );
+      render(
+        <FilterSelect
+          filters={filters}
+          filterKey={filterKey}
+          filterKind={filterKind}
+          url={url}
+        />
+      );
 
-    filters.forEach((filterValue) => {
-      const linkElement = screen.getByText(capitalize(filterValue));
-      expect(linkElement).toBeInTheDocument();
+      filters.forEach((filterValue) => {
+        const linkElement = screen.getByText(
+          filterMetadataByOption[filterValue]?.label
+        );
+        expect(linkElement).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('Platform filters', () => {
+    const allIntegrationFilters = filterOptionsByName['platform'];
+    const url = '/start';
+    const filterKind = 'platform';
+    const filterKey = 'all';
+    const filters = [];
+
+    it('should render a link for each filter value', async () => {
+      render(
+        <FilterSelect
+          filters={filters}
+          filterKey={filterKey}
+          filterKind={filterKind}
+          url={url}
+        />
+      );
+
+      allIntegrationFilters.forEach((filterValue) => {
+        const linkElement = screen.getByText(
+          filterMetadataByOption[filterValue]?.label
+        );
+        expect(linkElement).toBeInTheDocument();
+      });
+    });
+  });
+
+  describe('Framework filters', () => {
+    const allIntegrationFilters = filterOptionsByName['framework'];
+    const url = '/start';
+    const filterKind = 'framework';
+    const filterKey = 'all';
+    const filters = [];
+
+    it('should render a link for each filter value', async () => {
+      render(
+        <FilterSelect
+          filters={filters}
+          filterKey={filterKey}
+          filterKind={filterKind}
+          url={url}
+        />
+      );
+
+      allIntegrationFilters.forEach((filterValue) => {
+        const linkElement = screen.getByText(
+          filterMetadataByOption[filterValue]?.label
+        );
+        expect(linkElement).toBeInTheDocument();
+      });
     });
   });
 });
