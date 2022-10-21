b01c39dbe93f00b7b4a625f4e9559482d136d0f1
Mónica Fidalgo <41929489+MonicaFidalgo@users.noreply.github.com>
2022-10-22 00:51:44 +0100

    replace amplify framework by amplify (#4709)

--- tasks/build-algolia-search.mjs
+++ tasks/build-algolia-search.mjs
@@ -266,7 +266,7 @@ function transformPostsToSearchObjects(articles) {
       const skip =
         subcategory === 'undefined' ||
         subcategory === undefined ||
-        text.startsWith('Learn more about how to use Amplify Framework') ||
+        text.startsWith('Learn more about how to use Amplify') ||
         text.length === 0;
       if (!skip) {
         const obj = {
