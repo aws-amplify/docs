90283e9990a1ce34939b6664e4f9198ea975d955
Rene Brandel <renbran@amazon.com>
2022-11-03 08:19:14 -0700

    Adding media-src statement for self.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,6 +38,7 @@ const getCspContent = (context) => {
     connect-src 'self' *.shortbread.aws.dev https://amazonwebservices.d2.sc.omtrdc.net https://aws.demdex.net https://dpm.demdex.net https://cm.everesttech.net https://a0.awsstatic.com/ https://d2c.aws.amazon.com https://vs.aws.amazon.com
     https://*.algolia.net https://*.algolianet.com *.amazonaws.com https://docs-backend.amplify.aws;
     img-src 'self' https://img.shields.io cm.everesttech.net https://amazonwebservices.d2.sc.omtrdc.net https://aws.demdex.net https://dpm.demdex.net https://cm.everesttech.net https://a0.awsstatic.com/;
+    media-src 'self';
     script-src 'self' ${cspInlineScriptHash} https://a0.awsstatic.com/;
   `;
 };
