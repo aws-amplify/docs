84b27016229c623f656ed7480810ffd964010a67
Abdallah Shaban <abdallah.w.shaban@gmail.com>
2022-11-07 16:44:05 -0500

    cleaned up legacy UI artifacts

--- src/components/Menu/VersionSwitcher/index.tsx
+++ src/components/Menu/VersionSwitcher/index.tsx
@@ -2,30 +2,6 @@ import Link from "next/link";
 import {ActiveSwitchStyle, SwitchStyle} from "./styles";
 import directory from "../../../directory/directory";
 
-const ui = directory["ui"].items;
-const uiLegacy = directory["ui-legacy"].items;
-const uiLegacyPaths = [];
-const uiPaths = [];
-const itemsAndPaths: [object, string[]][] = [
-  [ui, uiPaths],
-  [uiLegacy, uiLegacyPaths],
-];
-for (const [dirItems, paths] of itemsAndPaths) {
-  for (const [_, value] of Object.entries(dirItems)) {
-    const {items} = value;
-    items.forEach((item) => {
-      const {route, filters} = item;
-      filters.forEach((filter) => {
-        const path = route + "/q/framework/" + filter + "/";
-        paths.push(path);
-      });
-      paths.push(route);
-    });
-  }
-}
-uiLegacyPaths.push("/ui-legacy");
-uiPaths.push("/ui");
-
 const Option = function({href, title, isActive}) {
   const SwitchStyle = isActive ? ActiveSwitchStyle : "a";
   return (
