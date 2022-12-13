e33b8b60e7bde7930cd98b17dc70e3168de11943
josefaidt <josef.aidt@gmail.com>
2022-12-13 15:53:20 -0800

    fix: manual eslint fix, disable eslint errors for test props

--- src/components/Menu/MenuCloseButton/__tests__/MenuCloseButton.test.tsx
+++ src/components/Menu/MenuCloseButton/__tests__/MenuCloseButton.test.tsx
@@ -3,9 +3,11 @@ import { render, screen } from '@testing-library/react';
 import MenuCloseButton from '../index';
 
 describe('MenuCloseButton', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   const closeMenu = () => {};
 
   it('should render the MenuCloseButton', async () => {
+    // eslint-disable-next-line react/jsx-no-bind
     render(<MenuCloseButton closeMenu={closeMenu} />);
 
     const closeButton = await screen.findByRole('button', { hidden: true });
