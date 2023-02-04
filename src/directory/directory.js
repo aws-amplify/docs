57e136c9d7df536dd5e10abe48f402a947189853
vimo <vidyesh@yahoo.com>
2023-02-04 18:46:35 +0530

    Resolves: #5081

--- src/directory/directory.js
+++ src/directory/directory.js
@@ -797,7 +797,7 @@ const directory = {
             route: '/lib/utilities/serviceworker',
             filters: ['js']
           },
-          { title: 'Cache', route: '/lib/utilities/cache', filters: ['js'] },
+          { title: 'Cache', route: '/lib/utilities/cache', filters: ['js', 'react-native'] },
           {
             title: 'Hub',
             route: '/lib/utilities/hub',
