54b54e15ca4c49535d370d3a054090365c55517e
wpeck-amplify <107895670+wpeck-amplify@users.noreply.github.com>
2023-02-10 13:09:07 -0800

    CMS updates

--- src/directory/directory.js
+++ src/directory/directory.js
@@ -2378,7 +2378,7 @@ const directory = {
             route: '/console/data/relationships'
           },
           {
-            title: 'Content management',
+            title: 'Data management',
             route: '/console/data/content-management'
           }
         ]
