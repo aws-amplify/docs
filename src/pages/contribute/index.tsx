cd51d61980c0e202d5f99b95429c938fd4cc0263
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2023-03-09 14:55:22 -0800

    Add custom env var to conditionally fetch data for contribute page (#5228)

--- src/pages/contribute/index.tsx
+++ src/pages/contribute/index.tsx
@@ -20,37 +20,42 @@ import { Endpoints } from '@octokit/types';
 type listRepoIssuesResponse = Endpoints['GET /repos/{owner}/{repo}/issues']['response'];
 
 export async function getStaticProps() {
-  const octokit = new Octokit({});
+  if (process.env.PROD_ENV === 'production') {
+    const octokit = new Octokit({});
 
-  const {
-    data: JsIssues
-  }: {
-    data: listRepoIssuesResponse['data'];
-  } = await octokit.rest.issues.listForRepo({
-    owner: 'aws-amplify',
-    repo: 'amplify-js',
-    state: 'open',
-    labels: 'good first issue',
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    per_page: 6
-  });
+    const {
+      data: JsIssues
+    }: {
+      data: listRepoIssuesResponse['data'];
+    } = await octokit.rest.issues.listForRepo({
+      owner: 'aws-amplify',
+      repo: 'amplify-js',
+      state: 'open',
+      labels: 'good first issue',
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      per_page: 6
+    });
 
-  const {
-    data: CLIissues
-  }: {
-    data: listRepoIssuesResponse['data'];
-  } = await octokit.rest.issues.listForRepo({
-    owner: 'aws-amplify',
-    repo: 'amplify-cli',
-    state: 'open',
-    labels: 'good first issue',
-    // eslint-disable-next-line @typescript-eslint/camelcase
-    per_page: 6
-  });
+    const {
+      data: CLIissues
+    }: {
+      data: listRepoIssuesResponse['data'];
+    } = await octokit.rest.issues.listForRepo({
+      owner: 'aws-amplify',
+      repo: 'amplify-cli',
+      state: 'open',
+      labels: 'good first issue',
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      per_page: 6
+    });
+
+    return {
+      props: { JsIssues, CLIissues }
+    };
+  }
 
   return {
-    props: { JsIssues, CLIissues },
-    revalidate: 60 * 60
+    props: {}
   };
 }
 
