c48642de4a98f8577e421c51bd495d2579aad701
Katie Goines <katiegoi@amazon.com>
2023-03-30 14:52:31 -0700

    fix nav hydration errors

--- src/components/SecondaryNav/index.tsx
+++ src/components/SecondaryNav/index.tsx
@@ -18,96 +18,105 @@ import { Container } from '../Container';
 import { parseLocalStorage } from '../../utils/parseLocalStorage';
 
 import SearchBar from '../SearchBar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function SecondaryNav() {
   const router = useRouter();
   const path = router.asPath;
   const filterKeys = parseLocalStorage('filterKeys', {});
+  const [domLoaded, setDomLoaded] = useState(false);
+
+  useEffect(() => {
+    setDomLoaded(true);
+  }, []);
 
   return (
     <HostStyle>
-      <Container>
-        <SecondaryNavStyle id="secondary-nav">
-          <div className="secondary-nav-links">
-            {[
-              {
-                label: 'Getting Started',
-                url: '/start'
-              },
-              {
-                label: 'Libraries',
-                url: '/lib',
-                additionalActiveChildRoots: ['/lib', '/sdk']
-              },
-              {
-                label: 'CLI',
-                url: '/cli'
-              },
-              {
-                label: 'Studio',
-                url: '/console'
-              },
-              {
-                label: 'Hosting',
-                url: HOSTING_REFERENCE,
-                external: true
-              },
-              {
-                label: 'Guides',
-                url: '/guides'
-              },
-              ...('platform' in filterKeys &&
-              (filterKeys as { platform: string }).platform
-                ? [
-                    {
-                      label: 'API Reference',
-                      url: (() => {
-                        switch ((filterKeys as { platform: string }).platform) {
-                          case 'ios': {
-                            return IOS_REFERENCE;
+      {domLoaded && (
+        <Container>
+          <SecondaryNavStyle id="secondary-nav">
+            <div className="secondary-nav-links">
+              {[
+                {
+                  label: 'Getting Started',
+                  url: '/start'
+                },
+                {
+                  label: 'Libraries',
+                  url: '/lib',
+                  additionalActiveChildRoots: ['/lib', '/sdk']
+                },
+                {
+                  label: 'CLI',
+                  url: '/cli'
+                },
+                {
+                  label: 'Studio',
+                  url: '/console'
+                },
+                {
+                  label: 'Hosting',
+                  url: HOSTING_REFERENCE,
+                  external: true
+                },
+                {
+                  label: 'Guides',
+                  url: '/guides'
+                },
+                ...('platform' in filterKeys &&
+                (filterKeys as { platform: string }).platform
+                  ? [
+                      {
+                        label: 'API Reference',
+                        url: (() => {
+                          switch (
+                            (filterKeys as { platform: string }).platform
+                          ) {
+                            case 'ios': {
+                              return IOS_REFERENCE;
+                            }
+                            case 'android': {
+                              return ANDROID_REFERENCE;
+                            }
+                            default: {
+                              return JS_REFERENCE;
+                            }
                           }
-                          case 'android': {
-                            return ANDROID_REFERENCE;
-                          }
-                          default: {
-                            return JS_REFERENCE;
-                          }
-                        }
-                      })(),
-                      external: true
-                    }
-                  ]
-                : [])
-            ].map(({ url, label, external, additionalActiveChildRoots }) => {
-              const matchingRoots =
-                additionalActiveChildRoots === undefined
-                  ? [url]
-                  : [url, ...additionalActiveChildRoots];
-              const active = matchingRoots.some((root) => {
-                return path.startsWith(root);
-              });
-              const LinkStyle = active ? LinkActiveStyle : LinkInactiveStyle;
-              if (external) {
-                return (
-                  <ExternalLink href={url} key={label} graphic="black">
-                    <span>{label}</span>
-                  </ExternalLink>
-                );
-              } else {
-                return (
-                  <InternalLink href={url} key={label}>
-                    <LinkStyle href={url}>{label}</LinkStyle>
-                  </InternalLink>
-                );
-              }
-            })}
-          </div>
-          <SearchBarContainer>
-            <SearchBar />
-          </SearchBarContainer>
-        </SecondaryNavStyle>
-      </Container>
+                        })(),
+                        external: true
+                      }
+                    ]
+                  : [])
+              ].map(({ url, label, external, additionalActiveChildRoots }) => {
+                const matchingRoots =
+                  additionalActiveChildRoots === undefined
+                    ? [url]
+                    : [url, ...additionalActiveChildRoots];
+                const active = matchingRoots.some((root) => {
+                  return path.startsWith(root);
+                });
+                const LinkStyle = active ? LinkActiveStyle : LinkInactiveStyle;
+                if (external) {
+                  return (
+                    <ExternalLink href={url} key={label} graphic="black">
+                      <span>{label}</span>
+                    </ExternalLink>
+                  );
+                } else {
+                  return (
+                    <InternalLink href={url} key={label}>
+                      <LinkStyle href={url}>{label}</LinkStyle>
+                    </InternalLink>
+                  );
+                }
+              })}
+            </div>
+            <SearchBarContainer>
+              <SearchBar />
+            </SearchBarContainer>
+          </SecondaryNavStyle>
+        </Container>
+      )}
     </HostStyle>
   );
 }
