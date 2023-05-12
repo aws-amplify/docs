df8f17d0e509928855866a24bc8205753913180d
Erica Eaton <67125657+eeatonaws@users.noreply.github.com>
2023-05-12 11:24:00 -0700

    Update Amplify Android version

--- src/constants/versions.ts
+++ src/constants/versions.ts
@@ -1,5 +1,5 @@
 module.exports = {
-  ANDROID_VERSION: "2.8.3",
+  ANDROID_VERSION: "2.8.4",
   ANDROID_DEVPREVIEW: "1.36.5-dev-preview.0",
   ANDROID_V1_VERSION: "1.38.5",
   ANDROID_V1_GEO_VERSION: "1.0.1",
