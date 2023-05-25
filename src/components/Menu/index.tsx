aws-amplify/docs

commit e67ca1243cfaa547641f1acedd2dfd4d487550fd

redesigned feedback component and mobile secondary nav (#5334)
* feedback-component

* desktop

* mobile styling

* style changes

* fixed transpiler issue

* animation updates

* adjustments from alee and rlee

* window undefined errors

* moved component into page from table of contents

* fix hight discrepancy

* mobile menu update

* animation for mobile menus

* Update src/components/FeedbackSticky/index.tsx

Co-authored-by: Tim Nguyen <54393192+timngyn@users.noreply.github.com>

* animation speed adjustments

* refactor

* PR comment resolutions

* wording changes
;

* fix mobile menu scroll

* passing refs

* ref passing trials

* ..

* fix hook issues and finish refactor

* cleanup

* fix tests

* remove errant }

* alee & jacob adjustments

* make expand longer

* collapse duration

* animation adjustment

* change collapse speed

* change animation ease

* fix toc on studio mobile

* move mobile menu icons close to divider

* animation speed adjustment"

---------

Co-authored-by: Tim Nguyen <54393192+timngyn@users.noreply.github.com>

src/components/Menu/index.tsx
@@ -9,6 +9,7 @@ import React, {
   forwardRef,
   useEffect,
   useImperativeHandle,
+  useRef,
   useState
 } from 'react';
 import MenuOpenButton from './MenuOpenButton';
@@ -19,6 +20,7 @@ import RepoActions from './RepoActions';
 import FilterSelect from './FilterSelect';
 import { VersionSwitcher, LibVersionSwitcher } from './VersionSwitcher';
 import { useLastUpdatedDatesContext } from '../LastUpdatedProvider';
+import { Button } from '@cloudscape-design/components';
 
 type MenuProps = {
   filters: string[];
@@ -27,24 +29,42 @@ type MenuProps = {
   url: string;
   directoryPath: string;
   setMenuIsOpen?: any;
+  buttonsRef: any;
 };
 
 function Menu(props: MenuProps, ref) {
   const [isOpen, setIsOpen] = useState(true);
   const { state } = useLastUpdatedDatesContext();
+  const menuRef = useRef<HTMLElement>(null);
+  const [onDesktop, setOnDesktop] = useState(false);
 
   useEffect(() => {
     const MQTabletJS = MQTablet.substring(6);
     // If the media query matches, then the user is on desktop and should see the menu by default
-    setIsOpen(
-      typeof window !== 'undefined' && window.matchMedia(MQTabletJS).matches
-    );
+    const onDesktop =
+      typeof window !== 'undefined' && window.matchMedia(MQTabletJS).matches;
+    setIsOpen(onDesktop);
+    setOnDesktop(onDesktop);
   }, []);
 
   useImperativeHandle(ref, () => ({
+    ref: menuRef,
     closeMenu: () => closeMenu(),
     openMenu: () => openMenu()
   }));
+
+  const hideMenu = () => {
+    if (!onDesktop) {
+      const buttons = props.buttonsRef.current;
+      const menu = menuRef.current;
+      if (menu) {
+        menu.classList.add('slideOut'), menu.classList.remove('slideIn');
+      }
+      if (buttons) {
+        buttons.classList.add('slideIn'), buttons.classList.remove('slideOut');
+      }
+    }
+  };
 
   const closeMenu = () => {
     setIsOpen(false);
@@ -57,13 +77,14 @@ function Menu(props: MenuProps, ref) {
   const openMenu = () => {
     setIsOpen(true);
 
-    if (props.setMenuIsOpen) {
+    if (props.setMenuIsOpen && onDesktop) {
       props.setMenuIsOpen(true);
     }
   };
 
   let showVersionSwitcher = false;
   let showLibVersionSwitcher = false;
+
   if (
     (props.url.startsWith('/ui') || props.url.startsWith('/ui-legacy')) &&
     props.filterKey !== 'react-native' &&
@@ -99,11 +120,17 @@ function Menu(props: MenuProps, ref) {
 
   if (isOpen) {
     return (
-      <MenuStyle>
+      <MenuStyle ref={menuRef}>
         <div>
           <div>
             <MenuHeaderStyle>
-              <MenuCloseButton closeMenu={closeMenu} />
+              {!onDesktop && (
+                <div className="mobileHeader">
+                  <h2>Table of Contents</h2>
+                  <Button variant="icon" iconName="close" onClick={hideMenu} />
+                </div>
+              )}
+              {onDesktop && <MenuCloseButton closeMenu={closeMenu} />}
               {typeof props.filterKey !== 'undefined' && (
                 <FilterSelect
                   filters={props.filters}

src/components/Menu/styles.tsx
@@ -1,15 +1,86 @@
 import styled from '@emotion/styled';
-import { MQMobile, MQTablet, MQLaptop, MQDesktop } from '../media';
+import { keyframes } from '@emotion/react';
+import { MQTablet, MQLaptop, MQDesktop } from '../media';
+
+const slideIn = keyframes`
+  from {
+    left: -100vw;
+  }
+  to {
+    left: 0;
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    left: 0;
+  }
+  to {
+    left: -100vw;
+  }
+`;
 
 export const MenuHeaderStyle = styled.div`
   display: flex;
-  flex-direction: row;
-  margin: 1.75rem 2.75rem 0 2.5rem;
+  flex-direction: column;
+  margin: 0 2.5rem;
+  div.mobileHeader {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 64px;
+    border-bottom: 2px solid #D1D5DB;
+    margin-bottom: 35px;
+
+    h2 {
+      font-family: 'Amazon Ember';
+      font-style: normal;
+      font-weight: 800;
+      font-size: 16px;
+      line-height: 20px;
+      display: flex;
+      align-items: center;
+      letter-spacing: -0.0008em;
+      margin: 0;
+    }
+  }
+
+  ${MQDesktop}, ${MQTablet}, ${MQLaptop} {
+    flex-direction: row;
+    margin: 1.75rem 2.75rem 0 2.5rem;
+  }
 `;
 
 export const MenuStyle = styled.div`
   border-right: 0.0625rem solid var(--border-color);
-  min-width: 20rem;
+  min-width: 100vw;
+  height: 100%;
+  z-index: 11;
+  position: fixed;
+  left: -100vw;
+  // transition: left 0.4s;
+  top: calc(var(--docs-dev-center-nav));
+  background: #ffffff;
+  overflow: scroll;
+
+  &.slideOut {
+    animation: ${slideOut} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+  &.slideIn {
+    animation: ${slideIn} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
+  ${MQDesktop}, ${MQTablet}, ${MQLaptop} {
+    top: calc(3rem + var(--docs-dev-center-nav));
+    position: sticky;
+    min-width: 20rem;
+    z-index: 0;
+    left: unset;
+    overflow: unset;
+  }}
 
   > div {
     position: relative;

src/components/MobileMenuIcons/styles.tsx
@@ -1,0 +1,73 @@
+import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
+import { MQTablet } from '../media';
+
+const slideIn = keyframes`
+  from {
+    transform: translateX(calc(100vw - 1rem - 1rem));
+  }
+  to {
+    transform: translateX(0);
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    transform: translateX(1rem);
+  }
+  to {
+    transform: translateX(calc(100vw + 1rem));
+  }
+`;
+
+export const Toggle = styled.div`
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  position: fixed;
+  right: 1rem;
+  margin-top: 14px;
+  width: 52px;
+  height: 116px;
+  background: #ffffff;
+  border: 2px solid #d1d5db;
+  box-shadow: 0px 4px 20px rgba(0, 7, 22, 0.12);
+  border-radius: 48px;
+  transition: right 0.4s;
+  cursor: auto;
+
+  &.slideOut {
+    animation: ${slideOut} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
+  &.slideIn {
+    animation: ${slideIn} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
+  button {
+    padding: 0 !important;
+
+    span {
+      cursor: unset;
+    }
+  }
+
+  ${MQTablet} {
+    display: none;
+  }
+`;
+
+export const Divider = styled.div`
+  height: 2px;
+  width: 20px;
+  left: 32px;
+  background: #e9ebed;
+  align-self: center;
+  margin: 10px;
+`;

src/components/Page/metaContent.tsx
@@ -1,0 +1,95 @@
+import CodeBlockProvider from '../CodeBlockProvider/index';
+import Menu from '../Menu/index';
+import TableOfContents from '../TableOfContents/index';
+import NextPrevious from '../NextPrevious/index';
+import { ContentStyle, ChapterTitleStyle } from './styles';
+import MobileMenuIcons from '../MobileMenuIcons';
+import { useRef, useState } from 'react';
+import { MQTablet } from '../media';
+import FeedbackSticky from '../FeedbackSticky';
+import LastUpdatedDatesProvider from '../LastUpdatedProvider';
+
+export default function MetaContent({
+  title,
+  chapterTitle,
+  headers,
+  children,
+  filters,
+  filterKey,
+  filterKind,
+  url,
+  directoryPath,
+  parentPageLastUpdatedDate,
+  footerRef
+}: {
+  title: string;
+  chapterTitle: string;
+  headers: any;
+  children: any;
+  filters: any;
+  filterKey: any;
+  filterKind: any;
+  url: any;
+  directoryPath: any;
+  parentPageLastUpdatedDate: string;
+  footerRef: any;
+}) {
+  const menuRef = useRef(null);
+  const buttonsRef = useRef(null);
+  const contentsRef = useRef(null);
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
+  // Slice off the "@media " string at the start for use in JS instead of CSS
+  const MQTabletJS = MQTablet.substring(6);
+  // If the media query matches, then the user is on desktop and should not see the mobile toggle
+  const onDesktop =
+    typeof window === 'undefined'
+      ? false
+      : window.matchMedia(MQTabletJS).matches;
+
+  return (
+    <>
+      <LastUpdatedDatesProvider
+        parentPageLastUpdatedDate={parentPageLastUpdatedDate}
+      >
+        <Menu
+          filters={filters}
+          filterKey={filterKey}
+          filterKind={filterKind}
+          url={url}
+          directoryPath={directoryPath}
+          ref={menuRef}
+          setMenuIsOpen={setMenuIsOpen}
+          buttonsRef={buttonsRef}
+        ></Menu>
+        <ContentStyle menuIsOpen={menuIsOpen}>
+          <div>
+            <ChapterTitleStyle>{chapterTitle}</ChapterTitleStyle>
+            <div>
+              <h1>{title}</h1>
+            </div>
+            <CodeBlockProvider>
+              {children}
+              <NextPrevious url={url} filterKey={filterKey} />
+            </CodeBlockProvider>
+          </div>
+          <FeedbackSticky footerRef={footerRef} />
+        </ContentStyle>
+        <TableOfContents
+          title={title}
+          ref={contentsRef}
+          buttonsRef={buttonsRef}
+        >
+          {headers}
+        </TableOfContents>
+        {!onDesktop && url != '/start' && (
+          <MobileMenuIcons
+            ref={buttonsRef}
+            contentsRef={contentsRef}
+            menuRef={menuRef}
+          />
+        )}
+      </LastUpdatedDatesProvider>
+    </>
+  );
+}

src/components/TableOfContents/index.tsx
@@ -7,9 +7,10 @@ import {
   H3AnchorStyle,
   HeaderStyle
 } from './styles';
-import { useEffect } from 'react';
+import { forwardRef, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import Feedback from '../Feedback';
+import { MQTablet } from '../media';
+import { Button } from '@cloudscape-design/components';
 
 const stickyHeaderHeight = 124;
 function scroll(hash) {
@@ -20,8 +21,14 @@ function scroll(hash) {
   }
 }
 
-export default function TableOfContents({ children, title }) {
+function TableOfContents({ children, title, buttonsRef }, ref) {
   const router = useRouter();
+  const MQTabletJS = MQTablet.substring(6);
+  const onDesktop =
+    typeof window === 'undefined'
+      ? false
+      : window.matchMedia(MQTabletJS).matches;
+
   if (children.length === 0) {
     return <></>;
   }
@@ -115,16 +122,35 @@ export default function TableOfContents({ children, title }) {
     };
   }, []);
 
+  const closeToc = () => {
+    if (typeof document !== 'undefined' && !onDesktop) {
+      const toc = ref.current;
+      const buttons = buttonsRef.current;
+      if (toc) {
+        toc.classList.add('slideOut'), toc.classList.remove('slideIn');
+      }
+      if (buttons) {
+        buttons.classList.add('slideIn'), buttons.classList.remove('slideOut');
+      }
+    }
+  };
+
   return (
-    <TOCStyle id="toc">
+    <TOCStyle id="toc" ref={ref}>
       <TOCInnerStyle>
+        {!onDesktop && (
+          <div className="mobileHeader">
+            <h2>On this Page</h2>
+            <Button variant="icon" iconName="close" onClick={closeToc} />
+          </div>
+        )}
         <HeaderStyle>
           <h4>{title}</h4>
         </HeaderStyle>
         {children.map(([name, id, level], index) => {
           const slugged = `#${id}`;
           const anchor = (
-            <a href={slugged}>
+            <a href={slugged} onClick={closeToc}>
               <div>{name}</div>
             </a>
           );
@@ -132,8 +158,9 @@ export default function TableOfContents({ children, title }) {
             return <H2AnchorStyle key={index}>{anchor}</H2AnchorStyle>;
           else return <H3AnchorStyle key={index}>{anchor}</H3AnchorStyle>;
         })}
-        <Feedback />
       </TOCInnerStyle>
     </TOCStyle>
   );
 }
+
+export default forwardRef(TableOfContents);

src/components/TableOfContents/styles.tsx
@@ -1,5 +1,28 @@
 import styled from '@emotion/styled';
+import { keyframes } from '@emotion/react';
 import { MQTablet, MQDesktop } from '../media';
+
+const slideIn = keyframes`
+  from {
+    left: -100vw;
+  }
+  to {
+    left: 0;
+  }
+`;
+
+const slideOut = keyframes`
+  from {
+    left: 0;
+  }
+  to {
+    left: -100vw;
+  }
+`;
+
+type ToggleProps = {
+  inView?: boolean;
+};
 
 export const TOCInnerStyle = styled.div`
   overflow: auto;
@@ -7,15 +30,54 @@ export const TOCInnerStyle = styled.div`
 `;
 
 export const TOCStyle = styled.div`
-  display: none;
+  display: flex;
   flex-direction: column;
   padding: 2rem 0;
   font-size: 0.875rem;
-  min-width: 16.875rem;
-  position: sticky;
+  position: fixed;
+  top: calc(var(--docs-dev-center-nav));
+  z-index: 11;
+  min-width: 100%;
+  width: 100%;
+  height: 100%;
+  background: white;
   align-self: flex-start;
-  top: calc(3rem + var(--docs-dev-center-nav));
   max-height: 100vh;
+  padding: 0 2.5rem;
+  left: -100vw;
+  transition: left 0.4s;
+  overflow: scroll;
+
+  &.slideOut {
+    animation: ${slideOut} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+  &.slideIn {
+    animation: ${slideIn} 0.4s ease-in-out;
+    animation-fill-mode: forwards;
+  }
+
+  div.mobileHeader {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    height: 64px;
+    border-bottom: 2px solid #d1d5db;
+    margin-bottom: 35px;
+
+    h2 {
+      font-family: 'Amazon Ember';
+      font-style: normal;
+      font-weight: 800;
+      font-size: 16px;
+      line-height: 20px;
+      display: flex;
+      align-items: center;
+      letter-spacing: -0.0008em;
+      margin: 0;
+    }
+  }
 
   ${MQTablet} {
     &.more-width {
@@ -25,6 +87,16 @@ export const TOCStyle = styled.div`
 
   ${MQDesktop} {
     display: flex;
+  }
+
+  ${MQTablet}, ${MQDesktop} {
+    min-width: 16.875rem;
+    width: unset;
+    position: sticky;
+    top: calc(3rem + var(--docs-dev-center-nav));
+    padding: 2rem 0;
+    z-index: 0;
+    overflow: unset;
   }
 
   a {
@@ -68,8 +140,11 @@ export const H3AnchorStyle = styled.div`
 `;
 
 export const HeaderStyle = styled.div`
-  padding-left: 1.5rem;
   padding-right: 1.5rem;
   padding-bottom: 0.5rem;
   border-left: 0.05rem solid var(--border-color);
+
+  ${MQTablet}, ${MQDesktop} {
+    padding-left: 1.5rem;
+  }
 `;

src/components/FeedbackSticky/index.tsx
@@ -1,0 +1,227 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+import {
+  FeedbackContainer,
+  VoteButton,
+  VoteButtonReplace,
+  VoteButtonsContainer,
+  FeedbackText,
+  Divider,
+  ButtonStyles
+} from './styles';
+import { trackFeedbackSubmission } from '../../utils/track';
+import { Icon, Button } from '@cloudscape-design/components';
+
+enum FeedbackState {
+  START = 'START',
+  UP = 'UP',
+  DOWN = 'DOWN',
+  HIDDEN = 'HIDDEN'
+}
+
+type Feedback = {
+  vote: boolean;
+  page_path: string;
+  id?: string;
+  comment?: string;
+};
+
+export default function Feedback({ footerRef }) {
+  const [state, setState] = useState(FeedbackState.START);
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const footer = footerRef.current;
+
+    if (typeof window !== 'undefined') {
+      window.addEventListener('touchmove', hideFeedback);
+      window.addEventListener('scroll', hideFeedback);
+
+      let prevScrollPos = window.scrollY;
+
+      // eslint-disable-next-line no-inner-declarations
+      function hideFeedback() {
+        // Scroll variables and calculations
+        const currPos = window.scrollY,
+          footerBuffer = 50,
+          footerVisible =
+            document.body.scrollHeight - footer.offsetHeight + footerBuffer <=
+            currPos + window.innerHeight,
+          scrollUp = prevScrollPos >= currPos,
+          scrollDown = prevScrollPos < currPos,
+          container = containerRef.current;
+
+        if (
+          container &&
+          ((scrollUp && footerVisible) ||
+            (scrollDown && currentState != FeedbackState.DOWN) ||
+            (scrollDown && footerVisible)) &&
+          !container.classList.contains('close')
+        ) {
+          container.classList.remove('slideIn');
+          container.classList.add('slideOut');
+          container.ariaHidden = 'true';
+        } else if (
+          container &&
+          scrollUp &&
+          !container.classList.contains('close')
+        ) {
+          container.classList.remove('slideOut');
+          container.classList.add('slideIn');
+          container.ariaHidden = 'false';
+        }
+        prevScrollPos = currPos;
+      }
+    }
+  }, []);
+
+  // Feedback Component Customizations
+  const c = {
+    feedbackQuestion: 'Was this page helpful?',
+    yesVoteResponse: 'Thanks for your feedback!',
+    noVoteResponse: 'Thanks for your feedback!',
+    noVoteCTA: 'Can you provide more details?',
+    noVoteCTAButton: 'File an issue on GitHub',
+    ctaIcon: 'external',
+    iconPosition: 'right',
+    buttonLink: 'https://github.com/aws-amplify/docs/issues/new/choose'
+  };
+
+  let currentState = state;
+
+  const onYesVote = useCallback(() => {
+    currentState = FeedbackState.UP;
+    setState(currentState);
+    trackFeedbackSubmission(true);
+  }, []);
+
+  const onNoVote = useCallback(() => {
+    currentState = FeedbackState.DOWN;
+    setState(currentState);
+    trackFeedbackSubmission(false);
+  }, []);
+
+  const close = useCallback(() => {
+    if (containerRef.current) {
+      containerRef.current.classList.add('close'),
+        containerRef.current.classList.remove('slideIn'),
+        (containerRef.current.ariaHidden = 'true');
+    }
+  }, []);
+
+  return (
+    <FeedbackContainer
+      id="feedback-container"
+      ref={containerRef}
+      className={state}
+      aria-hidden={state == FeedbackState.UP ? true : false}
+    >
+      <div className="sizing" aria-hidden="true">
+        {c.yesVoteResponse}
+      </div>
+      <div className="sizing" aria-hidden="true">
+        {c.noVoteResponse}
+      </div>
+      <div className="sizing" aria-hidden="true">
+        {c.feedbackQuestion}
+      </div>
+
+      {(() => {
+        switch (state) {
+          case 'START':
+            return (
+              <div aria-label={c.feedbackQuestion} tabIndex={0}>
+                <FeedbackText>{c.feedbackQuestion}</FeedbackText>
+                <VoteButtonsContainer>
+                  <VoteButton
+                    href="#"
+                    onClick={onYesVote}
+                    aria-label="Yes"
+                    role="button"
+                    tabIndex={0}
+                  >
+                    <Icon name="thumbs-up" variant="link" size="medium"></Icon>
+                  </VoteButton>
+                  <Divider />
+                  <VoteButton
+                    href="#"
+                    onClick={onNoVote}
+                    aria-label="No"
+                    role="button"
+                    tabIndex={0}
+                  >
+                    <Icon
+                      name="thumbs-down"
+                      variant="link"
+                      size="medium"
+                    ></Icon>
+                  </VoteButton>
+                </VoteButtonsContainer>
+              </div>
+            );
+          case 'UP':
+            return (
+              <div>
+                <FeedbackText>{c.yesVoteResponse}</FeedbackText>
+                <VoteButtonsContainer className="up">
+                  <Icon
+                    name="thumbs-up-filled"
+                    variant="success"
+                    size="medium"
+                  ></Icon>
+                  <Divider />
+                  <Icon name="thumbs-down" variant="link" size="medium"></Icon>
+                </VoteButtonsContainer>
+                <VoteButtonReplace>
+                  <Icon
+                    name="thumbs-up-filled"
+                    variant="success"
+                    size="medium"
+                  ></Icon>
+                </VoteButtonReplace>
+              </div>
+            );
+          case 'DOWN':
+            return (
+              <div>
+                <div className="response">
+                  <FeedbackText>{c.noVoteResponse}</FeedbackText>
+                  <VoteButtonsContainer className="down">
+                    <Icon name="thumbs-up" variant="link" size="medium"></Icon>
+                    <Divider />
+                    <Icon
+                      name="thumbs-down-filled"
+                      variant="error"
+                      size="medium"
+                    ></Icon>
+                  </VoteButtonsContainer>
+                </div>
+                <div className="expanding-section">
+                  <div className="cta">
+                    <p>{c.noVoteCTA}</p>
+                    <Button
+                      iconName="close"
+                      variant="icon"
+                      aria-label="close"
+                      onClick={close}
+                    ></Button>
+                  </div>
+                  <ButtonStyles>
+                    <Button
+                      href={c.buttonLink}
+                      iconName={c.ctaIcon}
+                      iconAlign={c.iconPosition}
+                      aria-label={c.noVoteCTAButton}
+                    >
+                      {c.noVoteCTAButton}
+                    </Button>
+                  </ButtonStyles>
+                </div>
+              </div>
+            );
+          default:
+            return <div></div>;
+        }
+      })()}
+    </FeedbackContainer>
+  );
+}

commit 044374067528c406e695e057a228087345cb3f0e

styling fixes for issues introduced by #5334 (#5506)
* fix for desktop sizing issues

* set explicit widths for toc and secondary nav

* tablet breakpoint changes

* using correct sizing attributes for flex

* change width from % to vw

* remove unnecessary flex

* remove unneeded flex attributes

src/components/Menu/index.tsx
@@ -14,7 +14,7 @@ import React, {
 } from 'react';
 import MenuOpenButton from './MenuOpenButton';
 import MenuCloseButton from './MenuCloseButton';
-import { MQTablet } from '../media';
+import { MQDesktop } from '../media';
 import Directory from './Directory';
 import RepoActions from './RepoActions';
 import FilterSelect from './FilterSelect';
@@ -39,10 +39,10 @@ function Menu(props: MenuProps, ref) {
   const [onDesktop, setOnDesktop] = useState(false);
 
   useEffect(() => {
-    const MQTabletJS = MQTablet.substring(6);
+    const MQDesktopJS = MQDesktop.substring(6);
     // If the media query matches, then the user is on desktop and should see the menu by default
     const onDesktop =
-      typeof window !== 'undefined' && window.matchMedia(MQTabletJS).matches;
+      typeof window !== 'undefined' && window.matchMedia(MQDesktopJS).matches;
     setIsOpen(onDesktop);
     setOnDesktop(onDesktop);
   }, []);

src/components/Menu/styles.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { keyframes } from '@emotion/react';
-import { MQTablet, MQLaptop, MQDesktop } from '../media';
+import { MQTablet, MQDesktop } from '../media';
 
 const slideIn = keyframes`
   from {
@@ -46,7 +46,7 @@ export const MenuHeaderStyle = styled.div`
     }
   }
 
-  ${MQDesktop}, ${MQTablet}, ${MQLaptop} {
+  ${MQDesktop}, ${MQTablet} {
     flex-direction: row;
     margin: 1.75rem 2.75rem 0 2.5rem;
   }
@@ -54,8 +54,7 @@ export const MenuHeaderStyle = styled.div`
 
 export const MenuStyle = styled.div`
   border-right: 0.0625rem solid var(--border-color);
-  min-width: 100vw;
-  width: 100%;
+  width: 100vw;
   height: 100%;
   z-index: 11;
   position: fixed;
@@ -73,14 +72,20 @@ export const MenuStyle = styled.div`
     animation-fill-mode: forwards;
   }
 
-  ${MQDesktop}, ${MQTablet}, ${MQLaptop} {
+  ${MQDesktop} {
     top: calc(3rem + var(--docs-dev-center-nav));
     position: sticky;
-    min-width: 20rem;
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 20rem;
     z-index: 0;
     left: unset;
     overflow: unset;
-  }}
+  }
+  ${MQTablet} {
+    width: 20rem;
+  }
+}
 
   > div {
     position: relative;

src/components/MobileMenuIcons/styles.tsx
@@ -57,10 +57,6 @@ export const Toggle = styled.div`
       cursor: unset;
     }
   }
-
-  ${MQTablet} {
-    display: none;
-  }
 `;
 
 export const Divider = styled.div`

src/components/Page/metaContent.tsx
@@ -5,7 +5,7 @@ import NextPrevious from '../NextPrevious/index';
 import { ContentStyle, ChapterTitleStyle } from './styles';
 import MobileMenuIcons from '../MobileMenuIcons';
 import { useRef, useState } from 'react';
-import { MQTablet } from '../media';
+import { MQDesktop } from '../media';
 import FeedbackSticky from '../FeedbackSticky';
 import LastUpdatedDatesProvider from '../LastUpdatedProvider';
 
@@ -40,12 +40,12 @@ export default function MetaContent({
   const [menuIsOpen, setMenuIsOpen] = useState(false);
 
   // Slice off the "@media " string at the start for use in JS instead of CSS
-  const MQTabletJS = MQTablet.substring(6);
+  const MQDesktopJS = MQDesktop.substring(6);
   // If the media query matches, then the user is on desktop and should not see the mobile toggle
   const onDesktop =
     typeof window === 'undefined'
       ? false
-      : window.matchMedia(MQTabletJS).matches;
+      : window.matchMedia(MQDesktopJS).matches;
 
   return (
     <>

src/components/TableOfContents/index.tsx
@@ -7,9 +7,9 @@ import {
   H3AnchorStyle,
   HeaderStyle
 } from './styles';
-import { forwardRef, useEffect, useState } from 'react';
+import { forwardRef, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { MQTablet } from '../media';
+import { MQDesktop } from '../media';
 import { Button } from '@cloudscape-design/components';
 
 const stickyHeaderHeight = 124;
@@ -23,11 +23,11 @@ function scroll(hash) {
 
 function TableOfContents({ children, title, buttonsRef }, ref) {
   const router = useRouter();
-  const MQTabletJS = MQTablet.substring(6);
+  const MQDesktopJS = MQDesktop.substring(6);
   const onDesktop =
     typeof window === 'undefined'
       ? false
-      : window.matchMedia(MQTabletJS).matches;
+      : window.matchMedia(MQDesktopJS).matches;
 
   if (children.length === 0) {
     return <></>;

src/components/TableOfContents/styles.tsx
@@ -30,18 +30,14 @@ export const TOCInnerStyle = styled.div`
 `;
 
 export const TOCStyle = styled.div`
-  display: flex;
-  flex-direction: column;
   padding: 2rem 0;
   font-size: 0.875rem;
   position: fixed;
   top: calc(var(--docs-dev-center-nav));
   z-index: 11;
-  min-width: 100%;
-  width: 100%;
+  width: 100vw;
   height: 100%;
   background: white;
-  align-self: flex-start;
   max-height: 100vh;
   padding: 0 2.5rem;
   left: -100vw;
@@ -80,18 +76,13 @@ export const TOCStyle = styled.div`
   }
 
   ${MQTablet} {
-    &.more-width {
-      display: flex;
-    }
+    width: 16.875rem;
   }
-
   ${MQDesktop} {
     display: flex;
-  }
-
-  ${MQTablet}, ${MQDesktop} {
-    min-width: 16.875rem;
-    width: unset;
+    flex-grow: 0;
+    flex-shrink: 0;
+    flex-basis: 16.875rem;
     position: sticky;
     top: calc(3rem + var(--docs-dev-center-nav));
     padding: 2rem 0;
@@ -144,7 +135,7 @@ export const HeaderStyle = styled.div`
   padding-bottom: 0.5rem;
   border-left: 0.05rem solid var(--border-color);
 
-  ${MQTablet}, ${MQDesktop} {
+  ${MQDesktop} {
     padding-left: 1.5rem;
   }
 `;