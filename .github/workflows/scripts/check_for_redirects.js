951adcd0a92eee5681c88c889f4f6fb9e5bbcfc0
Tim Nguyen <timngyn@gmail.com>
2023-06-15 15:35:55 -0700

    Use regex to check test if str from artifacts are numbers

--- .github/workflows/scripts/check_for_redirects.js
+++ .github/workflows/scripts/check_for_redirects.js
@@ -18,21 +18,28 @@ module.exports = {
       .readFileSync(`./${artifactName}.txt`)
       .toString();
 
-    const [prNumberStr, numOfDeletedFilesStr] = artifactContents.split('\n');
+    const [prNumberStr, numOfDeletedFilesStr] = artifactContents
+      .split('\n')
+      .map((str) => str.trim());
 
     console.log('PR number that triggered workflow:', prNumberStr);
     console.log('Number of deleted files:', numOfDeletedFilesStr);
 
-    const prNumber = Number(prNumberStr);
-    const numOfDeletedFiles = Number(numOfDeletedFilesStr);
+    if (
+      /^[1-9]\d*$/.test(prNumberStr) &&
+      /^(0|[1-9]\d*)$/.test(numOfDeletedFilesStr)
+    ) {
+      const prNumber = Number(prNumberStr);
+      const numOfDeletedFiles = Number(numOfDeletedFilesStr);
 
-    if (numOfDeletedFiles > 0) {
-      github.rest.issues.addLabels({
-        owner: ownerLogin,
-        repo: repoName,
-        issue_number: prNumber,
-        labels: ['redirects-needed']
-      });
+      if (numOfDeletedFiles > 0) {
+        github.rest.issues.addLabels({
+          owner: ownerLogin,
+          repo: repoName,
+          issue_number: prNumber,
+          labels: ['redirects-needed']
+        });
+      }
     }
   }
 };
