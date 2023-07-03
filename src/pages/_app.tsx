d408da98f7f8e42adf996dd3f8c69619a59df4d6
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2023-07-03 23:14:02 +0000

    Revert "Reorder AA scripts (#5575)"
    This reverts commit ea0b58a6ff9d32bcf7a7b218a2493410ab3d34dc.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -89,13 +89,13 @@ function MyApp({ Component, pageProps }) {
 
       {process.env.NODE_ENV !== 'production' ? (
         <>
-          <script src="https://alpha.d2c.marketing.aws.dev/client/loader/v1/d2c-load.js"></script>
           <script src="https://aa0.awsstatic.com/s_code/js/3.0/awshome_s_code.js"></script>
+          <script src="https://alpha.d2c.marketing.aws.dev/client/loader/v1/d2c-load.js"></script>
         </>
       ) : (
         <>
-          <script src="https://d2c.aws.amazon.com/client/loader/v1/d2c-load.js"></script>
           <script src="https://a0.awsstatic.com/s_code/js/3.0/awshome_s_code.js"></script>
+          <script src="https://d2c.aws.amazon.com/client/loader/v1/d2c-load.js"></script>
         </>
       )}
       <script src="/scripts/aws-ux-shortbread-v1-0-14.js"></script>
