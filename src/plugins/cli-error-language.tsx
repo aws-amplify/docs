fa690874909207c49835009e29966f5bcb89ac40
ykethan <ykethan@amazon.com>
2023-08-03 17:13:33 -0400

    fix: update regex to consider more use cases and bring branch up to speed

--- src/plugins/cli-error-language.tsx
+++ src/plugins/cli-error-language.tsx
@@ -4,10 +4,14 @@ module.exports = function(Prism) {
     error: /.*/
   };
 
+  /*
+  Example success: patterns will match lines that start with any number of spaces, followed by one or more of the symbols '✔', '✔️', '✅', '✓', or '☑️' (ignoring case), followed by any other characters.
+  */
+
   // amplify cli output messages
   Prism.languages['amplify-cli-output'] = {
-    success: /^✔|✅.*/,
-    warning: /^[\u26A0\uFE0F].*/, // Unicode representation of the warning emoji
-    error: /^🛑.*/
+    success: /(^\s*(\u2714\uFE0F?|\u2705|\u2713|\u2611\uFE0F|\u1F7E2|\u1F7E9|\u1F535)+).*/gim, // Unicode representation of the success emojis
+    warning: /(^\s*(\u26A0\uFE0F|\u2757|\u2755|\u26A1|\u1F4A5)+).*/gim, // Unicode representation of the warning emojis
+    error: /(^\s*(\u1F6D1|\u26D4|\u1F6AB|\u274C|\u2B55)+).*/gim // Unicode representation of the error emojis
   };
 };
