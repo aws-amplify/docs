8ee629328669e3676fd5b0ae18ef3378baccdae6
timngyn <timngyn@gmail.com>
2023-08-16 15:00:29 -0700

    Add width and height props

--- src/components/YoutubeEmbed/YoutubeEmbed.tsx
+++ src/components/YoutubeEmbed/YoutubeEmbed.tsx
@@ -1,10 +1,18 @@
 type YoutubeEmbedProps = {
   src: string;
+  width?: string;
+  height?: string;
 };
 
-export function YoutubeEmbed({ src }: YoutubeEmbedProps) {
+export function YoutubeEmbed({
+  src,
+  width = '560',
+  height = '315'
+}: YoutubeEmbedProps) {
   return (
     <iframe
+      width={width}
+      height={height}
       sandbox="allow-scripts allow-same-origin"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
