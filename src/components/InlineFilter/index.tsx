a6a231b56af96fa5e56084cc218a140f5aa359e2
Rene Brandel <renbran@amazon.com>
2023-08-22 17:54:32 -0400

    fixed ESLint errors

--- src/components/InlineFilter/index.tsx
+++ src/components/InlineFilter/index.tsx
@@ -1,21 +1,20 @@
-import type React from 'react';
 import FilterChildren from '../FilterChildren';
 
 type InlineFilterProps = {
-  children: React.ReactNode
-  filters: string[]
-}
+  children: React.ReactNode;
+  filters: string[];
+};
 
 export default function InlineFilter({ filters, children }: InlineFilterProps) {
   if (!filters || !Array.isArray(filters) || filters.length < 1) {
-    return <></>
+    return <></>;
   }
 
-  const filteredChildren: Array<React.JSX.Element> = []
-  
-  filters.forEach(filter => {
-    filteredChildren.push(<div key={filter}>{children}</div>)
-  })
+  const filteredChildren: Array<React.JSX.Element> = [];
+
+  filters.forEach((filter) => {
+    filteredChildren.push(<div key={filter}>{children}</div>);
+  });
 
   return <FilterChildren>{filteredChildren}</FilterChildren>;
 }
