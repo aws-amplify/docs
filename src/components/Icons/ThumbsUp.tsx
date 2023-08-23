a1673a8452e7de71bd30c9f177a55b6a11dd6cd3
Katie Goines <30757403+katiegoines@users.noreply.github.com>
2023-08-23 15:37:01 -0700

    feedback component v2 (#5778)
    * remove cloudscape designs
    
    * remove unused import
    
    * update menu tests
    
    * update feedback icons
    
    * remove extra feedback links
    
    * change visibility of feedback component on load
    
    * updated sizing and color, added close button
    
    * add end of content feedback component
    
    * remove pill for phase 1
    
    * fix tests
    
    * feedback pill changes
    
    * fixing animation on feedback component
    
    * animation for bottom feedback and logic for hiding
    
    * fix tests
    
    * changes from alee
    
    * put back removed package
    
    * remove floating feedback component
    
    ---------
    
    Co-authored-by: Jacob Logan <lognjc@amazon.com>
    Co-authored-by: katiegoines <katiegoines@gmail.com>

---
+++ src/components/Icons/ThumbsUp.tsx
@@ -0,0 +1,29 @@
+import { Icon } from '@aws-amplify/ui-react';
+import styled from '@emotion/styled';
+
+export const IconWrapper = styled.span`
+  svg {
+    vertical-align: top;
+    path {
+      stroke: currentColor;
+      stroke-width: 1.5px;
+      stroke-linejoin: round;
+    }
+  }
+`;
+
+export const ThumbsUpIcon = () => (
+  <IconWrapper>
+    <Icon
+      ariaLabel="Thumbs up"
+      pathData="M6 6.42c0-.27.06-.55.19-.79l1.64-3.28c.1-.21.32-.34.55-.34.34 0 .62.28.62.62v3.33h3.44a2 2 0 0 1 1.94 2.48l-1.01 4.05a2 2 0 0 1-1.94 1.52H6V6.42ZM6 14H2V7h4"
+      viewBox={{
+        minX: 0,
+        minY: 0,
+        width: 16,
+        height: 16
+      }}
+      fill="none"
+    />
+  </IconWrapper>
+);
