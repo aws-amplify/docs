0c9cd28dcdaabde0f36219237f3a97f56f0f8d10
Jacob Logan <lognjc@amazon.com>
2023-09-12 16:57:40 -0700

    add static paths/props to all dynamic mdx pages

--- src/plugins/page.tsx
+++ src/plugins/page.tsx
@@ -7,7 +7,7 @@ module.exports = (async () => {
         tree.children.splice(index + 1, 0, {
           type: 'export',
           default: true,
-          value: `export default ({ frontmatter, children, platform, filterKind }) => <Page frontmatter={frontmatter} meta={meta} platform={platform} filterKind={filterKind}>{children}</Page>`
+          value: `export default ({ frontmatter, children, platform, filterKind, integration }) => <Page frontmatter={frontmatter} meta={meta} platform={platform||integration} filterKind={filterKind}>{children}</Page>`
         });
       }
     });
