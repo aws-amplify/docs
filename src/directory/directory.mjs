b49ad4c838afea92d39f724201ef060233bb092b
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-09-13 13:49:58 -0700

    chore: add download

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -1440,7 +1440,7 @@ export const directory = {
           {
             title: 'Download files',
             route: '/lib-v1/storage/download',
-            filters: ['android', 'ios', 'flutter']
+            filters: ['android', 'ios', 'flutter', `js`, `react-native`]
           },
           {
             title: 'Get file properties',
