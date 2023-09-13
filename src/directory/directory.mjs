01eacf75669a7462af56e65d42a3ac5433acd611
Venkata Ramyasri Kota <venkataramyasrikota98@gmail.com>
2023-09-13 13:41:18 -0700

    chore: remove escape hatch

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -1475,7 +1475,7 @@ export const directory = {
           {
             title: 'Escape hatch',
             route: '/lib-v1/storage/escapehatch',
-            filters: ['android', 'ios', 'js', 'react-native']
+            filters: ['android', 'ios']
           },
           {
             title: 'Use existing AWS resources',
