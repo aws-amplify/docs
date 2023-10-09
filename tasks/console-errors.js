4a90dee682f8ffec738daf0e39d8337360e15dff
Jacob Logan <lognjc@amazon.com>
2023-10-09 15:18:17 -0700

    update script to exclude known errors and run against all pages on the sitemap

--- tasks/console-errors.js
+++ tasks/console-errors.js
@@ -1,19 +1,18 @@
 const puppeteer = require('puppeteer');
+const { getSitemapUrls } = require('./get-sitemap-links');
 
-const caughtErrorTypes = [
+const excludedErrors = [
   {
-    type: 'Hydration Error',
-    errorText: 'Minified React error #418'
+    type: 'Shortbread',
+    errorText:
+      "Shortbread failed to set user's cookie preference because the domain name that was passed"
   }
 ];
 
-const PAGESTOCHECK = [
-  '/',
-  '/lib/q/platform/js/',
-  '/cli/',
-  '/console/',
-  '/guides/q/platform/js/',
-  '/start/q/integration/js/'
+const excludedScripts = [
+  'prod.assets.shortbread.aws',
+  'prod.tools.shortbread.aws',
+  'aa0.awsstatic.com'
 ];
 
 const checkPage = async (url) => {
@@ -22,32 +21,57 @@ const checkPage = async (url) => {
 
   const page = await browser.newPage();
 
-  page.on('pageerror', (message) => {
-    caughtErrorTypes.forEach((errorType) => {
-      if (message.message.includes(errorType.errorText)) {
+  page
+    .on('pageerror', (message) => {
+      let errorText = message.message;
+      const excluded = excludedErrors.some((excludedError) => {
+        return errorText.includes(excludedError.errorText);
+      });
+
+      if (!excluded) {
         errorsFound.push({
           page: url,
-          errorType: errorType.type,
-          message: message.message
+          message: errorText
         });
       }
+    })
+    .on('console', (message) => {
+      if (message.type().toLowerCase() === 'error') {
+        let errorText = message.text();
+        let callingScript = message.location().url;
+        const excludedFromError = excludedErrors.some((excludedError) => {
+          return errorText.includes(excludedError.errorText);
+        });
+        const excludedFromScript = excludedScripts.some((excludedScript) => {
+          return callingScript.includes(excludedScript);
+        });
+        const excluded = excludedFromError || excludedFromScript;
+
+        if (!excluded) {
+          errorsFound.push({
+            page: url,
+            message: errorText
+          });
+        }
+      }
     });
-  });
 
   await page.goto(url, { waitUntil: 'domcontentloaded' });
 
-  browser.close();
+  await browser.close();
 
   return errorsFound;
 };
 
 const consoleErrors = async (domain) => {
+  let pagesToCheck = await getSitemapUrls(domain);
   let errorMessage = '';
-  for (let i = 0; i < PAGESTOCHECK.length; i++) {
-    let url = `${domain}${PAGESTOCHECK[i]}`;
+  for (let i = 0; i < pagesToCheck.length; i++) {
+    let url = pagesToCheck[i];
+    console.log(`checking page ${url}`);
     let errorsFound = await checkPage(url);
     errorsFound.forEach((error) => {
-      errorMessage += `${error.errorType} found on ${error.page}\n`;
+      errorMessage += `${error.message} found on ${error.page}\n`;
     });
   }
   console.log(errorMessage);
