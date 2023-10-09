5c43eaf64c1a0c9693e00f9fad8c6526b12939d6
Heather <hbuchel@gmail.com>
2023-10-09 15:02:58 -0400

    add some more test pages

--- src/components/TestNav/TestNav.tsx
+++ src/components/TestNav/TestNav.tsx
@@ -9,6 +9,34 @@ export const TestNav = () => {
           <a href="/javascript">/javascript</a>
           <ul>
             <li>
+              <a href="/javascript/get-started">Get started</a>
+              <ul>
+                <li><a href="/javascript/get-started/project-setup">Project setup</a></li>
+                <li><a href="/javascript/get-started/sample-apps">Sample Apps</a></li>
+                <li>
+                  <a href="/javascript/get-started/tutorials">Tutorials</a>
+                  <ul>
+                    <li><a href="/javascript/get-started/tutorials/connect-api-and-database">Connect API and Database</a></li>
+                    <li><a href="/javascript/get-started/tutorials/deploy-and-host-app">Deploy and Host App</a></li>
+                    <li><a href="/javascript/get-started/tutorials/setup-amplify">Setup Amplify</a></li>
+                    <li><a href="/javascript/get-started/tutorials/setup-fullstack-project">Setup fullstack project</a></li>
+                    <li><a href="/javascript/get-started/tutorials/next-steps">Next Steps</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/javascript/build-a-backend">Build a backend</a>
+              <ul>
+                <li>
+                  <a href="/javascript/build-a-backend/api-graphql">API GraphQL</a>
+                </li>
+                <li>
+                  <a href="/javascript/build-a-backend/api-rest">API Rest</a>
+                </li>
+              </ul>
+            </li>
+            <li>
               <a href="/javascript/test-page">/javascript/test-page</a>
             </li>
           </ul>
@@ -17,9 +45,41 @@ export const TestNav = () => {
         <li>
           <a href="/android">/android</a>
           <ul>
-            <a href="/android/test-page">/android/test-page</a>
+            <li>
+              <a href="/android/get-started">Get started</a>
+              <ul>
+                <li><a href="/android/get-started/project-setup">Project setup</a></li>
+                <li><a href="/android/get-started/sample-apps">Sample Apps</a></li>
+                <li>
+                  <a href="/android/get-started/tutorials">Tutorials</a>
+                  <ul>
+                    <li><a href="/android/get-started/tutorials/connect-api-and-database">Connect API and Database</a></li>
+                    <li><a href="/android/get-started/tutorials/deploy-and-host-app">Deploy and Host App</a></li>
+                    <li><a href="/android/get-started/tutorials/setup-amplify">Setup Amplify</a></li>
+                    <li><a href="/android/get-started/tutorials/setup-fullstack-project">Setup fullstack project</a></li>
+                    <li><a href="/android/get-started/tutorials/next-steps">Next Steps</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/android/build-a-backend">Build a backend</a>
+              <ul>
+                <li>
+                  <a href="/android/build-a-backend/api-graphql">API GraphQL</a>
+                </li>
+                <li>
+                  <a href="/android/build-a-backend/api-rest">API Rest</a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="/android/test-page">/javascript/test-page</a>
+            </li>
           </ul>
         </li>
+
+       
       </ul>
     </View>
   );
