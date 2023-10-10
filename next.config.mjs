e455bf36ee148d0044b3aefc232b287f0d45e42e
katiegoines <katiegoines@gmail.com>
2023-10-10 12:32:37 -0700

    move redirects into next.config

--- next.config.mjs
+++ next.config.mjs
@@ -101,6 +101,254 @@ export default async (phase, { defaultConfig }) => {
           ]
         }
       ];
+    },
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async rewrites() {
+      return [
+        {
+          source: '/:path',
+          destination: '/404/index.html'
+        }
+      ];
+    },
+    // eslint-disable-next-line @typescript-eslint/require-await
+    async redirects() {
+      return [
+        {
+          source: '/lib/ssr/ssr/q/platform/js/',
+          destination: '/lib/ssr/q/platform/js/',
+          permanent: true
+        },
+        {
+          source: '/cli/function/function/',
+          destination: '/cli/function/',
+          permanent: true
+        },
+        {
+          source: '/lib/ssr/ssr/',
+          destination: '/lib/ssr/',
+          permanent: true
+        },
+        {
+          source: '/cli/plugins/',
+          destination: '/cli/plugins/plugins/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/tags/',
+          destination: '/cli/project/tags/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/permissions-boundary/',
+          destination: '/cli/project/permissions-boundary/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/command-hooks/',
+          destination: '/cli/project/command-hooks/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/monorepo/',
+          destination: '/cli/project/monorepo/',
+          permanent: true
+        },
+        {
+          // page has ben removed, has note, do we want this redirected elsewhere?
+          source: '/cli/usage/iam/',
+          destination: '/cli/reference/iam/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/iam-roles-mfa/',
+          destination: '/cli/reference/iam-roles-mfa/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/customcf/',
+          destination: '/cli/custom/cloudformation/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/upgrade/',
+          destination: '/cli/start/workflows/#upgrade-amplify-cli',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/uninnstall/',
+          destination: '/cli/start/workflows/#uninstall-amplify-cli',
+          permanent: true
+        },
+        {
+          source: '/cli/graphql-transformer/:path*',
+          destination: '/cli-legacy/graphql-transformer/:path*',
+          permanent: true
+        },
+        {
+          source: '/cli-legacy/',
+          destination: '/cli/',
+          permanent: true
+        },
+        {
+          source: '/console/adminui/intro/',
+          destination: '/console',
+          permanent: true
+        },
+        {
+          source: '/ui/customization/theming/q/framework/react/',
+          destination: 'https://ui.docs.amplify.aws/react/theming', // updated from https://ui.docs.amplify.aws/theming to remove additional redirect once on UI
+          permanent: true
+        },
+        {
+          source: '/cli/hosting/',
+          destination: '/cli/hosting/hosting',
+          permanent: false
+        },
+        {
+          // this path does not exist in Amplify UI
+          source: '/ui/storage/s3-image-picker/q/framework/:path/',
+          destination:
+            'https://ui.docs.amplify.aws/components/storage?platform=:path#s3-image-picker',
+          permanent: false
+        },
+        {
+          source: '/cli/usage/uninstall/',
+          destination: '/cli/start/workflows/#uninstall-amplify-cli',
+          permanent: false
+        },
+        {
+          // consolidated platform specific redirects
+          source:
+            '/guides/location-service/setting-up-your-app/q/platform/:platform/',
+          destination: '/lib/geo/getting-started/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          // consolidated platform specific redirects
+          source:
+            '/guides/location-service/tracking-device-location/q/platform/:platform/',
+          destination: '/lib/geo/escapehatch/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          // consolidated platform specific redirects
+          source: '/lib/auth/social_signin_web_ui/q/platform/:platform/',
+          destination: '/lib/auth/social/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          source: '/start/q/integration/ionic/',
+          destination: '/start/q/integration/js/',
+          permanent: true
+        },
+        {
+          source: '/start/getting-started/installation/q/integration/ionic/',
+          destination: '/start/getting-started/installation/q/integration/js/',
+          permanent: true
+        },
+        {
+          source: '/lib/in-app-messaging/prerequisites/q/platform/:platform/',
+          destination:
+            '/lib/in-app-messaging/getting-started/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          // consolidated platforms
+          source: '/lib/devpreview/getting-started/q/platform/:platform/',
+          destination: '/lib/project-setup/upgrade-guide/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          source: '/cli/migration/lambda-node-version-update/',
+          destination: '/cli/function/configure-options/#updating-the-runtime',
+          permanent: true
+        },
+        {
+          source: '/lib/in-app-messaging/customize/q/platform/js/',
+          destination:
+            'https://ui.docs.amplify.aws/react/connected-components/in-app-messaging',
+          permanent: true
+        },
+        {
+          // made not platform specific
+          source: '/guides/storage/transfer-acceleration/q/platform/:platform/',
+          destination:
+            '/lib/storage/transfer-acceleration/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          source: '/lib/auth/customui/q/platform/js/',
+          destination:
+            'https://ui.docs.amplify.aws/react/connected-components/authenticator',
+          permanent: true
+        },
+        {
+          source: '/lib/auth/customui/q/platform/react-native/',
+          destination:
+            'https://ui.docs.amplify.aws/react-native/connected-components/authenticator',
+          permanent: true
+        },
+        {
+          // made not platform specific
+          source:
+            '/lib/graphqlapi/create-or-re-use-existing-backend/q/platform/:platform',
+          destination:
+            '/lib/graphqlapi/existing-resources/q/platform/:platform/',
+          permanent: true
+        },
+        {
+          source: '/console/uibuilder/textfieldtoarea/',
+          destination: '/console/uibuilder/figmatocode/#figma-file-changelog',
+          permanent: true
+        },
+        {
+          source: '/cli/graphql/offline-data-access-and-conflict-resolution/',
+          destination: '/lib/datastore/conflict/q/platform/js/',
+          permanent: true
+        },
+        {
+          source: '/console/storage/develop',
+          destination: '/console/storage/file-browser/',
+          permanent: true
+        },
+        {
+          source: '/cli/usage/add-custom-resources',
+          destination: '/cli/custom/cdk/',
+          permanent: true
+        },
+        {
+          // made not platform specific
+          source: '/lib/push-notifications/overview/q/platform/:platform/',
+          destination:
+            '/lib/push-notifications/getting-started/q/platform/:platform',
+          permanent: false
+        },
+        {
+          // made not platform specific
+          source:
+            '/lib/push-notifications/working-with-api/q/platform/:platform/',
+          destination:
+            '/lib/push-notifications/getting-started/q/platform/:platform',
+          permanent: false
+        },
+        {
+          source: '/ui/:path*',
+          destination: 'https://ui.docs.amplify.aws/',
+          permanent: true
+        },
+        {
+          source: '/ui-legacy/:path*',
+          destination: 'https://ui.docs.amplify.aws/',
+          permanent: true
+        },
+        {
+          // destination page doesn't exist
+          source: '/ui-legacy/interactions/chatbot/:path*',
+          destination: 'https://ui.docs.amplify.aws/components/chatbot',
+          permanent: true
+        }
+      ];
     }
   });
 
