d1395c4e6a832640138c3f299eafa4db871881e0
Jacob Logan <lognjc@amazon.com>
2023-10-10 15:23:40 -0700

    update index script

--- writeIndex.mjs
+++ writeIndex.mjs
@@ -1,16 +1,29 @@
 import directory from './src/directory/directory.mjs';
 import fs from 'fs';
-
-console.log(directory);
+// import generatePathMap from './generatePathMap.cjs.js';
 
 const values = Object.values(directory);
 values.forEach((dirRoot) => {
   const productRoot = dirRoot.productRoot;
   let route = productRoot.route;
   const filePath = `./src/pages${route}/index.mdx`;
-  if (fs.existsSync(filePath)) {
+  const secondaryPath = `./src/pages${route}/index.tsx`;
+  if (fs.existsSync(filePath) || fs.existsSync(secondaryPath)) {
     console.log(`index file exists for ${productRoot.title}`);
   } else {
     console.log(`index file does not extist for ${filePath}`);
   }
 });
+
+// let directoryPages = generatePathMap(directory);
+// console.log(directoryPages);
+// console.log(
+//   Object.values(directoryPages).filter((d) =>
+//     d.page.startsWith('/lib/project-setup/prereq')
+//   )
+// );
+// console.log(
+//   Object.keys(directoryPages).filter((d) =>
+//     d.startsWith('/lib/project-setup/prereq')
+//   )
+// );
