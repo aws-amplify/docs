f226ef4beabe0f7a100e688d2de09a6289d41c9c
timngyn <timngyn@gmail.com>
2023-10-10 19:07:19 -0700

    Setting up with next/link

--- src/components/Menu-new/Menu.tsx
+++ src/components/Menu-new/Menu.tsx
@@ -1,7 +1,9 @@
 import { ReactElement, useCallback, useState } from 'react';
-import { Flex, Icon, View, Text } from '@aws-amplify/ui-react';
+import { Icon, View, Text } from '@aws-amplify/ui-react';
+import Link from 'next/link';
 import { directory } from './buildDirectory.mjs';
 import styles from './Menu.module.scss';
+import { useRouter } from 'next/router';
 
 export function Menu(): ReactElement {
   const rootPage = directory;
@@ -27,6 +29,7 @@ export function Menu(): ReactElement {
 const CATEGORY_LEVEL = 1;
 
 function generateListItem(pageNode, level): ReactElement {
+  const router = useRouter();
   const [open, setOpen] = useState(false);
   const toggleDrawer = useCallback(() => {
     setOpen((prevOpen) => !prevOpen);
@@ -36,38 +39,55 @@ function generateListItem(pageNode, level): ReactElement {
     return <></>;
   }
 
+  const pathname = `/${router.query.platform}/${pathHelper(pageNode.route)}`;
+  console.log(pathname);
+  console.log('router', router);
+  const current = router.asPath === pathname;
+  console.log(current);
+
   return (
     <li
       tabIndex={0}
       key={pageNode.route}
       className={styles['category-container']}
     >
-      <a
-        className={`${styles['heading']} ${
-          level === CATEGORY_LEVEL ? styles['category'] : styles['subcategory']
-        }`}
-        onClick={level > CATEGORY_LEVEL ? toggleDrawer : undefined}
+      <Link
+        href={{
+          pathname: `/[platform]/${pathHelper(pageNode.route)}`,
+          query: { platform: 'javascript' }
+        }}
+        passHref
+        legacyBehavior
       >
-        {pageNode.title}
-        {pageNode.children && level !== CATEGORY_LEVEL && (
-          <Icon
-            className={open ? styles['chevron-open'] : ''}
-            viewBox={{ width: 12, height: 12 }}
-            fill="none"
-            width="12"
-            height="12"
-            paths={[
-              {
-                d:
-                  'M3.96289 0.75L2.99954 1.71335L7.28566 6L2.99954 10.2867L3.96289 11.25L9.21289 6L3.96289 0.75Z',
-                fillRule: 'evenodd',
-                clipRule: 'evenodd',
-                fill: '#000716'
-              }
-            ]}
-          />
-        )}
-      </a>
+        <a
+          className={`${styles['heading']} ${
+            level === CATEGORY_LEVEL
+              ? styles['category']
+              : styles['subcategory']
+          } ${current ? styles['current'] : ''}`}
+          onClick={level > CATEGORY_LEVEL ? toggleDrawer : undefined}
+        >
+          {pageNode.title}
+          {pageNode.children && level !== CATEGORY_LEVEL && (
+            <Icon
+              className={open ? styles['chevron-open'] : ''}
+              viewBox={{ width: 12, height: 12 }}
+              fill="none"
+              width="12"
+              height="12"
+              paths={[
+                {
+                  d:
+                    'M3.96289 0.75L2.99954 1.71335L7.28566 6L2.99954 10.2867L3.96289 11.25L9.21289 6L3.96289 0.75Z',
+                  fillRule: 'evenodd',
+                  clipRule: 'evenodd',
+                  fill: '#000716'
+                }
+              ]}
+            />
+          )}
+        </a>
+      </Link>
       {pageNode.children && (
         <ul style={!open && level > CATEGORY_LEVEL ? { display: 'none' } : {}}>
           {pageNode.children.map((child) => generateListItem(child, level + 1))}
@@ -76,3 +96,17 @@ function generateListItem(pageNode, level): ReactElement {
     </li>
   );
 }
+
+const pathHelper = (path: string) => {
+  const prefix = 'pages/[platform]/';
+
+  const pathNoPrefix = path.slice(prefix.length).split('.')[0];
+
+  const lastIndex = pathNoPrefix.lastIndexOf('index');
+
+  if (lastIndex > -1) {
+    return pathNoPrefix.substring(0, lastIndex);
+  } else {
+    return pathNoPrefix;
+  }
+};
