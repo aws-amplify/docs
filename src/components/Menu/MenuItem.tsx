341c5f57a9b84043169ed5821544161db32c6e73
timngyn <timngyn@gmail.com>
2023-10-16 14:22:35 -0700

    Refactor menu component to use Amplify UI primitives and remove module scss

--- src/components/Menu/MenuItem.tsx
+++ src/components/Menu/MenuItem.tsx
@@ -1,11 +1,15 @@
 import { useRouter } from 'next/router';
 import { ReactElement, useCallback, useEffect, useState } from 'react';
-import { Icon } from '@aws-amplify/ui-react';
+import { Link as AmplifyUILink, Flex, View } from '@aws-amplify/ui-react';
+import { IconExternalLink, IconChevron } from '@/components/Icons';
 import Link from 'next/link';
-import styles from './Menu.module.scss';
 import { Platform } from '@/data/platforms';
 
-const CATEGORY_LEVEL = 1;
+enum Levels {
+  Category = 1,
+  Subcategory = 2,
+  Page = 3
+}
 
 const pathHelper = (path: string) => {
   const prefix = 'pages/[platform]/';
@@ -23,10 +27,10 @@ const pathHelper = (path: string) => {
 
 type PageNode = {
   title: string;
-  route: string;
-  isExternal?: boolean;
   platforms: string[];
   children: PageNode[];
+  route: string;
+  isExternal: boolean;
 };
 
 type MenuItemProps = {
@@ -51,9 +55,24 @@ export function MenuItem({
   const pathname = `/${currentPlatform}/${pathHelper(pageNode.route)}/`;
   const current = router.asPath === pathname;
 
-  const currentStyle = current ? styles['current'] : '';
-  const categoryStyle =
-    level === CATEGORY_LEVEL ? styles['category'] : styles['subcategory'];
+  const currentStyle = current ? 'menu__item--current' : '';
+  // const categoryStyle =
+  //   level === Levels.Category
+  //     ? 'menu__item--category'
+  //     : 'menu__item--subcategory';
+
+  let categoryStyle = '';
+  switch (level) {
+    case Levels.Category:
+      categoryStyle = 'menu__item--category';
+      break;
+    case Levels.Subcategory:
+      categoryStyle = 'menu__item--subcategory';
+      break;
+    default:
+      categoryStyle = 'menu__item--page';
+      break;
+  }
 
   useEffect(() => {
     if (current) {
@@ -72,12 +91,30 @@ export function MenuItem({
   if (!pageNode) {
     return <></>;
   }
-  console.log('currentPlatform', currentPlatform);
-  console.log('router', router);
 
-  if (pageNode.platforms.includes(currentPlatform)) {
+  if (pageNode.isExternal) {
     return (
-      <li key={pageNode.route} className={styles['category-container']}>
+      <li key={pageNode.route}>
+        <AmplifyUILink href={pageNode.route} isExternal={true}>
+          <View
+            className={`menu__item`}
+            onClick={level > Levels.Category ? toggleDrawer : undefined}
+          >
+            <Flex
+              justifyContent="space-between"
+              alignItems="center"
+              className={`menu__item__inner ${categoryStyle} ${currentStyle}`}
+            >
+              {pageNode.title}
+              <IconExternalLink />
+            </Flex>
+          </View>
+        </AmplifyUILink>
+      </li>
+    );
+  } else if (pageNode.platforms.includes(currentPlatform)) {
+    return (
+      <li key={pageNode.route}>
         <Link
           href={{
             pathname: `/[platform]/${pathHelper(pageNode.route)}`,
@@ -85,33 +122,25 @@ export function MenuItem({
           }}
           passHref
         >
-          <div
-            className={`${styles['heading']} ${categoryStyle} ${currentStyle}`}
-            onClick={level > CATEGORY_LEVEL ? toggleDrawer : undefined}
+          <View
+            className={`menu__item ${categoryStyle} ${currentStyle}`}
+            onClick={level > Levels.Category ? toggleDrawer : undefined}
           >
-            {pageNode.title}
-            {pageNode.children && level !== CATEGORY_LEVEL && (
-              <Icon
-                className={open ? styles['chevron-open'] : ''}
-                viewBox={{ width: 12, height: 12 }}
-                fill="none"
-                width="12"
-                height="12"
-                paths={[
-                  {
-                    d: 'M3.96289 0.75L2.99954 1.71335L7.28566 6L2.99954 10.2867L3.96289 11.25L9.21289 6L3.96289 0.75Z',
-                    fillRule: 'evenodd',
-                    clipRule: 'evenodd',
-                    fill: '#000716'
-                  }
-                ]}
-              />
-            )}
-          </div>
+            <Flex
+              justifyContent="space-between"
+              alignItems="center"
+              className={`menu__item__inner ${categoryStyle} ${currentStyle}`}
+            >
+              {pageNode.title}
+              {pageNode.children && level !== Levels.Category && (
+                <IconChevron className={open ? 'menu__item--open' : ''} />
+              )}
+            </Flex>
+          </View>
         </Link>
         {pageNode.children && (
           <ul
-            style={!open && level > CATEGORY_LEVEL ? { display: 'none' } : {}}
+            style={!open && level > Levels.Category ? { display: 'none' } : {}}
           >
             {pageNode.children.map((child) => (
               <MenuItem
