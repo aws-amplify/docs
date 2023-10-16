c100d331d3250aa1dfb988565162c35b76d83275
Jacob Logan <lognjc@amazon.com>
2023-10-16 01:42:08 -0700

    add platform images

--- src/components/PlatformNavigator/index.tsx
+++ src/components/PlatformNavigator/index.tsx
@@ -1,21 +1,26 @@
 import { useState } from 'react';
-import { Button, Link } from '@aws-amplify/ui-react';
+import { Button, Link, Flex, Image } from '@aws-amplify/ui-react';
 import { MenuChevron } from '../Icons';
+import * as img from '../../constants/img';
 
 const platformList = [
-  { title: 'Android', link: '/android' },
-  { title: 'Angular', link: '/angular' },
-  { title: 'Flutter', link: '/flutter' },
-  { title: 'Javascript', link: '/javascript' },
-  { title: 'Next.js', link: '/next' },
-  { title: 'React', link: '/react' },
-  { title: 'React Native', link: '/react-native' },
-  { title: 'Swift', link: '/swift' },
-  { title: 'Vue', link: '/vue' }
+  { title: 'Android', link: '/android', imgName: 'ANDROID' },
+  { title: 'Angular', link: '/angular', imgName: 'ANGULAR' },
+  { title: 'Flutter', link: '/flutter', imgName: 'FLUTTER' },
+  { title: 'Javascript', link: '/javascript', imgName: 'JS' },
+  { title: 'Next.js', link: '/next', imgName: 'NEXT' },
+  { title: 'React', link: '/react', imgName: 'REACT' },
+  { title: 'React Native', link: '/react-native', imgName: 'REACTNATIVE' },
+  { title: 'Swift', link: '/swift', imgName: 'IOS' },
+  { title: 'Vue', link: '/vue', imgName: 'VUE' }
 ];
 
 export function PlatformNavigator({ currentPlatform }) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const platformItem = platformList.filter((platform) => {
+    return platform.title === currentPlatform;
+  })[0];
 
   return (
     <nav aria-labelledby="platformBtn" className={`platform-navigator`}>
@@ -29,7 +34,15 @@ export function PlatformNavigator({ currentPlatform }) {
           setIsOpen(!isOpen);
         }}
       >
-        {currentPlatform}
+        <Flex as="span" alignItems="center">
+          <Image
+            alt={img[platformItem.imgName].alt}
+            height={30}
+            width={30}
+            src={img[platformItem.imgName].src}
+          />
+          {currentPlatform}
+        </Flex>
         <MenuChevron open={isOpen} />
       </Button>
       <ul
@@ -52,7 +65,15 @@ export function PlatformNavigator({ currentPlatform }) {
               }`}
             >
               <li className={`platform-navigator__dropdown__item`}>
-                {platform.title}
+                <Flex as="span" alignItems="center">
+                  <Image
+                    alt={img[platform.imgName].alt}
+                    height={20}
+                    width={20}
+                    src={img[platform.imgName].src}
+                  />
+                  {platform.title}
+                </Flex>
               </li>
             </Link>
           );
