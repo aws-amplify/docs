f39de14ebfea6070c9547d880b0651b85dff6af8
Venkata Ramyasri Kota <34170013+kvramyasri7@users.noreply.github.com>
2023-10-23 09:47:02 -0700

    chore: update 'guest' to `guest` (#5981)
    Co-authored-by: Jim Blanchard <jim.l.blanchard@gmail.com>
    Co-authored-by: AllanZhengYP <zheallan@amazon.com>

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -1448,11 +1448,6 @@ export const directory = {
             filters: ['android', 'ios', 'flutter', 'js']
           },
           {
-            title: 'Upload files',
-            route: '/lib-v1/storage/upload',
-            filters: ['android', 'ios', 'flutter', 'js']
-          },
-          {
             title: 'Download files',
             route: '/lib-v1/storage/download',
             filters: ['android', 'ios', 'flutter', 'js']
@@ -1471,6 +1466,11 @@ export const directory = {
             title: 'Copy files',
             route: '/lib-v1/storage/copy',
             filters: ['js']
+          },
+          {
+            title: 'Upload files',
+            route: '/lib-v1/storage/upload',
+            filters: ['android', 'ios', 'flutter', 'js']
           },
           {
             title: 'Remove files',
