0293c70c853f1384329685c48f421e28a10043b3
Heather <hbuchel@gmail.com>
2023-10-23 17:45:06 -0400

    use static id in next logo

--- src/components/Icons/IconNext.tsx
+++ src/components/Icons/IconNext.tsx
@@ -27,15 +27,15 @@ export const IconNext = ({ ...rest }) => {
       />
       <path
         d="M19.9345 21.0027L9.21902 7.20007H7.20009V16.7961H8.81524V9.25122L18.6666 21.9794C19.1112 21.6819 19.5346 21.3554 19.9345 21.0027Z"
-        fill={`url(#${gradient1Id})`}
+        fill="url(#paint0_linear_16_31)"
       />
       <path
         d="M16.9333 7.20007H15.3333V16.8001H16.9333V7.20007Z"
-        fill={`url(#${gradient2Id})`}
+        fill="url(#paint1_linear_16_31)"
       />
       <defs>
         <linearGradient
-          id={gradient1Id}
+          id="paint0_linear_16_31"
           x1="14.5334"
           y1="15.5334"
           x2="19.2668"
@@ -46,7 +46,7 @@ export const IconNext = ({ ...rest }) => {
           <stop offset="1" stopColor="white" stopOpacity="0" />
         </linearGradient>
         <linearGradient
-          id={gradient2Id}
+          id="paint1_linear_16_31"
           x1="16.1333"
           y1="7.20007"
           x2="16.1065"
