b2383be1b19849cf057d8079a74fe87b1640f3d1
Heather Buchel <hbuchel@gmail.com>
2023-10-23 15:27:00 -0400

    chore(next-release/main): clean up get started styles, add aria-hidden to icons (#6057)
    * chore: clean up get started styles, add aria-hidden to icons
    
    * icon cleanup, focus styles

--- src/components/Icons/IconFlutter.tsx
+++ src/components/Icons/IconFlutter.tsx
@@ -3,6 +3,7 @@ import { Icon } from '@aws-amplify/ui-react';
 export const IconFlutter = ({ ...rest }) => {
   return (
     <Icon
+      aria-hidden="true"
       {...rest}
       viewBox={{
         minX: 0,
@@ -12,21 +13,21 @@ export const IconFlutter = ({ ...rest }) => {
       }}
     >
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M14.1101 24L7.63571 17.5482L11.2145 13.8963L21.3088 24H14.1101Z"
         fill="#02539A"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M7.55557 17.5531L13.974 11.1159H21.1893L11.2044 21.1298L7.55557 17.5531Z"
         fill="#45D1FD"
-        fill-opacity="0.85"
+        fillOpacity="0.85"
       />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
+        fillRule="evenodd"
+        clipRule="evenodd"
         d="M2 11.9385L5.64384 15.5907L21.1891 0H13.9363L2 11.9385Z"
         fill="#45D1FD"
       />
--- src/components/Icons/IconSwift.tsx
+++ src/components/Icons/IconSwift.tsx
@@ -3,6 +3,7 @@ import { Icon } from '@aws-amplify/ui-react';
 export const IconSwift = ({ ...rest }) => {
   return (
     <Icon
+      aria-hidden="true"
       {...rest}
       viewBox={{
         minX: 0,
