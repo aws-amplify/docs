3cacf8b7f3f2a9bdcba47f87e7b8c46a6db909f5
katiegoines <katiegoines@gmail.com>
2023-10-26 15:27:24 -0700

    script to writing new pages

--- tasks/new_ia_migration.ts
+++ tasks/new_ia_migration.ts
@@ -40,7 +40,7 @@ sheets.forEach((sheet) => {
   }
 });
 
-// filter out pages that aren't being migrated
+// filter out pages that aren't being migrated (no Original backend source || no New backend source)
 const migrationData = [];
 excelData.forEach((item) => {
   if (
@@ -87,6 +87,168 @@ oldPages.forEach((path) => {
 // console.log('pages not accounted for in migration doc but that exist in pages-old file structure:', notAccountedForInMigrationDoc);
 // console.log('pages that are found in pages-old structure and are notated in the excel for migration:', pagesThatWillMigrate);
 
+// change all 'Original backend source' to 'pages-old'
+migrationData.forEach((item) => {
+  const origSource = item['Original backend source'];
+  const oldPath = origSource.replace('pages', 'pages-old');
+  item['Original backend source'] = oldPath;
+});
+
+// combine multiple Excel entries for pages that have same 'New backend source' and set platform array
+migrationData.forEach((item) => {
+  const multiples =
+    excelDataNewSource.filter((path) => {
+      return path == item['New backend source'];
+    }).length > 1;
+
+  let platforms = [];
+  if (multiples) {
+    const toCombine = migrationData.filter((item2) => {
+      return item['New backend source'] == item2['New backend source'];
+    });
+
+    for (let i = 0; i < toCombine.length; i++) {
+      toCombine[i]['Platform specific'] = toCombine[i]['Platform specific']
+        .toLowerCase()
+        .replace(' (web)', '')
+        .replace('.js', '')
+        .replace('react native', 'react-native');
+
+      platforms.push(toCombine[i]['Platform specific']);
+
+      if (i != 0) {
+        migrationData.splice(migrationData.indexOf(toCombine[i]), 1);
+      }
+    }
+  } else {
+    item['Platform specific'] = item['Platform specific']
+      .toLowerCase()
+      .replace(' (web)', '')
+      .replace('.js', '')
+      .replace('react native', 'react-native');
+    platforms.push(item['Platform specific']);
+  }
+  platforms = platforms.filter((value, index) => {
+    return platforms.indexOf(value) === index;
+  });
+  item['Platform specific'] = platforms;
+});
+
+// console.log(migrationData);
+
+// Update meta and imports for all pages accounted for in Excel file
+migrationData.forEach((page) => {
+  fs.readFile(page['Original backend source'], 'utf8', (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      data = data.split('\n');
+      let exportIndex = '';
+      data.forEach((line) => {
+        if (line.includes('title: ')) {
+          data.splice(data.indexOf(line), 1, `  title: \`${page['Page']}\`,`);
+        } else if (line.includes('supportedPlatforms:')) {
+          exportIndex = data.indexOf(line);
+          data.splice(
+            data.indexOf(line),
+            1,
+            `  platforms: [ ${page['Platform specific']} ]`
+          );
+        } else if (line.includes('filterKey:')) {
+          data.splice(data.indexOf(line), 1, '<remove empty line>');
+        } else if (line.includes('import { generateStaticPaths }')) {
+          data.splice(data.indexOf(line), 1, '<remove empty line>');
+        } else if (line.includes('export const getStaticPaths')) {
+          data.splice(data.indexOf(line), 4, '<remove empty line>');
+        } else if (line.includes('export const getStaticProps')) {
+          data.splice(data.indexOf(line), 9, '<remove empty line>');
+        }
+      });
+
+      const importToAdd = `import { getCustomStaticPath } from '@/utils/getCustomStaticPath';
+      `;
+      const exportToAdd = `export const getStaticPaths = async () => {
+  return getCustomStaticPath(meta.platforms);
+};
+      
+export function getStaticProps(context) {
+  return {
+    props: {
+      platform: context.params.platform,
+      meta
+    }
+  };
+}`;
+      data.splice(exportIndex + 4, 0, exportToAdd);
+      data.unshift(importToAdd);
+
+      data = data.filter((lines) => {
+        return lines != '<remove empty line>';
+      });
+
+      const newFile = data.join('\n');
+
+      fs.writeFile(data, newFile, () => {
+        if (err) console.log(err);
+      });
+
+      //       const filterKeyLine = '\n  filterKey: "integration",';
+      //       const supportedPlatforms = '  supportedPlatforms:';
+      //       const generateStaticPaths = `import { generateStaticPaths } from "@/utils/generateStaticPaths.tsx";\n\n`;
+      //       const getStaticPaths = `
+      // export const getStaticPaths = () => {
+      //   return generateStaticPaths(meta.filterKey, meta.supportedPlatforms);
+      // };
+      //       `;
+      //       const getStaticProps = `export const getStaticProps = (context) => {
+      //       return {
+      //           props: {
+      //               integration: context.params.integration,
+      //               filterKind: meta.filterKey
+      //           }
+      //       };
+      //   };
+      //   `;
+      //       console.log(getStaticPaths);
+      //       data = data
+      //         .replace(filterKeyLine, '')
+      //         .replace(supportedPlatforms, '  platforms:')
+      //         .replace(generateStaticPaths, '')
+      //         .replace(getStaticPaths, '')
+      //         .replace(getStaticProps, '');
+
+      //       console.log(data);
+
+      // data = data.split('\n');
+      // console.log(data);
+
+      // const contentArray = data.split('\n');
+      // contentArray.forEach((line) => {
+      //   if (line.includes('filterKey: "')) {
+      //     contentArray.splice(contentArray.indexOf(line), 1);
+      //   }
+      //   if (line.includes('supportedPlatforms:')) {
+      //     const revLine = line.replace('supportedPlatforms: ', 'platforms: ');
+      //     line = revLine; // unsure if this is setting correctly
+      //   }
+      // });
+      // if (data[0].includes('export const meta')) {
+      //   console.log(data[0]);
+      // }
+      // const meta = data.indexOf('export const meta = {');
+      // if (meta != -1) {
+      //   console.log(data);
+      // }
+      // console.log(data[0], '\n', '--------');
+      // console.log(
+      //   revised,
+      //   '\n',
+      //   '----------------------------------------------------------------'
+      // );
+    }
+  });
+});
+
 // create necessary directories exist in 'pages' for each file path
 // migrationData.forEach((item) => {
 //   const newPath = item['New backend source'];
@@ -100,46 +262,33 @@ oldPages.forEach((path) => {
 //   }
 // });
 
-// combine multiple entries for pages that are available for multiple platforms
-migrationData.forEach((item) => {
-  const multiples =
-    excelDataNewSource.filter((path) => {
-      return path == item['New backend source'];
-    }).length > 1;
+// move existing pages from pages-old into new IA locations
+// migrationData.forEach((item) => {
+//   // console.log(origSource, '-->', oldPath);
+//   if (fs.existsSync(oldPath)) {
+//     // console.log(oldPath);
+//     // fs.rename(oldPath, item['New backend source'], (err) => {
+//     //   if (err) {
+//     //     console.log(item['New backend source'], err);
+//     //   } else {
+//     //     console.log(
+//     //       'rename successful: ',
+//     //       oldPath,
+//     //       '-->',
+//     //       item['New backend source']
+//     //     );
+//     //   }
+//     // });
+//   } else {
+//     // console.log('file already moved:', oldPath);
+//   }
 
-  if (multiples) {
-    console.log(item);
-  }
-
-  // move existing pages from pages-old into new IA locations
-
-  const origSource = item['Original backend source'];
-  const oldPath = origSource.replace('pages', 'pages-old'); // change path to account for move of old pages into 'pages-old'
-  // console.log(origSource, '-->', oldPath);
-  if (fs.existsSync(oldPath)) {
-    // console.log(oldPath);
-    // fs.rename(oldPath, item['New backend source'], (err) => {
-    //   if (err) {
-    //     console.log(item['New backend source'], err);
-    //   } else {
-    //     console.log(
-    //       'rename successful: ',
-    //       oldPath,
-    //       '-->',
-    //       item['New backend source']
-    //     );
-    //   }
-    // });
-  } else {
-    // console.log('file already moved:', oldPath);
-  }
-
-  // change content of migrated pages - new meta structure and imports
-  // fs.readFile(item['New backend source'], 'utf8', (err, data) => {
-  //   if (err) console.log(err)
-  //   // console.log(data)
-  // });
-});
+// change content of migrated pages - new meta structure and imports
+// fs.readFile(item['New backend source'], 'utf8', (err, data) => {
+//   if (err) console.log(err)
+//   // console.log(data)
+// });
+// });
 
 // remove all ChooseFilterPages from pages-old
 // glob('src/pages-old/**/*.mdx').then((files) => {
