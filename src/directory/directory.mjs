845f93f2f7f28efb5d8906d16ff9300fa3c9fa24
Chris Fang <cshfang@gmail.com>
2023-10-27 11:39:53 -0700

    Update project setup

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -995,12 +995,12 @@ export const directory = {
           {
             title: 'Prerequisites',
             route: '/lib-v1/project-setup/prereq',
-            filters: ['android', 'ios', 'flutter', 'js']
+            filters: ['android', 'ios', 'flutter', 'js', 'react-native']
           },
           {
             title: 'Create your application',
             route: '/lib-v1/project-setup/create-application',
-            filters: ['android', 'ios', 'flutter', 'js']
+            filters: ['android', 'ios', 'flutter', 'js', 'react-native']
           },
           {
             title: 'Using Combine with Amplify',
