9f3d3a00eca3cd59f56ea5f88e25bbc3c55e4fff
timngyn <timngyn@gmail.com>
2023-10-29 22:26:40 -0700

    Filter out pages that don't have the current platform

--- src/components/Overview/Overview.tsx
+++ src/components/Overview/Overview.tsx
@@ -3,6 +3,7 @@ import { PageNode } from 'src/directory/directory';
 import { Card, Flex, Grid, View, Text } from '@aws-amplify/ui-react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { Platform } from '@/data/platforms';
 
 type OverviewProps = {
   childPageNodes: PageNode[];
@@ -10,7 +11,7 @@ type OverviewProps = {
 
 export function Overview({ childPageNodes }: OverviewProps) {
   const router = useRouter();
-  const currentPlatform = router.query.platform;
+  const currentPlatform = router.query.platform as Platform;
 
   if (!childPageNodes) {
     return <></>;
@@ -18,25 +19,29 @@ export function Overview({ childPageNodes }: OverviewProps) {
 
   return (
     <Grid className="overview">
-      {childPageNodes.map((node) => (
-        <Link
-          key={node.route}
-          className="overview__link"
-          href={{
-            pathname: node.route,
-            query: { platform: currentPlatform ? currentPlatform : '' }
-          }}
-        >
-          <Card className="overview__link__card" variation="outlined">
-            <Flex direction="column">
-              <Text className="overview__link__card__title">{node.title}</Text>
-              <View className="overview__link__card__description">
-                {node.description}
-              </View>
-            </Flex>
-          </Card>
-        </Link>
-      ))}
+      {childPageNodes
+        .filter((node) => node.platforms.includes(currentPlatform))
+        .map((node) => (
+          <Link
+            key={node.route}
+            className="overview__link"
+            href={{
+              pathname: node.route,
+              query: { platform: currentPlatform }
+            }}
+          >
+            <Card className="overview__link__card" variation="outlined">
+              <Flex direction="column">
+                <Text className="overview__link__card__title">
+                  {node.title}
+                </Text>
+                <View className="overview__link__card__description">
+                  {node.description}
+                </View>
+              </Flex>
+            </Card>
+          </Link>
+        ))}
     </Grid>
   );
 }
