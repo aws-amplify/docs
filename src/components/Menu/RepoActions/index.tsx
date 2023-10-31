1b524e93a3642a759140c8e50ef3251e82d694d7
katiegoines <katiegoines@gmail.com>
2023-10-31 10:10:06 -0700

    remove unnecessary conditional;

--- src/components/Menu/RepoActions/index.tsx
+++ src/components/Menu/RepoActions/index.tsx
@@ -13,17 +13,14 @@ function createEditLink(directoryPath) {
 
 export default function RepoActions({ path }) {
   const directoryPath = path.path.replace('src/pages', '');
-  const shouldShowEditLink = path.path !== '/ChooseFilterPage';
   console.log(directoryPath);
   const editLink = createEditLink(directoryPath);
   return (
     <Flex className="repo-actions">
-      {shouldShowEditLink && (
-        <ExternalLink href={editLink}>
-          <Flex aria-label="Edit this page on GitHub">Edit on GitHub</Flex>
-          <IconPencil></IconPencil>
-        </ExternalLink>
-      )}
+      <ExternalLink href={editLink}>
+        <Flex aria-label="Edit this page on GitHub">Edit on GitHub</Flex>
+        <IconPencil></IconPencil>
+      </ExternalLink>
     </Flex>
   );
 }
