2de091a9469f6314c72bfdb47a92cb2836d83596
Heather <hbuchel@gmail.com>
2023-11-01 17:41:49 -0400

    Contribute changes

--- src/components/contribute/HowItWorks.tsx
+++ src/components/contribute/HowItWorks.tsx
@@ -14,50 +14,34 @@ import {
   MdCelebration,
   MdPageview
 } from 'react-icons/md';
-import {
-  FiExternalLink,
-  FiGithub,
-  FiCheck,
-  FiGitPullRequest
-} from 'react-icons/fi';
+import { FiExternalLink, FiCheck, FiGitPullRequest } from 'react-icons/fi';
+import { IconExternalLink } from '@/components/Icons';
 
 export default function HowItWorks() {
   return (
-    <Flex
-      direction="column"
-      maxWidth="1024px"
-      margin="0 auto"
-      gap="2em"
-      width={{ base: '90%', large: '60%' }}
-    >
-      <View>
-        <Badge
-          textTransform="uppercase"
-          backgroundColor="brand.rind"
-          color="brand.squidInk"
-          marginBottom="1em"
-        >
-          Making a Contribution
-        </Badge>
+    <Flex direction="column" maxWidth="800px" margin="0 auto" gap="2em">
+      <Flex direction="column" alignItems="flex-start" gap="xxxs">
+        <Badge textTransform="uppercase">Making a Contribution</Badge>
         <Heading level={2}>How it works</Heading>
-      </View>
+      </Flex>
       <Text fontSize="large" color="font.secondary">
-        The Amplify Contributor Program is open to everyone in the Amplify community! In the following steps, we describe what to expect when contributing:
+        The Amplify Contributor Program is open to everyone in the Amplify
+        community! In the following steps, we describe what to expect when
+        contributing:
       </Text>
 
       <View>
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
             >
-              {' '}
               <MdLabelOutline />
             </Flex>
             <Text>
@@ -70,15 +54,14 @@ export default function HowItWorks() {
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
             >
-              {' '}
               <FiGitPullRequest />
             </Flex>
             <Text>
@@ -90,15 +73,14 @@ export default function HowItWorks() {
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
             >
-              {' '}
               <MdPageview />
             </Flex>
             <Text>The Amplify team reviews your pull request.</Text>
@@ -108,39 +90,43 @@ export default function HowItWorks() {
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
             >
-              {' '}
               <FiCheck />
             </Flex>
-            <Text>If everything looks good, the Amplify team approves the pull request. (At this time, only accepted pull requests count toward earning badges.)</Text>
+            <Text>
+              If everything looks good, the Amplify team approves the pull
+              request. (At this time, only accepted pull requests count toward
+              earning badges.)
+            </Text>
           </Flex>
         </Card>
         <Divider size="small" />
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
+              flex="none"
             >
-              {' '}
               <MdOutlineBadge />
             </Flex>
             <Text>
-              If this is your first approved pull request, the Amplify team designates you as a Contributor in the{' '}
+              If this is your first approved pull request, the Amplify team
+              designates you as a Contributor in the{' '}
               <Link href="https://discord.com/invite/amplify" isExternal>
-                Amplify Community Discord server <FiExternalLink />
+                Amplify Community Discord server <IconExternalLink />
               </Link>
               .
             </Text>
@@ -150,15 +136,15 @@ export default function HowItWorks() {
         <Card>
           <Flex direction="row" alignItems="center">
             <Flex
-              backgroundColor="brand.rind"
-              color="brand.squidInk"
+              backgroundColor="orange.20"
+              color="neutral.100"
               width="32px"
               height="32px"
               borderRadius="50%"
               justifyContent="center"
               alignItems="center"
+              flex="none"
             >
-              {' '}
               <MdCelebration />
             </Flex>
             <Text>
@@ -169,7 +155,8 @@ export default function HowItWorks() {
               >
                 Amplify badge <FiExternalLink />
               </Link>{' '}
-              for your first contribution or make progress toward the Intermediate and Advanced badges!
+              for your first contribution or make progress toward the
+              Intermediate and Advanced badges!
             </Text>
           </Flex>
         </Card>
--- src/components/contribute/QuickstartResources.tsx
+++ src/components/contribute/QuickstartResources.tsx
@@ -2,30 +2,5 @@ import { Heading, View, Text, Flex } from '@aws-amplify/ui-react';
 import { YoutubeEmbed } from '../YoutubeEmbed';
 
 export default function QuickstartResources() {
-  return (
-    <Flex
-      direction="row"
-      justifyContent="center"
-      alignItems="top"
-      gap="5em"
-      wrap={'wrap'}
-    >
-      <View width={{ base: '90%', large: '60%' }}>
-        <Heading level={2}>Quickstart videos</Heading>
-        <Text fontSize="large">
-          Follow along with these videos to learn how to set up your local
-          environment to get started making open source contributions to the
-          Amplify project.
-        </Text>
-      </View>
-      <Flex wrap={'wrap'} justifyContent={'center'} width="100%" gap="2em">
-        <View width={{ base: '90%', large: '40%' }}>
-          <YoutubeEmbed embedId="8BUSqSkhqtw" width="600" height="350" />
-        </View>
-        <View width={{ base: '90%', large: '40%' }}>
-          <YoutubeEmbed embedId="WMKVE98hEzE" width="600" height="350" />
-        </View>
-      </Flex>
-    </Flex>
-  );
+  return <></>;
 }
