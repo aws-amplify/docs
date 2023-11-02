ed6c0bc85c7a4005cd731ac4f35df6d375410b86
Heather <hbuchel@gmail.com>
2023-11-02 12:00:48 -0400

    open gen2 banner in new tab

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -33,6 +33,8 @@ export const Banner: React.FC<BannerProps> = ({ url = '/gen2' }) => {
             href={url}
             size="small"
             gap="small"
+            target="_blank"
+            rel="noopener noreferrer"
             className="message-banner__button"
           >
             Go to Amplify IfC docs{' '}
