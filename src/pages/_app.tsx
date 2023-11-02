84b1046e8f263ea924a45f0901ec2b1c2c271523
timngyn <timngyn@gmail.com>
2023-11-02 15:16:41 -0700

    Listen to 'routeChangeComplete' event from nextjs to call a new page view event

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { MDXProvider } from '@mdx-js/react';
 import { Layout } from '@/components/Layout';
 import { CANONICAL_URLS } from '@/data/canonical-urls';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { trackPageVisit } from '../utils/track';
 
 function MyApp({ Component, pageProps }) {
   const {
@@ -18,6 +20,21 @@ function MyApp({ Component, pageProps }) {
     showLastUpdatedDate,
     useCustomTitle
   } = pageProps;
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      trackPageVisit();
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
   const getLayout =
     Component.getLayout ||
     ((page) => (
@@ -37,7 +54,6 @@ function MyApp({ Component, pageProps }) {
       </Layout>
     ));
 
-  const router = useRouter();
   let canonicalUrl = 'https://docs.amplify.aws';
   const canonicalPath = CANONICAL_URLS.includes(router.pathname)
     ? router.pathname.replace('[platform]', 'javascript')
