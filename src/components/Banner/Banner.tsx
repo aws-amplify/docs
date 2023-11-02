a19189418743f1359b8feb4f016918882b0f1e0b
Heather <hbuchel@gmail.com>
2023-11-02 12:19:29 -0400

    clean up cli pages a little, update gen2 banner

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -22,9 +22,8 @@ export const Banner: React.FC<BannerProps> = ({ url = '/gen2' }) => {
               The next generation of backend building with Amplify!
             </Text>
             <Text className="message-banner__content">
-              Interested in exploring Amplify Infrastructure from Code (Amplify
-              IfC)? Visit the documentation for our reimagined experience for
-              web developers.
+              Now in preview: Infrastructure from code capabilities in a
+              powerful new code-first DX for building fullstack apps
             </Text>
           </Flex>
 
@@ -37,7 +36,7 @@ export const Banner: React.FC<BannerProps> = ({ url = '/gen2' }) => {
             rel="noopener noreferrer"
             className="message-banner__button"
           >
-            Go to Amplify IfC docs{' '}
+            Go to Amplify Gen 2 docs
             <IconChevron className="icon-rotate-270" fontSize=".875em" />
           </Button>
         </Flex>
