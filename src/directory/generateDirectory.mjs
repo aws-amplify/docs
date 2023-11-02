af9fc13cbb72e20870e0a555324c7206b7a78aa8
jacoblogan <jacob.maiola.logan@gmail.com>
2023-11-02 06:21:58 -0700

    update directory and fill in missing files (#6160)
    * update directory and fill in missing files
    
    * add pages
    
    * update sitemap script
    
    * update cli/custom
    
    * update cli/custom
    
    ---------
    
    Co-authored-by: Jacob Logan <lognjc@amazon.com>

--- src/directory/generateDirectory.mjs
+++ src/directory/generateDirectory.mjs
@@ -23,7 +23,8 @@ async function getMetaStringObj(filePath) {
     try {
       // Using JSON5 because the meta object is a "relaxed" JSON
       // JSON5 can parse the meta object without needing quotes around the object keys
-      const result = JSON5.parse(match[1]);
+      let metaObj = match[1].replaceAll('`',"'");
+      const result = JSON5.parse(metaObj);
 
       return result;
     } catch (err) {
