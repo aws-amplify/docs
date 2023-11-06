090d1d0c82a3e10e6630de8dd12afbecad1903de
Jacob Logan <lognjc@amazon.com>
2023-11-06 15:24:07 -0700

    add tracking event to version switcher and make them tabable

--- src/components/VersionSwitcher/index.tsx
+++ src/components/VersionSwitcher/index.tsx
@@ -1,10 +1,11 @@
-import { Flex } from '@aws-amplify/ui-react';
+import { Flex, View } from '@aws-amplify/ui-react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import flatDirectory from 'src/directory/flatDirectory.json';
 import { IconCheck } from '@/components/Icons';
 import { PLATFORM_VERSIONS } from '@/data/platforms';
 import classNames from 'classnames';
+import { trackVersionChange } from '@/utils/track';
 
 
 const findRoute = (platform, isPrev) => {
@@ -38,14 +39,24 @@ export const VersionSwitcher = ({ platform, isPrev, ...rest }) => {
         }
     }
 
+    const fireTrackEvent = (prevVersion: boolean) => {
+        if (prevVersion !== isPrev) {
+            trackVersionChange(prevVersion);
+        }
+    }
+
     return <Flex className="version-switcher" {...rest}>
-        <Link href={isPrev ? activeHref : inactiveHref} className={classNames("version-switcher__link", { "active": isPrev })}>
-            {isPrev && <IconCheck fontSize="xl" />}
-            {versions.prev}
+        <Link href={isPrev ? activeHref : inactiveHref} onClick={() => fireTrackEvent(true)} className="version-switcher__link">
+            <View tabIndex={0} className={classNames("version-switcher__wrapper", { "active": isPrev })}>
+                {isPrev && <IconCheck fontSize="xl" />}
+                {versions.prev}
+            </View>
         </Link>
-        <Link href={!isPrev ? activeHref : inactiveHref} className={classNames("version-switcher__link", { "active": !isPrev })}>
-            {!isPrev && <IconCheck fontSize="xl" />}
-            {versions.current}
+        <Link href={!isPrev ? activeHref : inactiveHref} className={classNames("version-switcher__link")} onClick={() => fireTrackEvent(false)}>
+            <View tabIndex={0} className={classNames("version-switcher__wrapper", { "active": !isPrev })}>
+                {!isPrev && <IconCheck fontSize="xl" />}
+                {versions.current}
+            </View>
         </Link>
     </Flex>
 }
