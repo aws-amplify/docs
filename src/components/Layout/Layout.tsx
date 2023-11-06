e4641583d6d1bbc143eebed9ffdf49257ec19819
Tim Nguyen <54393192+timngyn@users.noreply.github.com>
2023-11-06 14:10:05 -0800

    Update when trackPageVisit gets called (#6180)
    * Listen to 'routeChangeComplete' event from nextjs to call a new page view event
    
    * fix: merge issue
    
    * Remove firstPageOfVisit variable since we're listening to route changes now

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -27,7 +27,6 @@ import { SpaceShip } from '@/components/SpaceShip';
 import SearchBar from '@/components/SearchBar';
 import { IconMenu, IconDoubleChevron } from '@/components/Icons';
 import { LEFT_NAV_LINKS, RIGHT_NAV_LINKS } from '@/utils/globalnav';
-import { trackPageVisit } from '../../utils/track';
 import { Menu } from '@/components/Menu';
 import { LayoutProvider } from '@/components/Layout';
 import { TableOfContents } from '@/components/TableOfContents';
@@ -68,10 +67,6 @@ export const Layout = ({
   const [tocHeadings, setTocHeadings] = useState<HeadingInterface[]>([]);
   const menuButtonRef = useRef<HTMLButtonElement>(null);
   const sidebarMenuButtonRef = useRef<HTMLButtonElement>(null);
-
-  useEffect(() => {
-    trackPageVisit();
-  }, []);
 
   useEffect(() => {
     const headings: HeadingInterface[] = [];
