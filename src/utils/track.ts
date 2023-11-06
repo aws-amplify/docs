fa4053d88e77aa2eab5d50cc0a82711cfd0d23f9
timngyn <timngyn@gmail.com>
2023-11-06 13:30:53 -0800

    Remove firstPageOfVisit variable since we're listening to route changes now

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,5 +1,4 @@
 let configured = false;
-let firstPageOfVisit = true;
 let AWSCShortbread;
 let s;
 let AWSMA;
@@ -68,16 +67,11 @@ type AnalyticsEvent =
   | AnalyticsEventPageDataFetchException;
 
 export const trackPageVisit = (): void => {
-  if (
-    typeof window !== 'undefined' &&
-    typeof s != 'undefined' &&
-    !firstPageOfVisit
-  ) {
+  if (typeof window !== 'undefined' && typeof s != 'undefined') {
     s.pageName = window.location.href;
     s.pageURL = window.location.href;
     s.t();
   }
-  firstPageOfVisit = false;
 };
 
 export const trackPageFetchException = (): void => {
