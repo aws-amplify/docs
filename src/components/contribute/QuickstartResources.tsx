6df4d12ccd77a97f45ef1388f044f4e6c7983f46
Heather <hbuchel@gmail.com>
2023-11-08 12:05:35 -0500

    remove unneeded component, remove stray empty line deletions

--- src/components/contribute/QuickstartResources.tsx
+++
@@ -1,6 +0,0 @@
-import { Heading, View, Text, Flex } from '@aws-amplify/ui-react';
-import { YoutubeEmbed } from '../YoutubeEmbed';
-
-export default function QuickstartResources() {
-  return <></>;
-}
