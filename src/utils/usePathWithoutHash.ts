1b4afdbbc8feeaba713d414590069224ad8d2908
timngyn <timngyn@gmail.com>
2023-11-08 11:14:38 -0800

    Change back to older version of custom hook since useRouter is already listening to route changes

--- src/utils/usePathWithoutHash.ts
+++ src/utils/usePathWithoutHash.ts
@@ -1,14 +1,10 @@
-import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export function usePathWithoutHash() {
-  const [path, setPath] = useState<string>('');
   const router = useRouter();
 
-  useEffect(() => {
-    // Get everything before the hashmark (#)
-    setPath(router.asPath.split('#')[0]);
-  }, [router.asPath]);
+  // Get everything before the hashmark (#)
+  const path = router.asPath.split('#')[0];
 
   return path;
 }
