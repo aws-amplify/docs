f860b50817bf79dffce7a91966f267b491c637c8
timngyn <timngyn@gmail.com>
2023-11-08 10:51:32 -0800

    Don't setstate during render

--- src/utils/usePathWithoutHash.ts
+++ src/utils/usePathWithoutHash.ts
@@ -1,10 +1,22 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
 export function usePathWithoutHash() {
+  const [path, setPath] = useState<string>('');
   const router = useRouter();
 
-  // Get everything before the hashmark (#)
-  const path = router.asPath.split('#')[0];
+  useEffect(() => {
+    const onRouteChangeComplete = () => {
+      // Get everything before the hashmark (#)
+      setPath(router.asPath.split('#')[0]);
+    };
+
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, [router.asPath, router.events]);
 
   return path;
 }
