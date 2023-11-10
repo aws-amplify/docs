f1961fbcc46bf17c9eddcb7fa16372dff8c50721
timngyn <timngyn@gmail.com>
2023-11-10 13:57:12 -0800

    Replace [platform] with current platform

--- src/components/MDXComponents/MDXLink.tsx
+++ src/components/MDXComponents/MDXLink.tsx
@@ -1,11 +1,14 @@
 import Link from 'next/link';
 import ExternalLink from '@/components/ExternalLink';
+import { useCurrentPlatform } from '@/utils/useCurrentPlatform';
 
 export const MDXLink = (props) => {
   const { href, children } = props;
   const isInternal = href && (href.startsWith('/') || href.startsWith('#'));
+  const currentPlatform = useCurrentPlatform();
+  const updatedHref = href.replace('%5Bplatform%5D', currentPlatform);
   return isInternal ? (
-    <Link href={href}>{children}</Link>
+    <Link href={updatedHref}>{children}</Link>
   ) : (
     <ExternalLink href={href}>{children}</ExternalLink>
   );
