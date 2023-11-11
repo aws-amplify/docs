baf6b539ccd55d814014315ebaad11ac0b1d5a97
timngyn <timngyn@gmail.com>
2023-11-10 19:49:21 -0800

    Use decodeURI

--- src/components/MDXComponents/MDXLink.tsx
+++ src/components/MDXComponents/MDXLink.tsx
@@ -5,10 +5,16 @@ import { useCurrentPlatform } from '@/utils/useCurrentPlatform';
 export const MDXLink = (props) => {
   const { href, children } = props;
   const isInternal = href && (href.startsWith('/') || href.startsWith('#'));
-  const currentPlatform = useCurrentPlatform();
-  const updatedHref = href.replace('%5Bplatform%5D', currentPlatform);
+
   return isInternal ? (
-    <Link href={updatedHref}>{children}</Link>
+    <Link
+      href={{
+        pathname: decodeURI(href),
+        query: { platform: useCurrentPlatform() }
+      }}
+    >
+      {children}
+    </Link>
   ) : (
     <ExternalLink href={href}>{children}</ExternalLink>
   );
