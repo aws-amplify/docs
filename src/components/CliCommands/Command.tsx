2633712fe34f2ff00637f5a839056d71ef61a0c1
Heather <hbuchel@gmail.com>
2023-11-14 16:38:31 -0500

    Update commands page so its a little easier to scan

--- src/components/CliCommands/Command.tsx
+++ src/components/CliCommands/Command.tsx
@@ -15,33 +15,49 @@ export function Command({
     <Flex className="commands-list__command">
       <CommandHeading>{name}</CommandHeading>
       <Text>{description}</Text>
+
       <MDXCode language="terminal" codeString={usage} showLineNumbers={false} />
+
       {flags && flags.length > 0 && (
         <Flex className="commands-list__command__flags">
-          {flags.map((flag: CliCommandFlag) => (
-            <Fragment key={`${name}-${flag.short}`}>
-              <code className="commands-list__command__flags__code">
-                {flag.short && <span>-{flag.short}|</span>}
-                <span>--{flag.long}</span>
-              </code>
-              <Text>{flag.description}</Text>
-            </Fragment>
-          ))}
+          <table>
+            <thead>
+              <tr>
+                <th>Flag</th>
+                <th>Description</th>
+              </tr>
+            </thead>
+            <tbody>
+              {flags.map((flag: CliCommandFlag) => (
+                <tr key={`${name}-${flag.short}`}>
+                  <td>
+                    <code>
+                      {flag.short && <span>-{flag.short}|</span>}
+                      <span>--{flag.long}</span>
+                    </code>
+                  </td>
+                  <td>{flag.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </Flex>
       )}
       {subCommands && subCommands.length > 0 && (
-        <Flex className="commands-list__command__subcommands">
+        <Flex
+          className="commands-list__command__subcommands"
+          alignItems="flex-start"
+          gap="small"
+        >
           {subCommands.map((subCommand) => (
             <Fragment key={`${name}-${subCommand.name}`}>
               <SubCommandHeading parentCommand={name}>
                 {subCommand.name}
               </SubCommandHeading>
               <Text>{subCommand.description}</Text>
-              <MDXCode
-                language="terminal"
-                codeString={`amplify ${name} ${subCommand.name}`}
-                showLineNumbers={false}
-              />
+              <code>
+                amplify {name} {subCommand.name}
+              </code>
             </Fragment>
           ))}
         </Flex>
