73d3e698c6960e2ac34c0e64f59564620996753a
Heather <hbuchel@gmail.com>
2023-12-18 13:05:51 -0500

    dark mode color updates

---
+++ src/themes/colors.ts
@@ -0,0 +1,36 @@
+import { defaultTheme } from '@aws-amplify/ui';
+
+export const red = defaultTheme.tokens.colors.red;
+export const orange = defaultTheme.tokens.colors.orange;
+export const yellow = defaultTheme.tokens.colors.yellow;
+export const neutral = defaultTheme.tokens.colors.neutral;
+
+export const blue = {
+  10: { value: 'hsl(207, 80%, 90%)' },
+  20: { value: 'hsl(220, 50%, 85%)' },
+  40: { value: 'hsl(220, 50%, 70%)' },
+  60: { value: 'hsl(220, 50%, 50%)' },
+  80: { value: 'hsl(220, 50%, 30%)' },
+  90: { value: 'hsl(220, 50%, 20%)' },
+  100: { value: 'hsl(220, 50%, 15%)' }
+};
+
+export const purple = {
+  10: { value: 'hsl(252, 100%, 98%)' },
+  20: { value: 'hsl(258, 41%, 85%)' },
+  40: { value: 'hsl(258, 41%, 70%)' },
+  60: { value: 'hsl(258, 41%, 50%)' },
+  80: { value: 'hsl(258, 41%, 30%)' },
+  90: { value: 'hsl(258, 41%, 24%)' },
+  100: { value: 'hsl(258, 41%, 15%)' }
+};
+
+export const teal = {
+  10: { value: 'hsl(175, 44%, 96%)' },
+  20: { value: 'hsl(175, 57%, 80%)' },
+  40: { value: 'hsl(175, 57%, 70%)' },
+  60: { value: 'hsl(175, 57%, 40%)' },
+  80: { value: 'hsl(181, 69%, 28%)' },
+  90: { value: 'hsl(181, 69%, 21%)' },
+  100: { value: 'hsl(181, 69%, 15%)' }
+};
