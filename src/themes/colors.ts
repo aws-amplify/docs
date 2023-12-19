3ec68dff3a6819f31fe0763d8e0952ffdaf15eea
Heather <hbuchel@gmail.com>
2023-12-19 11:30:20 -0500

    clean up code styles, fix darkmode code block focus

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,9 +1,19 @@
 import { defaultTheme } from '@aws-amplify/ui';
 
 export const red = defaultTheme.tokens.colors.red;
-export const orange = defaultTheme.tokens.colors.orange;
+// export const orange = defaultTheme.tokens.colors.orange;
 export const yellow = defaultTheme.tokens.colors.yellow;
 export const neutral = defaultTheme.tokens.colors.neutral;
+
+export const orange = {
+  10: { value: 'hsl(30, 75%, 95%)' },
+  20: { value: 'hsl(30, 75%, 85%)' },
+  40: { value: 'hsl(30, 75%, 75%)' },
+  60: { value: 'hsl(30, 50%, 50%)' },
+  80: { value: 'hsl(30, 50%, 30%)' },
+  90: { value: 'hsl(30, 60%, 20%)' },
+  100: { value: 'hsl(30, 100%, 15%)' }
+};
 
 export const blue = {
   10: { value: 'hsl(207, 80%, 90%)' },
