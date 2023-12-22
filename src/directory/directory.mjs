ffe95a87629c0091b4862a16bc78a09b99348118
Jim Eagan <84857865+hibler13@users.noreply.github.com>
2023-12-22 11:28:55 -0800

    Update directory.mjs

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -470,10 +470,7 @@ export const directory = {
               children: [
                 {
                   path: 'src/pages/[platform]/build-a-backend/existing-resources/cli/index.mdx'
-                }
-                // {
-                //  path: 'src/pages/[platform]/build-a-backend/existing-resources/cdk/index.mdx'
-                //}
+                }     
               ]
             },
             {
