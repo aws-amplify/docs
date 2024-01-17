a3aaef100a7c81a5922f4b10bb2be779ac4576a9
jacoblogan <jacob.maiola.logan@gmail.com>
2024-01-17 13:13:52 -0700

    Update src/protected/ProtectedAccountMessage/index.tsx
    Co-authored-by: Scott Rees <6165315+reesscot@users.noreply.github.com>

--- src/protected/ProtectedAccountMessage/index.tsx
+++ src/protected/ProtectedAccountMessage/index.tsx
@@ -1,5 +1,6 @@
 import { Callout } from '@/components/Callout';
 
+// WARNING: The messaging in this component should NOT be changed without the appropriate approvals
 export const ProtectedAccountMessage = () => (
   <Callout warning>
     We recommend operating Amplify workloads in dedicated accounts so IAM
