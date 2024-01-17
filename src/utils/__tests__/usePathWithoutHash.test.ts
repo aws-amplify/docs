9b69cf8b6235c821393d981d6735681c8661e889
timngyn <timngyn@gmail.com>
2024-01-17 15:12:45 -0800

    Add tests for usePathWithoutHash

---
+++ src/utils/__tests__/usePathWithoutHash.test.ts
@@ -0,0 +1,30 @@
+import { usePathWithoutHash } from '../usePathWithoutHash';
+import { renderHook } from '@testing-library/react';
+
+const routerMock = {
+  __esModule: true,
+  useRouter: () => {
+    return {
+      asPath: ''
+    };
+  }
+};
+
+jest.mock('next/router', () => routerMock);
+
+describe('usePathWithoutHash', () => {
+  it('should remove the hashmark and everything after it from the url', () => {
+    routerMock.useRouter = () => {
+      return {
+        asPath:
+          'https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/#add-your-first-record'
+      };
+    };
+
+    const { result } = renderHook(() => usePathWithoutHash());
+
+    expect(result.current).toEqual(
+      'https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/'
+    );
+  });
+});
