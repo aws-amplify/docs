836f27d787d129159a5643a32b6ea3bf9d2fe8ca
timngyn <timngyn@gmail.com>
2024-01-17 15:19:47 -0800

    Add test to check that url is not modified if it doesn't have a hashmark

--- src/utils/__tests__/usePathWithoutHash.test.ts
+++ src/utils/__tests__/usePathWithoutHash.test.ts
@@ -27,4 +27,19 @@ describe('usePathWithoutHash', () => {
       'https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/'
     );
   });
+
+  it('should not modify url if there are no hashmarks', () => {
+    routerMock.useRouter = () => {
+      return {
+        asPath:
+          'https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/'
+      };
+    };
+
+    const { result } = renderHook(() => usePathWithoutHash());
+
+    expect(result.current).toEqual(
+      'https://docs.amplify.aws/react/build-a-backend/graphqlapi/set-up-graphql-api/'
+    );
+  });
 });
