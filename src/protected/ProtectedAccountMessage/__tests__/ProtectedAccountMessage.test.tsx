5795f36d454b7a449978329c6ab1d8c2be48a3b7
Jacob Logan <lognjc@amazon.com>
2024-01-19 09:35:02 -0700

    add comments to ProtectedAccountMessage component

--- src/protected/ProtectedAccountMessage/__tests__/ProtectedAccountMessage.test.tsx
+++ src/protected/ProtectedAccountMessage/__tests__/ProtectedAccountMessage.test.tsx
@@ -9,6 +9,10 @@ const ADMINUI_START_PAGE_PATH =
 const PROTECTED_MESSAGE = `We recommend operating Amplify workloads in dedicated accounts so IAM principals not working with Amplify do not manipulate provisioned resources out-of-band.`;
 
 describe('Protected Account Message', () => {
+  /*
+        This test is to ensure that the ProtectedAccountMessage component appears on the Admin
+        UI Start page and cannot be removed or modified without approval.
+    */
   it('should render ProtectedAccountMessage component on the Admin UI Start page', async () => {
     const pageData = fs.readFileSync(ADMINUI_START_PAGE_PATH, {
       encoding: 'utf8'
@@ -16,6 +20,10 @@ describe('Protected Account Message', () => {
     expect(pageData).toMatch(/<ProtectedAccountMessage \/>/);
   });
 
+  /*
+      This test is to ensure that the messaging on the ProtectedAccountMessage component does not change
+      and cannot be removed or modified without approval.
+    */
   it('should render the protected message', async () => {
     render(<ProtectedAccountMessage />);
 
