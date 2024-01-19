df07b2df2caddbad15b257d6a59145a0a6cb9466
Jacob Logan <lognjc@amazon.com>
2024-01-19 09:22:44 -0700

    add test to ensure protected messaging hasn't changed

--- src/protected/ProtectedAccountMessage/__tests__/ProtectedAccountMessage.test.tsx
+++ src/protected/ProtectedAccountMessage/__tests__/ProtectedAccountMessage.test.tsx
@@ -1,7 +1,12 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { ProtectedAccountMessage } from '../index';
 import fs from 'fs';
 
 const ADMINUI_START_PAGE_PATH =
   'src/pages/[platform]/tools/console/adminui/start/index.mdx';
+
+const PROTECTED_MESSAGE = `We recommend operating Amplify workloads in dedicated accounts so IAM principals not working with Amplify do not manipulate provisioned resources out-of-band.`;
 
 describe('Protected Account Message', () => {
   it('should render ProtectedAccountMessage component on the Admin UI Start page', async () => {
@@ -9,5 +14,12 @@ describe('Protected Account Message', () => {
       encoding: 'utf8'
     });
     expect(pageData).toMatch(/<ProtectedAccountMessage \/>/);
+  });
+
+  it('should render the protected message', async () => {
+    render(<ProtectedAccountMessage />);
+
+    const protectedNode = await screen.findByText(PROTECTED_MESSAGE);
+    expect(protectedNode).toBeInTheDocument();
   });
 });
