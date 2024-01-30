75a44deb5a7158f85309113f5591e2912da00ffd
Heather <hbuchel@gmail.com>
2024-01-30 10:56:42 -0500

    add invalid meta object and missing page test

--- src/directory/__tests__/getMetaStringObj.test.js
+++ src/directory/__tests__/getMetaStringObj.test.js
@@ -3,7 +3,7 @@ import JSON5 from 'json5';
 
 describe('getMetaStringObj', () => {
   it('should return meta object for a valid page file path with valid meta object', async () => {
-    const filePath = './src/directory/__tests__/helpers/valid-meta-page.mdx';
+    const filePath = './src/directory/__tests__/helpers/valid-meta.mdx';
     const expectedMetaObject = JSON5.parse(
       `{"title":"Build & connect backend","description":"Learn more about how you can build the backend for your app or connect with existing resources.","platforms":["android","angular","flutter","javascript","nextjs","react","react-native","swift","vue"],"route":"/[platform]/build-a-backend","canonicalObjects":[{"platforms":["vue","javascript","react","angular","nextjs"],"canonicalPath":"/javascript/build-a-backend/"}]}`
     );
@@ -13,19 +13,27 @@ describe('getMetaStringObj', () => {
     expect(results).toEqual(expectedMetaObject);
   });
 
-  it('should throw a MissingMetaError for page with missing meta object ', async () => {
-    const filePath = './src/directory/__tests__/helpers/missing-meta-page.mdx';
+  it('should throw an error for page with missing meta object', async () => {
+    const filePath = './src/directory/__tests__/helpers/missing-meta.mdx';
 
     expect(async () => {
       await getMetaStringObj(filePath);
     }).rejects.toThrow();
   });
 
-  // it('should throw an InvalidMetaError for page with invalid meta object ', async () => {
-  //   const filePath = './src/directory/__tests__/helpers/invalid-meta-page.mdx';
+  it('should throw an error for page with invalid meta object', async () => {
+    const filePath = './src/directory/__tests__/helpers/missing-semicolon.mdx';
 
-  //   expect(async () => {
-  //     await getMetaStringObj(filePath);
-  //   }).rejects.toThrow();
-  // });
+    expect(async () => {
+      await getMetaStringObj(filePath);
+    }).rejects.toThrow();
+  });
+
+  it('should throw an error for page that does not exist', async () => {
+    const filePath = './src/directory/__tests__/helpers/missing-page.mdx';
+
+    expect(async () => {
+      await getMetaStringObj(filePath);
+    }).rejects.toThrow();
+  });
 });
