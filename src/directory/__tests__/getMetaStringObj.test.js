dca6da34eeb7f84fd91f95c43884568ded98acb3
Heather <hbuchel@gmail.com>
2024-01-30 12:44:34 -0500

    clean up test

--- src/directory/__tests__/getMetaStringObj.test.js
+++ src/directory/__tests__/getMetaStringObj.test.js
@@ -1,12 +1,31 @@
 import { getMetaStringObj } from '../getMetaStringObj.mjs';
-import JSON5 from 'json5';
 
 describe('getMetaStringObj', () => {
   it('should return meta object for a valid page file path with valid meta object', async () => {
     const filePath = './src/directory/__tests__/helpers/valid-meta.mdx';
-    const expectedMetaObject = JSON5.parse(
-      `{"title":"Build & connect backend","description":"Learn more about how you can build the backend for your app or connect with existing resources.","platforms":["android","angular","flutter","javascript","nextjs","react","react-native","swift","vue"],"route":"/[platform]/build-a-backend","canonicalObjects":[{"platforms":["vue","javascript","react","angular","nextjs"],"canonicalPath":"/javascript/build-a-backend/"}]}`
-    );
+    const expectedMetaObject = {
+      title: 'Build & connect backend',
+      description:
+        'Learn more about how you can build the backend for your app or connect with existing resources.',
+      platforms: [
+        'android',
+        'angular',
+        'flutter',
+        'javascript',
+        'nextjs',
+        'react',
+        'react-native',
+        'swift',
+        'vue'
+      ],
+      route: '/[platform]/build-a-backend',
+      canonicalObjects: [
+        {
+          platforms: ['vue', 'javascript', 'react', 'angular', 'nextjs'],
+          canonicalPath: '/javascript/build-a-backend/'
+        }
+      ]
+    };
 
     const results = await getMetaStringObj(filePath);
 
