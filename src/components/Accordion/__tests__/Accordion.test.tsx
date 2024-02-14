fa8e84d4adb0278cd9f83f9ad3deb9e80a78d222
katiegoines <katiegoines@gmail.com>
2024-02-14 15:51:16 -0800

    issue with window properties

--- src/components/Accordion/__tests__/Accordion.test.tsx
+++ src/components/Accordion/__tests__/Accordion.test.tsx
@@ -1,20 +1,54 @@
 import * as React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { Accordion } from '../index';
+import * as trackModule from '../../../utils/track';
 
 describe('Accordion', () => {
-  const accordion = (
+  const content =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla malesuada dignissim erat et lacinia. Quisque molestie vehicula dolor sit amet volutpat. Quisque eget orci quis mi sodales fringilla.';
+  const component = (
     <Accordion
-      title="Accordion example"
+      title="Accordion component example"
       headingLevel="4"
       eyebrow="Learn more"
-    ></Accordion>
+    >
+      <p>{content}</p>
+    </Accordion>
   );
 
-  it('should render the Accordion component', async () => {
-    render(accordion);
+  jest.mock('../../../utils/track', () => ({
+    trackExpanderOpen: jest
+      .fn()
+      .mockImplementation(() => 'accordion-component-example')
+  }));
 
-    const blockNode = await screen.findByText('Block Child');
-    expect(blockNode).toBeInTheDocument();
+  it('should render the Accordion component', async () => {
+    render(component);
+    const accordion = await screen.findByText(content);
+    expect(accordion).toBeInTheDocument();
+  });
+
+  it('should hide the Accordion body content on load', async () => {
+    render(component);
+    const bodyText = await screen.findByText(content);
+    expect(bodyText).not.toBeVisible();
+  });
+
+  it('should track Accordion open on click of heading', async () => {
+    jest.spyOn(trackModule, 'trackExpanderOpen');
+    render(component);
+
+    // const accordionBody = await screen.findByText(content);
+    // expect(accordionBody).toBeInTheDocument();
+    // expect(accordionBody).not.toBeVisible();
+
+    const accordionHeading = await screen.findByText(
+      'Accordion component example'
+    );
+    userEvent.click(accordionHeading);
+    await waitFor(() => {
+      expect(trackModule.trackExpanderOpen).toHaveBeenCalled();
+    });
   });
 });
