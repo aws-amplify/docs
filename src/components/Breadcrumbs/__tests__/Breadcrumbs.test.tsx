629bcbd8b65e9f23327651decb6b1e04dc61652f
katiegoines <katiegoines@gmail.com>
2024-02-19 15:13:11 -0800

    add tests for override values

--- src/components/Breadcrumbs/__tests__/Breadcrumbs.test.tsx
+++ src/components/Breadcrumbs/__tests__/Breadcrumbs.test.tsx
@@ -42,6 +42,45 @@ describe('Breadcrumbs', () => {
     }
   });
 
+  it('should replace "prev" with applicable version in Breadcrumbs text', async () => {
+    const component = (
+      <Breadcrumbs
+        route={'/javascript/prev/build-a-backend/auth/set-up-auth/'}
+      />
+    );
+    render(component);
+    const routeList = component.props.route.split('/').filter(function (el) {
+      return el != '';
+    });
+    const breadcrumbsNode = await screen.findByLabelText('Breadcrumb');
+
+    const link = breadcrumbsNode
+      .getElementsByClassName('amplify-breadcrumbs__link')
+      .item(1);
+
+    if (
+      link?.getAttribute('href')?.includes('prev') &&
+      (routeList[0] == 'javascript' ||
+        routeList[0] == 'react' ||
+        routeList[0] == 'react-native' ||
+        routeList[0] == 'angular' ||
+        routeList[0] == 'nextjs' ||
+        routeList[0] == 'vue')
+    ) {
+      expect(link.textContent).toEqual('V5');
+    } else if (
+      link?.getAttribute('href')?.includes('prev') &&
+      (routeList[0] == 'swift' || routeList[0] == 'android')
+    ) {
+      expect(link.textContent).toEqual('V1');
+    } else if (
+      link?.getAttribute('href')?.includes('prev') &&
+      routeList[0] == 'flutter'
+    ) {
+      expect(link.textContent).toEqual('V0');
+    }
+  });
+
   it('should render links for Breadcrumbs for gen2', async () => {
     const component = (
       <Breadcrumbs route={'/gen2/build-a-backend/auth/set-up-auth/'} />
