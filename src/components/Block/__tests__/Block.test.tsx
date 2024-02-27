3158023f6b5da24f2214ea6eddf2a1b3a09ba5b5
katiegoines <katiegoines@gmail.com>
2024-02-27 12:31:31 -0800

    chore: add addtl unit test for Block component

--- src/components/Block/__tests__/Block.test.tsx
+++ src/components/Block/__tests__/Block.test.tsx
@@ -3,11 +3,17 @@ import { render, screen } from '@testing-library/react';
 import Block from '../index';
 
 describe('Block', () => {
+  const child = <div>Block Child</div>;
+  const component = <Block name={`test-block`}>{child}</Block>;
   it('should render the Block component', async () => {
-    const child = <div>Block Child</div>;
-    render(<Block name={`test-block`}>{child}</Block>);
-
+    render(component);
     const blockNode = await screen.findByText('Block Child');
     expect(blockNode).toBeInTheDocument();
+  });
+
+  it('should display the Block title', async () => {
+    render(component);
+    const blockNode = await screen.findByText('Block Child');
+    expect(blockNode.parentElement?.getAttribute('name')).toEqual('test-block');
   });
 });
