eaccc04e2fb653e06fddca1ea8a8f8c73750809b
katiegoines <katiegoines@gmail.com>
2024-03-04 07:54:45 -0800

    chore: add unit test for Overview component

---
+++ src/components/Overview/__tests__/Overview.test.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Overview } from '../index';
+import { getChildPageNodes } from '@/utils/getChildPageNodes';
+
+const routerMock = {
+  __esModule: true,
+  useRouter: () => {
+    return {
+      query: { platform: 'react' }
+    };
+  }
+};
+
+jest.mock('next/router', () => routerMock);
+
+describe('Overview', () => {
+  const childPageNodes = getChildPageNodes('/[platform]/build-a-backend');
+  const component = <Overview childPageNodes={childPageNodes} />;
+
+  it('should render the Overview component', async () => {
+    render(component);
+    const overviewNode = await screen.findAllByRole('link');
+    const sampleOverviewCard = overviewNode[0];
+    childPageNodes
+      ? expect(sampleOverviewCard).toBeInTheDocument()
+      : expect(sampleOverviewCard).not.toBeInTheDocument();
+    expect(sampleOverviewCard.textContent).toContain('Authentication');
+  });
+});
