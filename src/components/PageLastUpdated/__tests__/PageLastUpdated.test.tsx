649fb20c2b635617d4bf66b98344aa7be9882e72
katiegoines <katiegoines@gmail.com>
2024-03-05 12:55:11 -0800

    test cleanup

--- src/components/PageLastUpdated/__tests__/PageLastUpdated.test.tsx
+++ src/components/PageLastUpdated/__tests__/PageLastUpdated.test.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { PageLastUpdated } from '../index';
 import flatDirectory from '../../../directory/flatDirectory.json';
 
 describe('PageLastUpdated', () => {
   const directoryData = flatDirectory['/[platform]/how-amplify-works'];
   const component = <PageLastUpdated directoryData={directoryData} />;
+
   it('should render the PageLastUpdated component', async () => {
     render(component);
 
@@ -15,7 +16,16 @@ describe('PageLastUpdated', () => {
 
   it('should show the last updated date', async () => {
     render(component);
-    const lastUpdated = document.querySelector('.page-last-updated');
+    const date = new Date(directoryData.lastUpdated).toLocaleDateString(
+      'en-us',
+      {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+      }
+    );
+
+    const lastUpdated = await screen.queryByText(`Page updated ${date}`);
     expect(lastUpdated).not.toBeNull();
   });
 });
