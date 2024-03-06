c6bd9579e7cde7530a54750cbb04cbe16353cfa4
Heather <hbuchel@gmail.com>
2024-03-06 16:56:37 -0500

    updates

--- src/components/GenSwitcher/GenSwitcher.tsx
+++ src/components/GenSwitcher/GenSwitcher.tsx
@@ -12,6 +12,10 @@ export const GenSwitcher = ({ isGen1 }: GenSwitcherProps) => {
         <Popover.Trigger size="small" className="gen-switcher__trigger">
           {isGen1 ? 'Gen1' : 'Gen2'}
         </Popover.Trigger>
+        <Popover.List anchor="left" className="gen-switcher__list">
+          <Popover.ListItem href="/gen1">Gen 1</Popover.ListItem>
+          <Popover.ListItem href="/gen2">Gen 2</Popover.ListItem>
+        </Popover.List>
       </Popover>
     </View>
   );
