d02589ba0faeed3d2a3c888d2331334b76291520
Heather <hbuchel@gmail.com>
2024-03-12 12:50:48 -0400

    update styles, update tests

--- src/components/GenSwitcher/GenSwitcher.tsx
+++ src/components/GenSwitcher/GenSwitcher.tsx
@@ -1,14 +1,16 @@
-import { View, ViewProps } from '@aws-amplify/ui-react';
-import { Popover } from '@/components/Popover';
+import { View, ViewProps, VisuallyHidden } from '@aws-amplify/ui-react';
 import classNames from 'classnames';
+import { IconCheck } from '@/components/Icons';
+import { Popover } from '@/components/Popover';
 
 interface GenSwitcherProps extends ViewProps {
   isGen1?: boolean;
+  testId?: string;
 }
 
-export const GenSwitcher = ({ isGen1 }: GenSwitcherProps) => {
+export const GenSwitcher = ({ isGen1, testId }: GenSwitcherProps) => {
   return (
-    <View className="gen-switcher">
+    <View className="gen-switcher" testId={testId}>
       <Popover>
         <Popover.Trigger
           size="small"
@@ -16,14 +18,19 @@ export const GenSwitcher = ({ isGen1 }: GenSwitcherProps) => {
             'gen-switcher__trigger--gen1': isGen1
           })}
         >
-          {isGen1 ? 'Gen1' : 'Gen2'}
+          {isGen1 ? 'Gen 1' : 'Gen 2'}
+          <VisuallyHidden>Open Amplify generation navigation</VisuallyHidden>
         </Popover.Trigger>
-        <Popover.List anchor="left" className="gen-switcher__list">
+        <Popover.List
+          ariaLabel="Amplify generation navigation"
+          anchor="left"
+          className="gen-switcher__list"
+        >
           <Popover.ListItem href="/" current={!isGen1}>
-            Gen 2
+            Gen 2 {isGen1 ? '' : <IconCheck className="gen-switcher__check" />}
           </Popover.ListItem>
           <Popover.ListItem href="/gen1" current={isGen1}>
-            Gen 1
+            Gen 1 {isGen1 ? <IconCheck className="gen-switcher__check" /> : ''}
           </Popover.ListItem>
         </Popover.List>
       </Popover>
---
+++ src/components/GenSwitcher/__tests__/GenSwitcher.test.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { render, screen, within } from '@testing-library/react';
+import { GenSwitcher } from '../GenSwitcher';
+
+describe('Popover', () => {
+  it('should render GenSwitcher component', async () => {
+    const testId = 'testGenSwitcher';
+    render(<GenSwitcher testId={testId} />);
+
+    const genSwitcher = screen.getByTestId(testId);
+
+    expect(genSwitcher).toBeInTheDocument();
+  });
+
+  it('should have Gen1 in button label when isGen1 is true', async () => {
+    render(<GenSwitcher isGen1={true} />);
+
+    const button = screen.getByRole('button');
+    const { getByText } = within(button);
+
+    expect(getByText(/Gen 1/)).toBeInTheDocument();
+  });
+
+  it('should have Gen2 in button label when isGen1 is false', async () => {
+    render(<GenSwitcher />);
+
+    const button = screen.getByRole('button');
+    const { getByText } = within(button);
+
+    expect(getByText(/Gen 2/)).toBeInTheDocument();
+  });
+
+  it('should have current class applied to Gen1 link when isGen1 is true ', async () => {
+    render(<GenSwitcher isGen1={true} />);
+
+    const gen1Link = screen.getByRole('link', { name: 'Gen 1' });
+
+    expect(gen1Link.classList).toContain('popover-list__link--current');
+  });
+
+  it('should have current class applied to Gen2 link when isGen1 is false ', async () => {
+    render(<GenSwitcher />);
+
+    const gen1Link = screen.getByRole('link', { name: 'Gen 2' });
+
+    expect(gen1Link.classList).toContain('popover-list__link--current');
+  });
+});
