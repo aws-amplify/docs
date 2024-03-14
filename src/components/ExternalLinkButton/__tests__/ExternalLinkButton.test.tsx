0948f5dc499768629bc8a72b51638f7096f204be
Katie Goines <30757403+katiegoines@users.noreply.github.com>
2024-03-14 10:02:22 -0700

    Update ExternalLinkButton.test.tsx

--- src/components/ExternalLinkButton/__tests__/ExternalLinkButton.test.tsx
+++ src/components/ExternalLinkButton/__tests__/ExternalLinkButton.test.tsx
@@ -38,20 +38,6 @@ describe('ExternalLinkButton', () => {
     );
   });
 
-  it('should render the ExternalLink icon', async () => {
-    render(component);
-
-    const externalLinkButtonNode = await screen.findByText('Click Here!');
-    const icon = Array.from(
-      externalLinkButtonNode.getElementsByClassName('amplify-icon')
-    );
-    const path = Array.from(icon[0].getElementsByTagName('path'))[0];
-    expect(path).toHaveAttribute(
-      'd',
-      'M3.42857 20.5714H20.5714V13.8H24V22.2857C24 23.2325 23.2325 24 22.2857 24H1.71429C0.767512 24 0 23.2325 0 22.2857V1.71429C0 0.767512 0.767512 0 1.71429 0H10.2857V3.42857H3.42857V20.5714Z'
-    );
-  });
-
   it('should trackExternalLink on click', async () => {
     jest.spyOn(trackModule, 'trackExternalLink');
     render(component);
