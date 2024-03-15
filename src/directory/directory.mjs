5c21c5935051b962b3aef14aaaada8b13a10f57c
Rene Brandel <renbran@amazon.com>
2024-03-15 14:47:40 -0400

    Updated to reflect changes to Gen 2 content

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -2080,9 +2080,6 @@ export const directory = {
                       path: 'src/pages/gen2/build-a-backend/data/customize-authz/configure-custom-identity-and-group-claim/index.mdx'
                     },
                     {
-                      path: 'src/pages/gen2/build-a-backend/data/customize-authz/use-iam-authz-within-appsync-console/index.mdx'
-                    },
-                    {
                       path: 'src/pages/gen2/build-a-backend/data/customize-authz/grant-lambda-function-access-to-api/index.mdx'
                     }
                   ]
