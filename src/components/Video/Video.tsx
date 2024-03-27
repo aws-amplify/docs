12a3c1c0df5e4b22bf7d102a44192e1576e001e5
Heather <hbuchel@gmail.com>
2024-03-27 15:24:41 -0400

    add prefers reduced motion check

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect, useRef } from 'react';
 import { View } from '@aws-amplify/ui-react';
 
 interface VideoProps {
@@ -5,9 +6,15 @@ interface VideoProps {
   muted?: boolean;
   loop?: true;
   src: string;
-  description?: string;
 }
 
+const reducedMotionMediaQuery = '(prefers-reduced-motion: no-preference)';
+
+/**
+ * @description The Video component defaults to a muted, auto play video.
+ * Currently, we also assume the surrounding content will adequately describe
+ * the video so we default to aria-hidden="true".
+ */
 export const Video = ({
   autoPlay = true,
   muted = true,
@@ -15,24 +22,57 @@ export const Video = ({
   src,
   ...rest
 }: VideoProps) => {
+  /**
+   * Assume user prefers reduced motion until we can check
+   * in the useEffect for the media query match, otherwise Next SSG
+   * will consider 'window' undefined.
+   */
+  const [prefersReducedMotion, setPrefersReducedMotion] =
+    useState<boolean>(true);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia(reducedMotionMediaQuery);
+
+    setPrefersReducedMotion(
+      !window.matchMedia(reducedMotionMediaQuery).matches
+    );
+
+    const mediaQueryListener = (event: MediaQueryListEvent) => {
+      setPrefersReducedMotion(!event.matches);
+    };
+
+    mediaQueryList.addEventListener('change', mediaQueryListener);
+    return () => {
+      mediaQueryList.removeEventListener('change', mediaQueryListener);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      videoRef?.current?.pause();
+    } else {
+      if (autoPlay) {
+        videoRef?.current?.play();
+      }
+    }
+  }, [prefersReducedMotion, autoPlay]);
+
   return (
-    <>
-      <View
-        as="video"
-        className="video"
-        autoPlay={autoPlay}
-        muted={muted}
-        loop={loop}
-        width="100%"
-        playsInline={true}
-        controls={true}
-        {...rest}
-        aria-label="Configuring AWS Amplify"
-        aria-describedby="videoDescription"
-      >
-        <source src={src} />
-      </View>
-      <p id="videoDescription">Some video description text</p>
-    </>
+    <View
+      ref={videoRef}
+      as="video"
+      className="video"
+      muted={muted}
+      loop={loop}
+      width="100%"
+      playsInline={true}
+      controls={true}
+      aria-hidden="true"
+      {...rest}
+    >
+      <source src={src} />
+    </View>
   );
 };
