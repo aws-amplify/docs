93263e938a79a915eb527ec32b1b1f945892e96e
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-22 12:18:56 -0400

    Cbonif/connect external ddb datasource (#7244)
    * add connect external ddb data sources page to directory
    
    * add content for connect external dynamodb data sources page
    
    * add images for connect external dynamodb data sources page

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -167,7 +167,12 @@ export const directory = {
                   ]
                 },
                 {
-                  path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/index.mdx'
+                  path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/index.mdx',
+                  children: [
+                    {
+                      path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-external-ddb-table/index.mdx'
+                    }
+                  ]
                 },
                 {
                   path: 'src/pages/[platform]/build-a-backend/data/custom-subscription/index.mdx'
