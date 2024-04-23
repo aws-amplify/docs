336076e23d0f19814533e2641cd8450fc489dc66
tjroach <tjroach@amazon.com>
2024-04-23 09:49:42 -0400

    Add Using Storagepath

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -338,6 +338,9 @@ export const directory = {
                   path: 'src/pages/[platform]/build-a-backend/storage/set-up-storage/index.mdx'
                 },
                 {
+                  path: 'src/pages/[platform]/build-a-backend/storage/storagepath/index.mdx'
+                },
+                {
                   path: 'src/pages/[platform]/build-a-backend/storage/upload/index.mdx'
                 },
                 {
