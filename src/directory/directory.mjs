2da40084c6d0467d18cc28f91e8231bbabb3674f
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-24 13:50:05 -0400

    add connect to eventbridge datasource page

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -168,6 +168,9 @@ export const directory = {
                   children: [
                     {
                       path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-external-ddb-table/index.mdx'
+                    },
+                    {
+                      path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-eventbridge-datasource/index.mdx'
                     }
                   ]
                 },
