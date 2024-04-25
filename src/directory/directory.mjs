654dd6473236c066276b0649f45c9929daee2c41
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-04-24 20:21:55 -0400

    add connect to amazon bedrock page to directory

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -177,6 +177,9 @@ export const directory = {
                   children: [
                     {
                       path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-external-ddb-table/index.mdx'
+                    },
+                    {
+                      path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-bedrock/index.mdx'
                     }
                   ]
                 },
