9174b38afc0369f92d725aa85e5abc2af8ed3a29
timngyn <timngyn@gmail.com>
2024-04-25 13:00:40 -0700

    Add custom MDX table component that is wrapped in ScrollView

---
+++ src/components/MDXComponents/MDXTable.tsx
@@ -0,0 +1,14 @@
+import { ScrollView } from '@aws-amplify/ui-react';
+import { HTMLAttributes, ReactNode } from 'react';
+
+interface MDXTableProps extends HTMLAttributes<HTMLTableElement> {
+  children: ReactNode;
+}
+
+export const MDXTable: React.FC<MDXTableProps> = ({ children, ...props }) => {
+  return (
+    <ScrollView tabIndex={0} aria-label="Accessible scrollview">
+      <table {...props}>{children}</table>
+    </ScrollView>
+  );
+};
