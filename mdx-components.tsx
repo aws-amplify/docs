1c28eecac1588d1ba8bf4f9e25008573d9420999
josefaidt <josef.aidt@gmail.com>
2024-04-24 17:39:14 -0700

    create types, declare globals for MDXComponent type checking

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
-import type { MDXComponents } from 'mdx/types';
+import type { Metadata } from 'next';
+import type { MDXComponents, MDXProps } from 'mdx/types';
+import type { PageNode } from '@/directory/directory';
+import type { Platform } from '@/constants/platforms';
 import ExportedImage from 'next-image-export-optimizer';
 import InlineFilter from './src/components/InlineFilter';
 import { YoutubeEmbed } from './src/components/YoutubeEmbed';
@@ -29,42 +32,69 @@ const MDXHeading4 = (props) => <MDXHeading level={4} {...props} />;
 const MDXHeading5 = (props) => <MDXHeading level={5} {...props} />;
 const MDXHeading6 = (props) => <MDXHeading level={6} {...props} />;
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    // Map markdown elements to custom components
-    a: MDXLink,
-    h1: MDXHeading1,
-    h2: MDXHeading2,
-    h3: MDXHeading3,
-    h4: MDXHeading4,
-    h5: MDXHeading5,
-    h6: MDXHeading6,
-    pre: (preProps) => {
-      const props = preToCodeBlock(preProps);
-      if (props) {
-        return <MDXCode {...props} />;
-      }
-      return <pre {...preProps} />;
-    },
-    img: ResponsiveImage,
+const components = {
+  // Map markdown elements to custom components
+  a: MDXLink,
+  h1: MDXHeading1,
+  h2: MDXHeading2,
+  h3: MDXHeading3,
+  h4: MDXHeading4,
+  h5: MDXHeading5,
+  h6: MDXHeading6,
+  pre: (preProps) => {
+    const props = preToCodeBlock(preProps);
+    if (props) {
+      return <MDXCode {...props} />;
+    }
+    return <pre {...preProps} />;
+  },
+  img: ResponsiveImage,
 
-    // Make common custom components available to content authors
-    Accordion,
-    Block,
-    BlockSwitcher,
-    Callout,
-    Fragments,
-    InlineFilter,
-    MigrationAlert,
-    YoutubeEmbed,
-    Overview,
-    ExternalLink,
-    ExternalLinkButton,
-    InternalLinkButton,
-    FilterContent,
-    Grid,
-    Columns,
-    View,
+  // Make common custom components available to content authors
+  Accordion,
+  Block,
+  BlockSwitcher,
+  Callout,
+  Fragments,
+  InlineFilter,
+  MigrationAlert,
+  YoutubeEmbed,
+  Overview,
+  ExternalLink,
+  ExternalLinkButton,
+  InternalLinkButton,
+  FilterContent,
+  Grid,
+  Columns,
+  View
+};
+
+/**
+ * Type for Next.js's getStaticProps return in MDX pages
+ * this is needed to satisfy the type-check for <Overview>'s childPageNodes
+ */
+type MDXGetStaticPropsResult = {
+  meta: Metadata & {
+    platforms: Platform[];
+  };
+  childPageNodes: PageNode[];
+};
+
+/**
+ * Declare types in the global scope for MDX to type-check
+ */
+declare global {
+  type MDXProvidedComponents = typeof components;
+  type Props = MDXProps & MDXGetStaticPropsResult;
+}
+
+/**
+ * @param _components Next.js components for MDX pages
+ * @returns all MDX components available for use in the page without import
+ */
+export function useMDXComponents(_components: MDXComponents) {
+  return {
+    ..._components,
     ...components
   };
 }
