454bc6439b122a3591f373a70dc2065d41bc5b20
Ali Spittel <ali07cat07@gmail.com>
2024-05-01 14:19:18 -0600

    add back troubleshooting guide

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -589,6 +589,14 @@ export const directory = {
             },
             {
               path: 'src/pages/[platform]/build-a-backend/q-developer/index.mdx'
+            },
+            {
+              path: 'src/pages/[platform]/build-a-backend/troubleshooting/index.mdx',
+              children: [
+                {
+                  path: 'src/pages/[platform]/build-a-backend/troubleshooting/library-not-configured/index.mdx'
+                }
+              ]
             }
           ]
         },
