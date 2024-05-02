1ddc8c4499f52526520ee273cc590f90a670ffad
Anil Maktala <maktala.anil@gmail.com>
2024-05-01 21:37:39 -0400

    corrected the path

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -193,7 +193,7 @@ export const directory = {
                   ]
                 },
                 {
-                  path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/working-with-files/index.mdx'
+                  path: 'src/pages/[platform]/build-a-backend/data/working-with-files/index.mdx'
                 },
                 {
                   path: 'src/pages/[platform]/build-a-backend/data/custom-subscription/index.mdx'
