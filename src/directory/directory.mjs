800a3ff507e03bc199590cef712e897fee4b944c
josefaidt <josef.aidt@gmail.com>
2024-05-01 17:17:04 -0700

    rm .json from the slug

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -841,7 +841,7 @@ export const directory = {
               path: 'src/pages/[platform]/reference/project-structure/index.mdx'
             },
             {
-              path: 'src/pages/[platform]/reference/amplify_outputs.json/index.mdx'
+              path: 'src/pages/[platform]/reference/amplify_outputs/index.mdx'
             },
             {
               path: 'src/pages/[platform]/reference/cdk-constructs/index.mdx'
