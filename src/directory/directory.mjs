9d9e8aeed46b358f38561d73c79f9bb3b0a7d7d3
josefaidt <josef.aidt@gmail.com>
2024-05-01 17:59:46 -0700

    rm ported "under the hood" (i.e. concepts)

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -160,9 +160,6 @@ export const directory = {
                   path: 'src/pages/[platform]/build-a-backend/auth/advanced-workflows/index.mdx'
                 },
                 {
-                  path: 'src/pages/[platform]/build-a-backend/auth/under-the-hood/index.mdx'
-                },
-                {
                   path: 'src/pages/[platform]/build-a-backend/auth/data-usage-policy/index.mdx'
                 },
                 {
