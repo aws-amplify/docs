7f9cc715ef5c817eaa53b3ecb0b5abcbbe87e34e
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-05-02 15:06:52 -0400

    review and edits

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -216,6 +216,9 @@ export const directory = {
                     },
                     {
                       path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-amazon-translate/index.mdx'
+                    },
+                    {
+                      path: 'src/pages/[platform]/build-a-backend/data/custom-business-logic/connect-http-datasource/index.mdx'
                     }
                   ]
                 },
