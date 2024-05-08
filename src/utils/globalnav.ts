3cba5850cbcd6cd6ebc3f640faf69ba9ee66c14e
timngyn <timngyn@gmail.com>
2024-05-08 10:53:14 -0700

    Move Learn to sidebar for gen 1

--- src/utils/globalnav.ts
+++ src/utils/globalnav.ts
@@ -16,37 +16,14 @@ export const RIGHT_NAV_LINKS = [
 export const LEFT_NAV_LINKS = [
   {
     type: 'DEFAULT',
-    label: 'Learn',
-    url: 'https://amplify.aws/learn',
-    order: 1
-  },
-  {
-    type: 'DEFAULT',
     label: 'UI Library',
     url: 'https://ui.docs.amplify.aws/',
-    order: 2
+    order: 1
   },
   {
     type: 'DEFAULT',
     label: 'Contribute',
     url: '/contribute/',
-    order: 3
-  }
-];
-
-export const SOCIAL_LINKS = [
-  {
-    type: 'ICON',
-    label: 'Discord',
-    url: 'https://discord.com/invite/amplify',
-    order: 8,
-    icon: 'DISCORD'
-  },
-  {
-    type: 'ICON',
-    label: 'Twitter',
-    url: 'https://twitter.com/AWSAmplify',
-    order: 9,
-    icon: 'TWITTER'
+    order: 2
   }
 ];
