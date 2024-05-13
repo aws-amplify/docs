7fc79d8bd785a1e1d6701474e7ea8aada4c3371e
Kha Truong <64438356+khatruong2009@users.noreply.github.com>
2024-05-13 13:14:11 -0700

    chore: delete functions pages from gen1 prev

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -2351,32 +2351,6 @@ export const directory = {
                       ]
                     },
                     {
-                      path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/index.mdx',
-                      children: [
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/set-up-function/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/layers/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/environment-variables/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/secrets/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/build-options/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/configure-options/index.mdx'
-                        },
-                        {
-                          path: 'src/pages/gen1/[platform]/prev/build-a-backend/functions/graphql-from-lambda/index.mdx'
-                        }
-                      ]
-                    },
-                    {
                       path: 'src/pages/gen1/[platform]/prev/build-a-backend/push-notifications/index.mdx',
                       children: [
                         {
