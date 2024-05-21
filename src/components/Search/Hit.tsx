451bc1a85de5f4d59d90594e57dbabf6b6ab8b87
Heather <hbuchel@gmail.com>
2024-05-21 15:48:47 -0400

    updates

--- src/components/Search/Hit.tsx
+++ src/components/Search/Hit.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
-
+import { Text } from '@aws-amplify/ui-react';
 export const Hit = ({ hit }) => {
   console.log(hit);
-  return <div>Hit</div>;
+  const { hierarchy, url_without_anchor, anchor, _snippetResult } = hit;
+  const snippet = _snippetResult ? _snippetResult.content.value : '';
+  return (
+    <>
+      <Text as="div" fontSize="small">
+        {hierarchy.lvl0}
+      </Text>
+      <a
+        href={`${url_without_anchor}${anchor !== 'pageMain' ? `#${anchor}` : ''}`}
+      >
+        <div dangerouslySetInnerHTML={{ __html: snippet }} />
+        <Text as="span" fontSize="small">
+          {hierarchy.lvl1}
+        </Text>
+      </a>
+    </>
+  );
 };
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import algoliasearch from 'algoliasearch/lite';
 import { SearchForm } from './SearchForm';
-import { InstantSearch, Hits } from 'react-instantsearch';
+import { InstantSearch, Hits, Configure } from 'react-instantsearch';
 import { Hit } from './Hit';
+import { useCurrentPlatform } from '@/utils/useCurrentPlatform';
+import { useIsGen1Page } from '@/utils/useIsGen1Page';
 
 const searchClient = algoliasearch(
   'HBI6AAVZLI',
   'de8634c2f3bcb5e46ea2b9debb77656d'
 );
 
-function HitComponent({ hit }) {
-  return JSON.stringify(hit);
-}
-
 export const Search = () => {
+  const currentPlatform = useCurrentPlatform() || '';
+  const isGen1 = useIsGen1Page();
   return (
     <InstantSearch
       future={{
@@ -22,8 +22,22 @@ export const Search = () => {
       searchClient={searchClient}
       indexName="next-amplify"
     >
+      <Configure
+        analytics={false}
+        facetFilters={[
+          `platform:${currentPlatform}`,
+          `gen:${isGen1 ? 'gen1' : 'gen2'}`
+        ]}
+        hitsPerPage={6}
+      />
       <SearchForm />
-      <Hits hitComponent={HitComponent} />
+      <Hits
+        classNames={{
+          list: 'search-results',
+          item: 'search-result'
+        }}
+        hitComponent={(hit) => <Hit hit={hit.hit} />}
+      />
     </InstantSearch>
   );
 };
