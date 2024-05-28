9920b7bd9e5b7abbf566bca12ab6877354fab62a
Heather <hbuchel@gmail.com>
2024-05-28 15:58:55 -0400

    add light mode/dark mode testing, clean up logging

--- .github/workflows/scripts/run_accessibility_scan.js
+++ .github/workflows/scripts/run_accessibility_scan.js
@@ -6,26 +6,68 @@ module.exports = {
     
     const violations = [];
     
+    // When flipping from dark mode to light mode, we need to add a small timeout
+    // to account for css transitions otherwise there can be false contrast issues found.
+    // Usage: await sleep(300);
+    const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+    const logViolation = (violation) => {
+      violation.nodes.forEach(node => {
+        console.log(node.failureSummary);
+        console.log(node.html);
+        node.target.forEach( target => {
+          console.log('CSS target: ', target)
+        })
+      })
+      
+    }
+
     async function runAxeAnalyze(pages) {
       for (const page of pages) {
-        console.log(`testing page http://localhost:3000${page}/`);
         const browser = await puppeteer.launch();
         const pageToVisit = await browser.newPage();
-        await pageToVisit.goto(`http://localhost:3000${page}/`);
+        await pageToVisit.goto(`http://localhost:3000${page}/`, {waitUntil: 'domcontentloaded'});
+        await pageToVisit.click('button[title="Light mode"]');
+        await pageToVisit.waitForSelector('[data-amplify-color-mode="light"]');
+        await sleep(300);
+
+        
         try {
+          console.log(`\nTesting light mode: http://localhost:3000${page}/`)
           const results = await new AxePuppeteer(pageToVisit).analyze();
           if(results.violations.length > 0) {
             results.violations.forEach(violation => {
-              console.log(violation);
+              logViolation(violation);
               violations.push(violation);
             })
           } else {
-            console.log('No violations found.');
+            console.log('No violations found. \n');
           }
           
         } catch (error) {
-          core.setFailed(`There was an error running the accessibility scan: `, error);
+          core.setFailed(`There was an error testing the page: ${error}`);
         }
+
+        await pageToVisit.click('button[title="Dark mode"]');
+        await pageToVisit.waitForSelector('[data-amplify-color-mode="dark"]');
+        await sleep(300);
+        
+        try {
+          console.log(`\nTesting dark mode: http://localhost:3000${page}/`)
+          const results = await new AxePuppeteer(pageToVisit).analyze();
+          if(results.violations.length > 0) {
+            results.violations.forEach(violation => {
+              logViolation(violation);
+              violations.push(violation);
+            })
+          } else {
+            console.log('No violations found. \n');
+          }
+          
+        } catch (error) {
+          core.setFailed(`There was an error testing the page: ${error}`);
+        }
+
         await browser.close();
       }
       if(violations.length > 0) {
