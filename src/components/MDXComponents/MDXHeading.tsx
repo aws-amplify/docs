d92b056110b428bfc992a6df4fa64e98992ad9f2
Heather <hbuchel@gmail.com>
2024-06-03 14:25:04 -0400

    remove code replaced by rehype plugin

--- src/components/MDXComponents/MDXHeading.tsx
+++ src/components/MDXComponents/MDXHeading.tsx
@@ -1,34 +1,8 @@
 import Link from 'next/link';
 import { Heading } from '@aws-amplify/ui-react';
-// import slug from './utils/slug';
 
 export const MDXHeading = (props) => {
-  console.log('props: ', props);
   const { level, children, id } = props;
-  // let href = '';
-
-  /* Test if children element is not a string before creating the url slug */
-  // if (children && typeof children != 'string') {
-  //   let newChildren = '';
-  //   /* Test if child element is a single object */
-  //   if (typeof children == 'object' && children.props?.children) {
-  //     newChildren = children.props.children;
-  //   } else {
-  //     /* If not a single object, we expect an array of
-  //     elements and loop through them */
-  //     for (let i = 0; i < children.length; i++) {
-  //       if (typeof children[i] == 'string') {
-  //         newChildren = newChildren + children[i];
-  //       } else {
-  //         newChildren = newChildren + children[i].props.children;
-  //       }
-  //     }
-  //   }
-  //   href = `${slug(newChildren)}`;
-  // } else {
-  //   /* If children element is a string, use that to create the url slug */
-  //   href = `${slug(children)}`;
-  // }
 
   return (
     <Heading level={level} id={id}>
