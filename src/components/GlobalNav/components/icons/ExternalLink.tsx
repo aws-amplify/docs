0056f9d9bf21e5f5b5a75ab688662f0120aeb31d
Heather <hbuchel@gmail.com>
2024-06-04 12:59:09 -0400

    Fix any type on GlobalNav

--- src/components/GlobalNav/components/icons/ExternalLink.tsx
+++ src/components/GlobalNav/components/icons/ExternalLink.tsx
@@ -1,7 +1,8 @@
 import { Flex } from '@aws-amplify/ui-react';
 import { IconExternalLink } from '@/components/Icons';
+import React from 'react';
 
-export function ExternalLink({ children }: { children: any }) {
+export function ExternalLink({ children }: { children: React.ReactNode }) {
   return (
     <Flex alignItems="center" gap="xs">
       {children}
