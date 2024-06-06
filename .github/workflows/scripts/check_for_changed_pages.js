e00f5ff12753e75376251fb5bc0298fb31022574
Heather <hbuchel@gmail.com>
2024-06-06 16:34:44 -0400

    covert chain promises to async/await, some formatting/comments updated

--- .github/workflows/scripts/check_for_changed_pages.js
+++ .github/workflows/scripts/check_for_changed_pages.js
@@ -1,63 +1,74 @@
 module.exports = {
-  getChangedPages: ({ github, context, buildDir }) => {
+  getChangedPages: async ({ github, context, buildDir }) => {
     const fs = require('fs');
     const cheerio = require('cheerio');
-    
+
     const urlList = [];
-    
+
     const {
       issue: { number: issue_number },
       repo: { owner, repo }
     } = context;
 
-    // Use the Github API to query for the list of files from the PR
-    return github
-      .paginate(
-        'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
-        { owner, repo, pull_number: issue_number },
-        (response) => response.data.filter((file) => (file.status === 'modified' || file.status === 'added'))
-      )
-      .then((files) => {
-        const possiblePages = [];
-        const platforms = [
-          'android',
-          'angular',
-          'flutter',
-          'javascript',
-          'nextjs',
-          'react',
-          'react-native',
-          'swift',
-          'vue',
-        ]
-        files.forEach(({filename}) => {
-          const isPage = filename.startsWith('src/pages') && (filename.endsWith('index.mdx') || filename.endsWith('index.tsx'));
-          if(isPage) {
+    const possiblePages = [];
+    const platforms = [
+      'android',
+      'angular',
+      'flutter',
+      'javascript',
+      'nextjs',
+      'react',
+      'react-native',
+      'swift',
+      'vue'
+    ];
 
-            const path = filename.replace('src/pages', '').replace('/index.mdx', '').replace('/index.tsx', '');
-            if(path.includes('[platform]')) {
-              platforms.forEach((platform) => {
-                possiblePages.push(path.replace('[platform]', platform));
-              })
-            } else {
-              possiblePages.push(path);
-            }
-          }
-        });
+    const changedFiles = await github.paginate(
+      'GET /repos/{owner}/{repo}/pulls/{pull_number}/files',
+      { owner, repo, pull_number: issue_number },
+      (response) =>
+        response.data.filter(
+          (file) => file.status === 'modified' || file.status === 'added'
+        )
+    );
 
-        const siteMap = fs.readFileSync(`${buildDir}/sitemap.xml`);
-    
-        const siteMapParse = cheerio.load(siteMap, {
-          xml: true
-        });
+    // Get only the changed files that are pages and build out the
+    // possiblePages array
+    changedFiles.forEach(({ filename }) => {
+      const isPage =
+        filename.startsWith('src/pages') &&
+        (filename.endsWith('index.mdx') || filename.endsWith('index.tsx'));
+      if (isPage) {
+        const path = filename
+          .replace('src/pages', '')
+          .replace('/index.mdx', '')
+          .replace('/index.tsx', '');
+        if (path.includes('[platform]')) {
+          platforms.forEach((platform) => {
+            possiblePages.push(path.replace('[platform]', platform));
+          });
+        } else {
+          possiblePages.push(path);
+        }
+      }
+    });
 
-        siteMapParse('url').each(function () {
-          urlList.push(siteMapParse(this).find('loc').text());
-        });
+    // Get the sitemap and parse for an array of site URLs
+    const siteMap = fs.readFileSync(`${buildDir}/sitemap.xml`);
 
-        const pages = possiblePages.filter((page) => urlList.includes(`https://docs.amplify.aws${page}/`));
-        
-        return pages;
-      });
-  },
-}
+    const siteMapParse = cheerio.load(siteMap, {
+      xml: true
+    });
+
+    siteMapParse('url').each(function () {
+      urlList.push(siteMapParse(this).find('loc').text());
+    });
+
+    // Filter the possiblePages for only those that are part of the sitemap
+    const pages = possiblePages.filter((page) =>
+      urlList.includes(`https://docs.amplify.aws${page}/`)
+    );
+
+    return pages;
+  }
+};
