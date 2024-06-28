0d02cbfc6be5e6ee11200b687ab002d465909122
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-06-28 15:55:37 -0400

    add protected redaction message components

---
+++ src/protected/ProtectedRedactionMessage/index.tsx
@@ -0,0 +1,37 @@
+import { Callout } from '@/components/Callout';
+
+export const PROTECTED_REDACTION_GEN1_MESSAGE = `With versions of Amplify CLI \`@aws-amplify/cli@12.12.2\` and API Category \`@aws-amplify/amplify-category-api@5.11.5\`, an improvement was made to how relational field data is handled in subscriptions when different authorization rules apply to related models in a schema. The improvement redacts the values for the relational fields, displaying them as null or empty, to prevent unauthorized access to relational data.
+
+  This redaction occurs whenever it cannot be determined that the child model will be protected by the same permissions as the parent model.
+
+  Because subscriptions are tied to mutations and the selection set provided in the result of a mutation is then passed through to the subscription, relational fields in the result of mutations must be redacted.
+
+  If an authorized end-user needs access to the redacted relational fields, they should perform a query to read the relational data.
+
+  Additionally, subscriptions will inherit related authorization when relational fields are set as required. To better protect relational data, consider modifying the schema to use optional relational fields.
+
+  Based on the security posture of your application, you can choose to revert to the subscription behavior before this improvement was made.
+  To do so, use the \`subscriptionsInheritPrimaryAuth\` feature flag under \`graphqltransformer\` in the \`amplify/backend/cli.json\` file.
+
+  - If enabled, subscriptions will inherit the primary model authorization rules for the relational fields.
+  - If disabled, relational fields will be redacted in mutation response when there is a difference between auth rules between primary and related models.`;
+
+export const PROTECTED_REDACTION_GEN2_MESSAGE = `With Amplify Data Construct \`@aws-amplify/data-construct@1.8.4\`, an improvement was made to how relational field data is handled in subscriptions when different authorization rules apply to related models in a schema. The improvement redacts the values for the relational fields, displaying them as null or empty, to prevent unauthorized access to relational data.
+
+  This redaction occurs whenever it cannot be determined that the child model will be protected by the same permissions as the parent model.
+
+  Because subscriptions are tied to mutations and the selection set provided in the result of a mutation is then passed through to the subscription, relational fields in the result of mutations must be redacted.
+
+  If an authorized end-user needs access to the redacted relational fields, they should perform a query to read the relational data.
+
+  Additionally, subscriptions will inherit related authorization when relational fields are set as required. To better protect relational data, consider modifying the schema to use optional relational fields.`;
+
+// WARNING: The messaging in this component should NOT be changed without the appropriate approvals
+export const ProtectedRedactionGen1Message = () => (
+  <Callout warning>{PROTECTED_REDACTION_GEN1_MESSAGE}</Callout>
+);
+
+// WARNING: The messaging in this component should NOT be changed without the appropriate approvals
+export const ProtectedRedactionGen2Message = () => (
+  <Callout warning>{PROTECTED_REDACTION_GEN2_MESSAGE}</Callout>
+);
