f82976a85c793035f28846dc8d75474322651cc0
katiegoines <katiegoines@gmail.com>
2024-09-05 13:28:09 -0700

    added caption to tables

--- src/components/MDXComponents/MDXTable.tsx
+++ src/components/MDXComponents/MDXTable.tsx
@@ -1,18 +1,40 @@
 import { ScrollView } from '@aws-amplify/ui-react';
-import { HTMLAttributes, ReactNode } from 'react';
+import { HTMLAttributes, ReactNode, useEffect, useRef, useState } from 'react';
 
 interface MDXTableProps extends HTMLAttributes<HTMLTableElement> {
   children: ReactNode;
 }
 
 export const MDXTable: React.FC<MDXTableProps> = ({ children, ...props }) => {
+  const ref = useRef<HTMLTableElement>(null);
+  const [caption, setCaption] = useState('');
+
+  useEffect(() => {
+    const getHeading = (el) => {
+      if (el?.previousElementSibling?.tagName.startsWith('H')) {
+        setCaption(el.previousElementSibling.textContent);
+      } else {
+        getHeading(ref.current?.previousElementSibling);
+      }
+    };
+    getHeading(ref.current);
+  }, []);
+
+  const tableId = caption.includes(' ') ? caption.split(' ').join('') : caption;
+
   return (
     <ScrollView
       tabIndex={0}
       aria-label="Scrollable table"
+      aria-labelledby={'table:' + tableId}
       className="scrollview"
+      role="region"
+      ref={ref}
     >
-      <table {...props}>{children}</table>
+      <table {...props}>
+        <caption id={'table:' + tableId}>Table: {caption}</caption>
+        {children}
+      </table>
     </ScrollView>
   );
 };
