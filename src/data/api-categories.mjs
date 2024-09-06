c1877afb240d29b53926cbf47f525b0195fad64e
Jacob Logan <lognjc@amazon.com>
2024-09-06 11:45:44 -0700

    add sub categories

--- src/data/api-categories.mjs
+++ src/data/api-categories.mjs
@@ -1,8 +1,11 @@
 // mapping of api categories coming in from libraries to the associated categories in the docs
 export const API_CATEGORIES = {
   auth: 'auth',
-  storage: 'storage',
-  'add-aws-services/analytics': 'analytics',
-  'add-aws-services/rest-api': 'api',
-  'add-aws-services/in-app-messaging': 'in-app-messaging'
+  storage: 'storage'
+};
+
+export const API_SUB_CATEGORIES = {
+  analytics: 'analytics',
+  'rest-api': 'api',
+  'in-app-messaging': 'in-app-messaging'
 };
