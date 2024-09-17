44cd92c7eec088ca3ffdb7f722d7b87b8405f52e
Jacob Logan <lognjc@amazon.com>
2024-09-17 15:02:36 -0700

    add api link legend

--- src/components/ApiDocs/ReferencePage.tsx
+++ src/components/ApiDocs/ReferencePage.tsx
@@ -1,7 +1,7 @@
 import { Fragment } from 'react';
 
 import { FunctionReference } from './FunctionReference';
-import { Divider, View } from '@aws-amplify/ui-react';
+import { Divider, View, Flex } from '@aws-amplify/ui-react';
 import { API_CATEGORIES, API_SUB_CATEGORIES } from '@/data/api-categories.mjs';
 import references from '@/directory/apiReferences.json';
 
@@ -18,6 +18,20 @@ export const ReferencePage = ({ category }) => {
           <FunctionReference func={child} />
         </Fragment>
       ))}
+      <Flex className="api-legend-container">
+        <Flex>
+          <View as="span" className="api-legend interface" />
+          Interface
+        </Flex>
+        <Flex>
+          <View as="span" className="api-legend reference" />
+          Reference
+        </Flex>
+        <Flex>
+          <View as="span" className="api-legend union" />
+          Union/Intersection
+        </Flex>
+      </Flex>
     </View>
   );
 };
