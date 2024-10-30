646cc6312cfd031ac52b892679ff3dc5d5aebef4
Ivan Artemiev <29709626+iartemiev@users.noreply.github.com>
2024-10-30 12:49:25 -0400

    feat: appsync events

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -321,6 +321,9 @@ export const directory = {
                   path: 'src/pages/[platform]/build-a-backend/data/optimistic-ui/index.mdx'
                 },
                 {
+                  path: 'src/pages/[platform]/build-a-backend/data/connect-event-api/index.mdx'
+                },
+                {
                   path: 'src/pages/[platform]/build-a-backend/data/override-resources/index.mdx'
                 },
                 {
