664a21ae179d766df620b5a0e2bf4fff068b6674
Allan Zheng <zheallan@amazon.com>
2025-03-16 00:03:22 -0700

    feat: add error handling page for Storage & Auth

--- src/directory/directory.mjs
+++ src/directory/directory.mjs
@@ -128,6 +128,9 @@ export const directory = {
                     },
                     {
                       path: 'src/pages/[platform]/build-a-backend/auth/connect-your-frontend/multi-step-sign-in/index.mdx'
+                    },
+                    {
+                      path: 'src/pages/[platform]/build-a-backend/auth/connect-your-frontend/error-handling/index.mdx'
                     }
                   ]
                 },
@@ -390,6 +393,9 @@ export const directory = {
                 },
                 {
                   path: 'src/pages/[platform]/build-a-backend/storage/manage-with-amplify-console/index.mdx'
+                },
+                {
+                  path: 'src/pages/[platform]/build-a-backend/storage/error-handling/index.mdx'
                 }
               ]
             },
